ec1eb809ccad103802f3200f0019e34c53785a9d
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-02 21:00:38 -0400

    new: count records and bytes

---
+++ countInput-map.lua
@@ -0,0 +1,21 @@
+#!/home/rel292/local/bin/torch
+-- count number of records and bytes in stdin map function
+-- stdin format: records consisting of pairs of strings
+--   (keyname --> count)
+-- stdout format:
+--   (keyname --> sum of counts for that keyname)
+
+local records = 0
+local keyBytes = 0
+local valueBytes = 0
+for line in io.stdin:lines('*l') do
+    if line == nil then break end
+    records = records + 1
+    local key, value = string.match(line, '^(.*)\t(.*)$')  -- TODO: make shared function
+    keyBytes = keyBytes + string.len(key)
+    valueBytes = valueBytes + string.len(value)
+end
+print('records' .. '\t' .. tostring(records))
+print('keyBytes' .. '\t' .. tostring(keyBytes))
+print('valueBytes' .. '\t' .. tostring(valueBytes))
+
---
+++ countInput-reduce.lua
@@ -0,0 +1,30 @@
+#!/home/rel292/local/bin/torch
+---- count number of records in standard in reduce function
+-- this is the reduce
+-- input is in some arbitrary order
+--   ('records' --> <number of records>) ...
+--   ('keyBytes' --> <number of bytes in keys>) ...
+--   ('valueBytes' --> <number of bytes in values>) ...
+-- output is total for each key value
+-- NOTE: keys are arbitary strings, not restricted to value named above
+
+local lastKey = nil
+local count = 0
+for line in io.stdin:lines('*l') do
+    if line == nil then break end
+    local key, value = string.match(line, '^(.*)\t(.*)$')
+    if lastKey == key then
+        count = count + tonumber(value)
+    else
+        if lastKey then
+            print(lastKey .. '\t' .. count)
+        end
+        lastKey = key
+        count = tonumber(value)
+    end
+end
+if lastKey then 
+    print(lastKey .. '\t' .. count)
+end
+
+
