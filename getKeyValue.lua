f54529dc831fb338b46d9c6c61122ccd75ed74ba
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-21 16:34:04 -0400

    first commit

---
+++ getKeyValue.lua
@@ -0,0 +1,14 @@
+-- parse key and value from input record
+-- ARGS:
+-- s : string, the input record
+-- 
+-- RETURNS:
+-- key : string, bytes in s up to but not including the first tab (\t)
+-- value : string, bytes in s after the tab (\t); potentially empty
+function getKeyValue(s)
+    local key, value = string.match(s, '^(.*)\t(.*)$')
+    if key == nil then
+        return s, nil
+    end
+    return key, value
+end
