4d52b63e0bb27c5b58d8c570551c21f903ec1250
Roy Lowrance <roy.lowrance@gmail.com>
2013-09-21 17:21:30 -0400

    fix documentation

--- getKeyValue.lua
+++ getKeyValue.lua
@@ -3,7 +3,7 @@
 -- s : string, the input record
 -- 
 -- RETURNS:
--- key : string, bytes in s up to but not including the first tab (\t)
+-- key   : string, bytes in s up to but not including the first tab (\t)
 -- value : string, bytes in s after the tab (\t); potentially empty
 function getKeyValue(s)
     local key, value = string.match(s, '^(.*)\t(.*)$')
