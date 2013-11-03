d276b2afe4139771ae695160099db2c34d5b89f1
Roy Lowrance <roy.lowrance@gmail.com>
2013-11-03 13:47:18 -0500

    Replace sumbolic links with copies.
    To get to work on servers after git hub pull. The linked-to directory
    is not on the server.

--- sgdBottou.lua
+++ sgdBottou.lua
@@ -1,1 +1,186 @@
-/home/roy/Dropbox/github-clones/optim/sgdBottou.lua
+-- sgdBottou.lua
+
+require 'makeVp'
+require 'pressEnter'
+require 'validateAttributes'
+
+if optim == nil then
+   optim = {}
+end
+
+-- An implementation of Leon Bottou's idea for dynamically adjusting the learning rate eta.
+-- 
+-- Ref: leon.bottou.org/slides/largescale/lstut.pdf page 37
+-- Bottou's idea:
+-- The update is w_{t+1} := w_t - eta gradient(w_t, x_t, y_t)
+-- "At any moment during training, we can:
+--  - Select a small subsample of the examples.
+--  - Try various gains eta on the subsample.
+--  - Pick the gain eta that most reduces the cost.
+--  - Use it for the next 100000 iterations on the full dataset."
+--
+-- ARGS:
+-- opfun  : function(x, [index]) --> f(x), df/dx
+--          return the loss and gradient using point x and one sample
+--          x     : 1D Tensor, the weights (aka, the parameters, theta)
+--          index : optional integer > 0, example number to use
+--                  if not supplied, use a random sample (for example, iterate through
+--                  all the samples in random order)
+--                  when doing normal iterations, the call is opfunc(x)
+--                  when testing on a subsample, the all is opfunc(x, subsampleindex)
+-- x      : 1D Tensor, the initial point
+-- config : table of configuration variables
+--          config.nSamples    : integer > 0, number of samples
+--          config.nSubsamples : integer > 0, number of subsamples (<= nSamples)
+--          config.eta         : initial eta
+--          config.newEtas     : function(eta) --> {eta1, eta2, ..., etaN}
+--                               eta's to test on the subsample
+--                               current eta value is added to this set
+--          config.evalCounter : number of iterations before finding a new eta
+--          config.printEta    : if true, print eta when it changes
+-- state  : table describing state of the optimizer
+--          considered private, so may vary in different implementations
+-- RETURNS
+-- x      : new x vector
+-- f(x)   : table with one entry, the function value, before the update
+function optim.sgdBottou(opfunc, x, config, state)
+   local timer = Timer()
+   local reportTiming = global.reportTiming.sgdBottou
+   local vp, verboseLevel = makeVp(0, 'optim.sgdBottou')
+   vp(1, 'config', config)
+   validateAttributes(opfunc, 'function')
+   validateAttributes(x, 'Tensor', '1D')
+   validateAttributes(config, 'table')
+   validateAttributes(state, 'table')
+
+   -- initialize state using values in config
+   if state.evalCounter == nil then
+      state.evalCounter = 0
+   end
+
+   if state.eta == nil then
+      state.eta = config.eta
+   end
+   vp(1, 'state after initialization', state)
+
+   -- initially and periodically explore new eta values
+   if state.evalCounter == 0 or state.evalCounter % config.evalCounter == 0 then
+      if reportTiming then
+         vp(0, 'state.evalCounter', state.evalCounter, 'config.evalCounter', config.evalCounter)
+      end
+
+      if verboseLevel >= 3 then
+         -- pressEnter('starting search for better eta')
+      end
+
+      local currentX = x
+
+      -- construct random subsample
+      local randperm = torch.randperm(config.nSamples)
+
+      local etas = config.newEtas(state.eta)  -- candidate etas
+
+      -- determine average loss on the sub sample for some eta value
+      local function avgLossOnSubsample(eta)
+         local x = currentX  -- start with current eta value
+         for i = 1, config.nSubsamples do
+            loss, gradient = opfunc(x, randperm[i])
+            x = x - gradient * eta
+               totalLoss = totalLoss + loss
+         end
+         local avgLoss = totalLoss / config.nSubsamples
+         return avgLoss
+      end
+
+      -- determine average loss and updated weights for a candidate eta value
+      local function testCandidate(candidateEta)
+            local totalLoss = 0
+            local x = currentX   -- start each test at the same theta value
+            for i = 1, config.nSubsamples do
+               local loss, gradient = opfunc(x, randperm[i])
+               x = x - gradient * candidateEta
+               totalLoss = totalLoss + loss
+            end
+            return totalLoss / config.nSubsamples, x
+      end
+
+      -- determine best Eta from the candidates
+      -- return bestEta and loss for best eta and weights for best eta
+      local function testCandidates(candidateEtas)
+         local vp = makeVp(0, 'testCandidates')
+         vp(1, 'candidateEtas', candidateEtas)
+         vp(1, 'config.nSubsamples', config.nSubsamples)
+         local bestEta = nil
+         local bestLoss = math.huge
+         local bestX = nil
+         
+         for _, eta in ipairs(candidateEtas) do
+            local candidateLoss, candidateX = testCandidate(eta)
+            --vp(0, 'eta', eta, 'avgLoss', avgLoss)
+            if candidateLoss < bestLoss then
+               bestEta = eta
+               bestLoss = candidateLoss
+               bestX = x
+            end
+         end
+
+         if bestEta == nil then
+            error('none of the candidate etas had a loss < infinity')
+         end
+
+         vp(1, 'bestEta', bestEta, 'bestLoss', bestLoss)
+         return bestEta, bestLoss, bestX
+      end
+
+      local function union(some, another)
+         for _, one in ipairs(some) do
+            if one == another then
+               return some
+            end
+         end
+         table.insert(some, another)
+         return some
+      end
+
+      -- determine and start to use the best eta, also considering the current eta
+      local bestEta, bestLoss, bestX = 
+         testCandidates(union(config.newEtas(state.eta), state.eta))
+      state.eta = bestEta   
+
+      -- keep track of all the eta values
+      if state.etas == nil then
+         state.etas = {bestEta}
+      else
+         table.insert(state.etas, bestEta)
+      end
+
+      -- restart the iterations using the best result from the candidate explorations
+      x = bestX
+
+      if config.printEta then
+         if verboseLevel >= 3 then
+            pressEnter('eta updated to ' .. tostring(state.eta))
+         end
+      end
+
+      if reportTiming then
+         vp(0, string.format('new best eta %f; cpu sec %f', bestEta, timer:cpu()))
+         timer:reset()
+      end
+   end
+
+   -- take one step of size eta
+   validateAttributes(x, 'Tensor')
+   local loss, gradient = opfunc(x)
+   x = x - gradient * state.eta
+   validateAttributes(x, 'Tensor')
+   state.evalCounter = state.evalCounter + 1
+   vp(3, 'evalCounter', evalCounter, 'updated x', x, 'loss before update', loss)
+
+   if reportTiming then
+      vp(0, 'take step; cpu sec', timer:cpu())
+      timer:reset()
+   end
+
+   return x, {loss}
+end
--- sgdBottouDriver.lua
+++ sgdBottouDriver.lua
@@ -1,1 +1,137 @@
-/home/roy/Dropbox/github-clones/optim/sgdBottouDriver.lua
+-- sgdBottouDriver.lua
+
+require 'makeVp'
+require 'optim'
+require 'sgdBottou'
+require 'validateAttributes'
+
+-- use optim.sgdBottou to optimize a function
+-- ARGS
+-- opfunc    : function(x, index) --> loss, gradient
+--             as needed for optim.sgdBottou
+-- config    : table of configuration parameters as needed for optim.sgdBottou
+-- nSamples  : number > 0
+-- initialX  : 1D Tensor
+-- tolX      : optional number, iterations stop if norm(x_t - x_{t+1}) < tolX
+--             test is performed at the end of an epoch, so t = epoch number
+-- tolF      : optional number, iterations stop if |f(x_t) - f(x_{t+1})| < tolF
+--             test is performed at end of an epoch using the average loss for the epoch
+-- maxEpochs : no more than this number of epochs are run
+-- verbose   : optional number, default 0
+--             if verbose == 1 then print the avg loss for each epoch
+--             if verbose == 2 then also print the weight vector at the end of each epoch
+-- RETURNS
+-- xStar     : optimal x value found
+-- avgLoss   : number, average loss from last Epoch
+-- state     : table, final state from optim.sgdBottou
+function sgdBottouDriver(opfunc, config, nSamples, initialX, 
+                         tolX, tolF, maxEpochs, 
+                         verbose)
+   local timer = Timer()
+   local reportTiming = global.reportTiming.sgdBottouDriver
+   local vp = makeVp(0, 'sgdBottouDriver')
+
+   -- validate args
+   validateAttributes(opfunc, 'function')
+   validateAttributes(config, 'table')
+   validateAttributes(nSamples, 'number', '>', 0)
+   validateAttributes(initialX, 'Tensor', '1D')
+   validateAttributes(tolX, {'number', 'nil'})
+   validateAttributes(tolF, {'number', 'nil'})
+   validateAttributes(maxEpochs, {'number', 'nil'})
+   validateAttributes(verbose, {'number', 'nil'})
+   assert(tolX ~= nil or tolF ~= nil or maxEpochs ~= nil)
+
+   -- provide defaults
+   if verbose == nil then
+      verbose = 0 
+   end
+   
+   -- run an epoch using the specified weight (=x)
+   -- return updated weight and average loss for the epoch
+   local state = {}
+   local function epoch(xArg)
+      local vp = makeVp(0, 'epoch')
+      vp(1, 'xArg', xArg)
+      local sumLosses = 0
+      local x = xArg:clone()  -- don't change caller's arg value
+      for iteration = 1, nSamples do
+         vp(2, 'x before update', x)
+         validateAttributes(x, 'Tensor')
+         local xNew, losses = optim.sgdBottou(opfunc, x, config, state)
+         vp(2, 'x after update', xNew)
+         x = xNew
+         sumLosses = sumLosses + losses[1]
+      end
+      local avgLoss = sumLosses / nSamples
+      return x, avgLoss
+   end
+
+   if reportTiming then
+      vp(0, 'setup; cpu sec', timer:cpu())
+      timer:reset()
+   end
+
+   -- iterate until convergence
+   local x = initialX
+   local lastAvgLoss = math.huge
+   local nEpochs = 0
+   local lastX = nil
+   local lastAvgLoss = nil
+   repeat 
+      local epochTimer = Timer()
+      local newX, avgLoss = epoch(x)
+      nEpochs = nEpochs + 1
+      x = newX
+
+      if lastAvgLoss ~= nil and avgLoss > lastAvgLoss then
+         -- loss is increasing; decrease step size eta
+         state.eta = state.eta / 2
+         if verbose > 0 then
+            print('loss increasing, reduced eta to ' .. tostring(state.eta))
+         end
+      end
+      
+      if verbose > 0 then
+         local s = string.format('epoch %d avg loss %f', nEpochs, avgLoss)
+         if verbose == 1 then
+            vp(0, s)
+         elseif verbose == 2 then
+            vp(0, s .. ' weights', x)
+         end
+      end
+
+      -- perform each convergence test
+      local testX = tolX ~= nil and nEpochs > 1 and torch.norm(x - lastX) < tolX
+      if testX then
+         state.converged = 'tolX'
+      end
+
+      local testF = tolF ~= nil and nEpochs > 1 and math.abs(avgLoss - lastAvgLoss) < tolF
+      if testF then
+         state.converged = 'testF'
+      end
+
+      local epochTest = maxEpoch ~= nil and nEpochs >= maxEpochs
+      if epochTest then
+         state.converged = 'maxEpochs'
+      end
+            
+      local converged = testX or testF or epochTest
+      
+      lastX = x
+      lastAvgLoss = avgLoss
+
+      if reportTiming then
+         vp(0, string.format('epoch %d cpu sec %f', nEpochs, epochTimer:cpu()))
+      end
+
+   until converged
+
+   if reportTiming then
+      vp(0, 'all epochs; cpu sec', timer:cpu())
+      timer:reset()
+   end
+      
+   return x, lastAvgLoss, state
+end
