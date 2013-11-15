75993af6ba3f43426fa2cd8ce163365ec1921775
Roy Lowrance <roy.lowrance@gmail.com>
2013-11-15 15:34:02 -0500

    update API to simplify usage

--- Timer.lua
+++ Timer.lua
@@ -12,12 +12,46 @@ if false then
    timer:system()        -- just system CPU
 
    timer:reset()         -- restart from 0
+   
+   -- timing portions of a function
+   timer = Timer(vp)
+   timer:lap('part 1')
+   timer:lap('part 2')
+   timer:verbose(2, 'cpu')    -- call vp(2, 'lap name', cpuInLap) for each lap
+   timer:verbose(2, 'wallclock')
 end
+
+require 'makeVp'
+require 'validateAttributes'
 
 torch.class('Timer')
 
-function Timer:__init()
+function Timer:__init(vp)
    self.timer = torch.Timer()
+   self.vp = vp
+   self.cpuTable = {}
+   self.wallclockTable = {}
+end
+
+-- save cpu and wallclock time; then reset
+function Timer:lap(lapName)
+   --local vp = makeVp(2, 'Timer:lap')
+   --vp(1, 'self', self, 'lapName', lapName)
+   validateAttributes(lapName, 'string')
+   x = self:cpu()
+   self.cpuTable[lapName] = self:cpu()
+   self.wallclockTable[lapName] = self:wallclock()
+   self:reset()
+end
+
+-- verbose print cpu time only (for now)
+function Timer:verbose(verboseLevel, what)
+   validateAttributes(verboseLevel, 'number')
+   validateAttributes(what, 'string')
+   assert(what == 'cpu' or what == 'wallclock')
+   for k, v in pairs(self[what .. 'Table']) do
+      self.vp(verboseLevel, what .. ' secs ' .. k, v)
+   end
 end
 
 function Timer:reset()
--- Timer_test.lua
+++ Timer_test.lua
@@ -1,8 +1,19 @@
 -- Timer_test.lua
 -- unit test of Timer.lua
 
+require 'makeVp'
 require 'Timer'
 
+local vp = makeVp(2, 'tester')
+
+-- new version of API
+timer = Timer(vp)
+timer:lap('one')
+timer:lap('two')
+timer:verbose(1, 'cpu')
+timer:verbose(1, 'wallclock')
+
+-- old version of API (still supported)
 timer = Timer()
 assert(timer:wallclock() >= timer:cpu())
 assert(timer:user() >= 0)
