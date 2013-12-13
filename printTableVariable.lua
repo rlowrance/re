6b4759b4fdd3dd6bedc3f1112074b98eee3cb490
Roy Lowrance <roy.lowrance@gmail.com>
2013-12-13 14:18:35 -0500

    fix error message

--- printTableVariable.lua
+++ printTableVariable.lua
@@ -7,7 +7,7 @@ function printTableVariable(variableName)
    assert(variableName ~= nil, 'missing variableName argument')
    local sf = StackFrame('caller')
    local table = sf:variableValue(variableName)
-   assert(type(table) == 'table', variableName .. ' is not a table')
+   assert(type(table) == 'table', variableName .. ' is not a table in the current function')
    for k, v in pairs(table) do
       print(string.format('function %s %s.%s = %s', sf:functionName(), variableName, tostring(k), tostring(v)))
    end
