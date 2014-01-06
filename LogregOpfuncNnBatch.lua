edbd73f703e09dc813d92b84e21c80913c7563bb
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-06 09:53:34 -0500

    new batch gradient using nn package

---
+++ LogregOpfuncNnBatch.lua
@@ -0,0 +1,188 @@
+-- LogregOpfuncNnBatch.lua
+-- logistic regression opfunc using nn package and 
+-- loss and gradient over the entire epoch (called a batch in this code)
+
+require 'argmax'
+require 'assertEq'
+require 'checkGradient'
+require 'keyboard'
+require 'LogregOpfunc'
+require 'makeNextPermutedIndex'
+require 'makeVp'
+require 'memoryUsed'
+require 'nn'
+require 'printAllVariables'
+require 'printTableVariable'
+require 'sgdBottouDriver'
+require 'Timer'
+require 'torch'
+require 'unique'
+require 'validateAttributes'
+
+-- CONSTRUCTOR
+
+local LogregOpfuncNnBatch, parent = torch.class('LogregOpfuncNnBatch', 'LogregOpfunc')
+
+function LogregOpfuncNnBatch:__init(X, y, s, nClasses, lambda)
+   parent.__init(self, X, y, s, nClasses, lambda)
+
+   -- define unregularized model
+   -- make modules within model explicit for testing purposes
+   self.model = nn.Sequential()
+   self.linear = nn.Linear(self.nFeatures, self.nClasses)
+   self.logsoftmax = nn.LogSoftMax()
+   self.model:add(self.linear)
+   self.model:add(self.logsoftmax)  -- be sure to call the constructor!
+   
+   -- couple and flatten parameters and gradient
+   -- the only parameters are in the linear module
+   self.modelTheta, self.modelGradient = self.model:getParameters()
+
+   -- define optimization criterion
+   self.criterion = nn.ClassNLLCriterion()
+
+   -- save initial parameters that were set by the Linear() constructor
+   -- NOTE: self.initialTheta is a function defined in the parent class!
+   self.initialThetaValue = self.modelTheta:clone()
+   
+   -- build table of randomly-permuted sample indices
+   self.randPermutationIndices = torch.randperm(self.nSamples)
+   self.nextRandomIndex = 0
+
+   --printTableVariable('self')
+end
+
+-- PUBLIC METHODS
+
+-- return flat parameters that are a suitable starting point for optimization
+-- RETURNS
+-- theta : Tensor 1D
+function LogregOpfuncNnBatch:runInitialTheta()
+   return self.initialThetaValue
+end
+
+-- return gradient at same parameters as lost call to loss method
+-- ARGS
+-- lossInfo : table from the loss method
+-- RETURNS
+-- gradient : Tensor !D
+function LogregOpfuncNnBatch:runGradient(lossInfo)
+   return lossInfo.gradient
+end
+
+-- return loss at randomly-chosen sample and secret values that can compute gradient quickly
+-- ARGS
+-- theta    : Tensor 1D, parameters
+-- RETURNS
+-- loss     : number at next randomly-selected X, y, s sample
+-- lossInfo : table with secret content
+function LogregOpfuncNnBatch:runLoss(theta)
+   assert(theta ~= nil, 'theta not supplied')
+   assert(theta:nDimension() == 1, 'theta is not a 1D Tensor')
+
+   local loss, gradient = self:_lossGradient(theta)
+   assert(loss)
+   assert(gradient)
+
+   return loss, {gradient = gradient}
+end
+
+-------------------------------------------------------------------------------
+-- PRIVATE METHODS
+-------------------------------------------------------------------------------
+
+-- compute regularized loss and gradient for all samples
+-- ref: torch/cogbits.com/doc/tutorials_supervised/
+function LogregOpfuncNnBatch:_lossGradient(theta)
+   local vp, verboseLevel = makeVp(0, 'LogregOpfuncNnBatch:_lossGradient')
+   local v = verboseLevel > 0
+   if v then
+      vp(1, 'theta', theta, 'self', self)
+      printTableVariable('self')
+   end
+   assert(type(self.lambda) == 'number', 'lambda=' .. tostring(lambda))
+
+   if self.modelTheta ~= theta then
+      self.modelTheta:copy(theta)
+   end
+
+   -- compute loss for all samples, which for one sample was
+   -- local loss = self.criterion(self.model:forward(input), target) * importance
+   local predictions = self.model:forward(self.X)
+   local s2d = torch.Tensor(self.s:storage(), 1, self.nSamples, 1, self.nClasses, 0)
+   local weightedPredictions = torch.cmul(predictions, s2d)
+   local loss = self.criterion(weightedPredictions, self.y)
+   if v and false then
+      vp(2, 'X', self.X)
+      vp(2, 'scores', self.linear.output)
+      vp(2, 'log predictions', predictions)
+      vp(2, 's', self.s, 's2d', s2d)
+      vp(2, 'weightedPredictions', weightedPredictions)
+      vp(2, 'loss', loss)
+   end
+
+   -- regularize loss
+   local weights = self.linear.weight
+   local regularizer = torch.sum(torch.cmul(weights, weights))
+   if v then
+      vp(2, 'weights', weights)
+      vp(2, 'regularizer', regularizer)
+      vp(2, 'self.lambda', self.lambda)
+   end
+   local lossRegularized = loss + self.lambda * regularizer
+
+
+   -- compute gradient into self.modelGradient
+   self.model:zeroGradParameters()
+   --self.model:backward(input, self.criterion:backward(self.model.output, target) * importance)
+   local gradientCriterionUnweighted = self.criterion:backward(self.model.output, self.y) 
+   local gradientCriterionWeighted = torch.cmul(gradientCriterionUnweighted, s2d)
+   self.model:backward(self.X, gradientCriterionWeighted)
+   if v then
+      vp(2, 'gradientCriterionUnweighted', gradientCriterionUnweighted)
+      vp(2, 'gradientCriterionWeighted', gradientCriterionWeighted)
+      vp(2, 'modelGradient', self.modelGradient)
+   end
+
+   -- regularize gradient; retain earlier slower versions as documentation
+   if v then
+      vp(3, 'self.modelGradient', self.modelGradient, 'weights', weights)
+   end
+
+   -- during development, we first coded version 1 and then parallelized it to get version 2
+   -- version 2 is faster
+   local gradientRegularized = nil
+   local gradientRegularizedVersion1 = nil
+   local gradientRegularziedVersion2 = nil
+   local gradientVersion = 2
+   if gradientVersion == 1 or gradientVersion == 'all' then
+      gradientRegularized1 = self.modelGradient:clone()
+      local index = 0
+      for c = 1, self.nClasses do
+         for d = 1, self.nFeatures do
+            index = index + 1
+            gradientRegularized1[index] = 
+            gradientRegularized1[index] + 2 * self.lambda * weights[c][d]
+         end
+      end
+      gradientRegularized = gradientRegularized1
+   elseif gradientVersion == 2 or gradientVersion == 'all' then
+      gradientRegularized2 = self.modelGradient:clone()
+      local weightsGradient = torch.Tensor(gradientRegularized2:storage(), 1, weights:nElement(), 1)
+      local weightsGradient1 = weightsGradient + (weights * 2 * self.lambda)
+      torch.add(weightsGradient, weightsGradient, 2 * self.lambda, weights)
+      gradientRegularized = gradientRegularized2
+   end
+   if gradientVersion == 'all' then
+      assertEq(gradientRegularizedVersion1, gradientRegularizedVersion2, .0001)
+   end
+
+   return lossRegularized, gradientRegularized
+end
+
+
+
+   
+
+   
+   
---
+++ LogregOpfuncNnBatch_test.lua
@@ -0,0 +1,324 @@
+-- LogregOpfuncNnBatch_test.lua
+-- unit test
+
+require 'assertEq'
+require 'finiteDifferenceGradient'
+require 'makeVp'
+require 'LogregOpfuncNnBatch'
+require 'printVariable'
+require 'printAllVariables'
+require 'printTableVariable'
+require 'Random'
+require 'Timer'
+
+torch.manualSeed(123)
+
+local vp = makeVp(2, 'tester')
+
+
+-- UNIT TESTS
+
+-- setup unit test example
+
+
+-- RETURN
+-- example : table containing X, y, s, and other values
+local function makeRandomExample(lambda)
+   local vp = makeVp(0, 'makeRandomExample')
+   lambda = lambda or .01
+
+   local nFeatures = 5
+   local nSamples = 10
+   local nClasses = 4
+
+   local X = torch.rand(nSamples, nFeatures)
+   local y = Random:integer(nSamples, 1, nClasses)
+   local s = Random:uniform(nSamples, .0001, 1)
+
+   local theta = torch.rand((nFeatures + 1) * nClasses)
+   local initialTheta = LogregOpfuncNnBatch(X, y, s, nClasses, lambda):initialTheta()
+   vp(2, 'theta', theta, 'initialTheta', initialTheta)
+   assert(initialTheta:nDimension() == 1)
+   assert(theta:size(1) == initialTheta:size(1))
+
+   local example = {
+      theta = theta,  -- theta where the loss is known
+      expectedLossOnBatch = nil,  -- don't compute it, as must then reimplement the class we are testing
+      lambda = lambda,
+      nFeatures = nFeatures,
+      nSamples = nSamples,
+      nClasses = nClasses,
+      X = X,
+      y = y,
+      s = s}
+
+
+   --printTableVariable('example')
+   return example
+
+end
+   
+
+
+-- ARGS
+-- lambdaValue : optional number, value of lambda in the example
+-- RETURN
+-- of      : instance of LogregOpfunc for the example
+-- example : table containing X, y, s, and other values
+local function makeKnownExample(lambdaValue)
+   local vp = makeVp(0, 'makeKnownExample')
+   lambdaValue = lambdaValue or .01
+   local function makeExample()
+      return {
+         nFeatures = 2,
+         nSamples = 2,
+         nClasses = 3,
+         X = torch.Tensor{{1,2}, {3,4}},
+         y = torch.Tensor{1, 3},
+         s = torch.Tensor{.1, .5},
+         lambda = lambdaValue,
+         theta = torch.Tensor{-1, 2, -3, 4, -5, 6, -7, 8, -9}}  -- biases are last 3 entries
+   end
+
+   local function sumSquaredWeights(example)
+      local function w(index)
+         return example.theta[index]
+      end
+      return w(1) * w(1) + w(2) * w(2) + w(3) * w(3) + w(4) * w(4) + w(5) * w(5) + w(6) * w(6)
+   end
+
+   -- DETERMINE LOSS WE EXPECT FOR SAMPLE 1
+   -- NOTE: The calculations automate a hand-calculation
+   local function expectedLossOnSample1FirstMethod(example)
+      local score11 = -7 - 1 * 1 + 2 * 2  -- prob that sample 1 has value 1
+      local score12 = 8 - 3 * 1 + 4 * 2   -- prob that sample 1 has value 2
+      local score13 = -9 - 5 * 1 + 6 * 2  -- prob that sample 1 has value 3
+      vp(2, string.format('scores for sample: %f %f %f', score11, score12, score13))
+
+      -- normalized probabilies for sample 1
+      local normalizer = math.exp(score11) + math.exp(score12) + math.exp(score13)
+      example.prob11 = math.exp(score11) / normalizer  -- prob that sample 1 has value  1
+      example.prob12 = math.exp(score12) / normalizer
+      example.prob13 = math.exp(score13) / normalizer
+      vp(2, string.format('probs for sample 1: %f %f %f',
+      example.prob11,
+      example.prob12,
+      example.prob13))
+      example.logProb11 = math.log(example.prob11)
+      example.logProb12 = math.log(example.prob12)
+      example.logProb13 = math.log(example.prob13)
+      vp(2, string.format('log probs for sample 1: %f %f %f',
+      example.logProb11,
+      example.logProb12,
+      example.logProb13))
+
+      return 17 * example.s[1] + example.lambda * (example.sumSquaredWeights)
+   end
+
+   local function expectedLossOnSample(example, sampleIndex)
+      local vp = makeVp(0, 'expectedLossOnSample')
+      local w1 = torch.Tensor{example.theta[1], example.theta[2]}
+      local w2 = torch.Tensor{example.theta[3], example.theta[4]}
+      local w3 = torch.Tensor{example.theta[5], example.theta[6]}
+      local b1 = example.theta[7]
+      local b2 = example.theta[8]
+      local b3 = example.theta[9]
+      local score1 = b1 + torch.dot(example.X[sampleIndex], w1)
+      local score2 = b2 + torch.dot(example.X[sampleIndex], w2)
+      local score3 = b3 + torch.dot(example.X[sampleIndex], w3)
+      local normalizer = math.exp(score1) + math.exp(score2) + math.exp(score3)
+      local prob1 = math.exp(score1) / normalizer
+      local prob2 = math.exp(score2) / normalizer
+      local prob3 = math.exp(score3) / normalizer
+      vp(2, string.format('probs for sample %d: %f %f %f', sampleIndex, prob1, prob2, prob3))
+      local logProb = nil
+      local y = example.y[sampleIndex]
+      if  y == 1 then
+         logProb = math.log(prob1)
+      elseif y == 2 then
+         logProb = math.log(prob2)
+      elseif  y == 3 then
+         logProb = math.log(prob3)
+      else
+         error(string.format('bad y (%d)', y))
+      end
+      local lossSample = - logProb  -- unweighted, unregularized
+      return lossSample * example.s[sampleIndex] + example.lambda * sumSquaredWeights(example)
+   end
+
+   local function expectedLossOnSample1(example)
+      local firstMethodLoss = expectedLossOnSample1FirstMethod(example)
+      local secondMethodLoss = expectedLossOnSample(example, 1)
+      assertEq(firstMethodLoss, secondMethodLoss, .0001)
+      return secondMethodLoss
+   end
+
+   local function expectedLossOnSample2(example)
+      return expectedLossOnSample(example, 2)
+   end
+
+   -- main function starts here
+
+   local vp = makeVp(0, 'makeKnownExample')
+
+   -- supply default arg
+   lambdaValue = lambdaValue or .01  -- supply default value
+   
+   local example = makeExample()
+   example.sumSquaredWeights = sumSquaredWeights(example)
+   local expectedLossSample1 = expectedLossOnSample1(example)
+   local expectedLossSample2 = expectedLossOnSample2(example)
+   vp(2, 'expected loss on sample 1', expectedLossSample1,
+         'expected loss on sample 2', expectedLossSample2)
+   example.expectedLossOnBatch = (expectedLossSample1 + expectedLossSample2) / 2
+
+   return example
+end
+
+-------------------------------------------------------------------------------      
+-- TEST  PRIVATE METHODS 
+-------------------------------------------------------------------------------
+
+-- Test the form of the returned values.
+-- The value of the returned values is tested in the tests of methods (below).
+local function _lossGradient_test(example)
+   local vp = makeVp(0, '_lossGradient_test')
+   
+   local of = LogregOpfuncNnBatch(example.X, example.y, example.s, example.nClasses, example.lambda)
+
+   local initialTheta = of:initialTheta()
+   local loss, gradient = of:_lossGradient(initialTheta)
+   assert(type(loss) == 'number')
+   assert(loss >= 0)
+   assert(gradient:nDimension() == 1)
+   assert(gradient:size(1) == initialTheta:size(1))
+end
+
+_lossGradient_test(makeRandomExample())
+_lossGradient_test(makeKnownExample())
+   
+-------------------------------------------------------------------------------
+-- TEST PUBLIC METHODS
+-------------------------------------------------------------------------------
+
+-- test method initialTheta
+local function initialTheta_test(example)
+   local vp = makeVp(0, 'initialTheta_test')
+   local of = LogregOpfuncNnBatch(example.X, example.y, example.s, example.nClasses, example.lambda)
+   local initialTheta = of:initialTheta()
+   vp(2, 'initialTheta', initialTheta)
+
+   assert(initialTheta:nDimension() == 1)
+   assert(initialTheta:size(1) == (example.nClasses) * (example.nFeatures + 1))
+end
+
+initialTheta_test(makeRandomExample())
+initialTheta_test(makeKnownExample())
+
+-------------------------------------------------------------------------------
+
+-- test method loss before testing gradient method
+-- because the test of the gradient method depends on the loss method working
+local function loss_test(lambda, makeExampleFunction)
+   local vp = makeVp(0, 'loss_test')
+   vp(1, 'lambda', lambda, 'makeExampleFunction', makeExampleFunction)
+
+   local example = makeExampleFunction(lambda)
+   local of = LogregOpfuncNnBatch(example.X, example.y, example.s, example.nClasses, lambda)
+
+   -- test on the batch (which is all of the samples)
+   local theta = example.theta 
+   local loss, lossInfo = of:loss(theta)
+
+   -- test the loss, which may or may not have an expected value
+   assert(type(loss) == 'number')
+   assert(loss >= 0)
+   if example.expectedLossOnBatch then
+      vp(2, 'loss', loss, 'expected loss', example.expectedLossOnBatch)
+      assertEq(loss,  example.expectedLossOnBatch, .0001)
+   end
+
+   assert(lossInfo)  -- must be something, but content is a secret
+end
+
+-- test on example with known loss
+loss_test(0, makeKnownExample)   -- first with no regularizer
+loss_test(.01, makeKnownExample)  -- then with regularizer
+
+-- test on example random example (for which we don't precompute the expected loss)
+loss_test(.01, makeRandomExample) 
+
+-------------------------------------------------------------------------------
+
+-- make sure that public method gradient and private method _lossGradient return the same value
+-- do this because the test of the gradient value relies on calling the private method
+local function gradient_test_returns_same(lambda)
+   local vp = makeVp(0, 'gradient_test_returns_same')
+   local example = makeKnownExample(lambda)
+   local of = LogregOpfuncNnBatch(example.X, example.y, example.s, example.nClasses, example.lambda)
+   local theta = of:initialTheta()  -- use random theta
+
+   local nTests = 10
+   for test = 1, nTests do
+      local randomTheta = torch.rand(theta:size(1))
+
+      local loss, lossInfo = of:loss(randomTheta)
+      local gradientFromPublicMethod = of:gradient(lossInfo)
+      vp(2, 'lossInfo', lossInfo, 'gradientFromPublicMethod', gradientFromPublicMethod)
+
+      local loss2, gradientFromPrivateMethod = of:_lossGradient(randomTheta)
+      vp(2, 'gradientFromPrivateMethod', gradientFromPrivateMethod)
+
+      assertEq(gradientFromPublicMethod, gradientFromPrivateMethod, .0001)
+   end
+end
+
+local function gradient_test_gradient_value(lambda)   
+   -- make sure the gradient value returned by the private method _lossGradient is correct
+   -- call the private method so that the index of the sample to use can be specified
+   local vp = makeVp(0, 'gradient_test_gradient_value')
+
+   local function test(makeExampleFunction, lambda)
+      local vp = makeVp(0, 'gradient_test_gradient_value::test')
+      vp(1, 'makeExampleFunction', makeExampleFunction, 'lambda', lambda)
+      local example = makeExampleFunction(lambda)
+      local of = LogregOpfuncNnBatch(example.X, example.y, example.s, example.nClasses, example.lambda)
+      local sampleIndex = 1
+
+      local function f(theta)
+         vp(3,'theta in f', theta)
+         local loss, lossInfo = of:loss(theta)
+         return loss
+      end
+
+      local loss, lossInfo = of:loss(example.theta)
+      local gradient = of:gradient(lossInfo)
+
+      local eps = 1e-5
+      local eps = .0001  -- just for testing
+      local fdGradient = finiteDifferenceGradient(f, example.theta, eps)
+      for i = 1, example.theta:size(1) do
+         vp(2, string.format('grad[%d] %f fdGrad[%d] %f', i, gradient[i], i, fdGradient[i]))
+      end
+      assertEq(gradient, fdGradient, .0001)
+   end
+   
+   test(makeKnownExample, lambda)
+   local nRandomTests = 10
+   for i = 1, nRandomTests do 
+      test(makeRandomExample, lambda)
+   end
+end
+
+-- test gradient in two steps, because the public method gradient(lossInfo) selects a random sample
+local function gradient_test(lambda)
+   gradient_test_returns_same(lambda)
+   gradient_test_gradient_value(lambda)
+end
+
+if true then
+   gradient_test(0)     -- first test without the regularizer
+   gradient_test(.001)  -- now test with the regularizer
+end
+
+print('ok LogregOpfuncNnOne')
