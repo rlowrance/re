d1e6caadc586cbf2b1b772ef7094987ef5c502d7
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-11 20:57:23 -0500

    attempt to repair mass delete (abandoned)

--- LogregOpfuncNnBatch.lua
+++ LogregOpfuncNnBatch.lua
@@ -101,119 +101,13 @@ function LogregOpfuncNnBatch:predict(newX, theta)
    assert(newX:nDimension() == 2, 'newX is not a 2D Tensor')
    assert(newX:size(2) == self.X:size(2), 'newX has wrong number of features')
 
-   -- avoid construction of a new LogregOpfuncNnBatch by replacing and restoring field X
-   local currentX = self.X
-   self.X = newX
-   local _, _, logProbabilities = self:_lossGradientPredictions(theta)
-   local probabilities = torch.exp(logProbabilities)
-   self.X = currentX  -- restore field X
-   return probabilities
+   -- construct new LogregOpfuncNnBatch for use in predictions
+   local nSamples = newX:size(1)
+   local newY = torch.Tensor(nSamples):fill(1)  -- set class arbitrarily to 1
+   local newS = torch.Tensor(nSamples):fill(1)  -- saliences are all one
+   local newOpfunc = LogRegOpfuncNnBatch(newX, newY, newS, self.nClasses, self.lambda)
+   local loss, gradient, logProbabilities = newOpfunc:_lossGradientPredictions(theta)
+   assert(logProbabilities:size(1) == nSamples)
+   assert(logProbabilities:size(2) == self.nClasses)
+   return torch.exp(logProbabilities)
 end
-   
-
-
--------------------------------------------------------------------------------
--- PRIVATE METHODS
--------------------------------------------------------------------------------
-
--- compute regularized loss and gradient and predictions for all samples
--- ref: torch/cogbits.com/doc/tutorials_supervised/
--- ARGS
--- theta          : 1D Tensor of parameters
--- RETURNS
--- loss           : number
--- gradient       ; 1D Tensor with same shape as theta
--- logProbabilies : 1D Tensor of size self.nClasses
-function LogregOpfuncNnBatch:_lossGradientPredictions(theta)
-   local vp, verboseLevel = makeVp(0, 'LogregOpfuncNnBatch:_lossGradient')
-   local v = verboseLevel > 0
-   if v then
-      vp(1, 'theta', theta, 'self', self)
-      printTableVariable('self')
-   end
-   assert(type(self.lambda) == 'number', 'lambda=' .. tostring(lambda))
-
-   if self.modelTheta ~= theta then
-      self.modelTheta:copy(theta)
-   end
-
-   -- compute loss for all samples, which for one sample was
-   -- local loss = self.criterion(self.model:forward(input), target) * importance
-   local predictions = self.model:forward(self.X)
-   local s2d = torch.Tensor(self.s:storage(), 1, self.nSamples, 1, self.nClasses, 0)
-   local weightedPredictions = torch.cmul(predictions, s2d)
-   local loss = self.criterion(weightedPredictions, self.y)
-   if v and false then
-      vp(2, 'X', self.X)
-      vp(2, 'scores', self.linear.output)
-      vp(2, 'log predictions', predictions)
-      vp(2, 's', self.s, 's2d', s2d)
-      vp(2, 'weightedPredictions', weightedPredictions)
-      vp(2, 'loss', loss)
-   end
-
-   -- regularize loss
-   local weights = self.linear.weight
-   local regularizer = torch.sum(torch.cmul(weights, weights))
-   if v then
-      vp(2, 'weights', weights)
-      vp(2, 'regularizer', regularizer)
-      vp(2, 'self.lambda', self.lambda)
-   end
-   local lossRegularized = loss + self.lambda * regularizer
-
-
-   -- compute gradient into self.modelGradient
-   self.model:zeroGradParameters()
-   --self.model:backward(input, self.criterion:backward(self.model.output, target) * importance)
-   local gradientCriterionUnweighted = self.criterion:backward(self.model.output, self.y) 
-   local gradientCriterionWeighted = torch.cmul(gradientCriterionUnweighted, s2d)
-   self.model:backward(self.X, gradientCriterionWeighted)
-   if v then
-      vp(2, 'gradientCriterionUnweighted', gradientCriterionUnweighted)
-      vp(2, 'gradientCriterionWeighted', gradientCriterionWeighted)
-      vp(2, 'modelGradient', self.modelGradient)
-   end
-
-   -- regularize gradient; retain earlier slower versions as documentation
-   if v then
-      vp(3, 'self.modelGradient', self.modelGradient, 'weights', weights)
-   end
-
-   -- during development, we first coded version 1 and then parallelized it to get version 2
-   -- version 2 is faster
-   local gradientRegularized = nil
-   local gradientRegularizedVersion1 = nil
-   local gradientRegularziedVersion2 = nil
-   local gradientVersion = 2
-   if gradientVersion == 1 or gradientVersion == 'all' then
-      gradientRegularized1 = self.modelGradient:clone()
-      local index = 0
-      for c = 1, self.nClasses do
-         for d = 1, self.nFeatures do
-            index = index + 1
-            gradientRegularized1[index] = 
-            gradientRegularized1[index] + 2 * self.lambda * weights[c][d]
-         end
-      end
-      gradientRegularized = gradientRegularized1
-   elseif gradientVersion == 2 or gradientVersion == 'all' then
-      gradientRegularized2 = self.modelGradient:clone()
-      local weightsGradient = torch.Tensor(gradientRegularized2:storage(), 1, weights:nElement(), 1)
-      local weightsGradient1 = weightsGradient + (weights * 2 * self.lambda)
-      torch.add(weightsGradient, weightsGradient, 2 * self.lambda, weights)
-      gradientRegularized = gradientRegularized2
-   end
-   if gradientVersion == 'all' then
-      assertEq(gradientRegularizedVersion1, gradientRegularizedVersion2, .0001)
-   end
-
-   return lossRegularized, gradientRegularized, predictions
-end
-
-
-
-   
-
-   
-   
