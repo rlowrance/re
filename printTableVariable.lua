b82c9e7e406ee9eb7e8c3aeb1eb8ff88d2410617
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-17 21:41:50 -0500

    further arg validation

--- printTableVariable.lua
+++ printTableVariable.lua
@@ -5,6 +5,7 @@ require 'StackFrame'
 
 function printTableVariable(variableName)
    assert(variableName ~= nil, 'missing variableName argument')
+   assert(type(variableName) == 'string', 'must supply a variable name in a string')
    local sf = StackFrame('caller')
    local table = sf:variableValue(variableName)
    assert(type(table) == 'table', variableName .. ' is not a table in the current function')
