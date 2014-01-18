f94882869b187ae89ad4d5ca4c03f247269b0a57
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-17 21:42:20 -0500

    broken unit test

--- ModelLogregNnBatch_test.lua
+++ ModelLogregNnBatch_test.lua
@@ -20,40 +20,62 @@ torch.manualSeed(123)
 -- RETURNS
 -- X, y, s, nCLasses : synthetic data
 -- actualTheta       : actual parameters used to generate y from X
-local function makeTrainingData(useSaliences, size)
-   local nSamples = 60
+local function makeTrainingData(bad)
+   local vp, verboseLevel = makeVp(2, 'makeTrainingData')
+   assert(bad == nil)
+   local nSamples = 60  
    local nFeatures = 8
    local nClasses = 14
-   if size == 'small' then
+   
+   if true then
       nSamples = 5
-      nFeatures = 2
-      nClasses = 3
-      print('using small sample size')
+      nFeatures = 3
+      nClasses = 2
    end
 
    local lambda = 0       -- arbitrary value needed for APIs
 
    -- randomly generate data
    local X = torch.rand(nSamples, nFeatures)
-   local y = Random():integer(nSamples, 1, nClasses)  -- class numbers are fixed up below
-   local s 
-   if useSaliences then
-      s = torch.abs(torch.rand(nSamples)) -- saliences must be non-negative
-   else
-      s = torch.Tensor(nSamples):zero()
-   end
+   local y = Random():integer(nSamples, 1, nClasses)  -- class numbers not derived from weights
+   local s = torch.abs(torch.rand(nSamples)) -- saliences must be non-negative
 
    -- get random weights from the corresponding optimization function
    local opfunc = OpfuncLogregNnBatch(X, y, s, nClasses, 0)
-   local actualTheta = opfunc:initialTheta()
+   local initialTheta = opfunc:initialTheta()  -- random weights
+   vp(2, 'initialTheta', initialTheta)
+   --local actualTheta = initialTheta:zero()
 
-   return X, y, s, nClasses, actualTheta
+   -- derive y's from initialTheta weights
+   local model = ModelLogregNnBatch(X, y, s, nClasses, lambda)
+   local probabilities, predictInfo = model:predict(X, initialTheta)
+   local predictedY = predictInfo.mostLikelyClasses
+   vp(2, 'X', X)
+   vp(2, 'probabilities', probabilities, 'predicted y', predictedY)
+   
+   -- maybe print distribution of predictedY's, which should be random
+   if verboseLevel > 1 then
+      local nOccurrences = torch.Tensor(nClasses):zero()
+      for i = 1, nSamples do
+         local index = predictedY[i]
+         nOccurrences[index] = nOccurrences[index] + 1
+      end
+      for classNumber = 1, nClasses do
+         vp(2, string.format('class number %d frequency %f',
+                             classNumber, nOccurrences[classNumber] / nSamples))
+      end
+   end
+   stop()
+
+   
+   return X, predictedY, s, nClasses, initialTheta
 end
 
 -- return model and X and y values for it
-local function makeModel(useSaliences, lambda)
-   local X, y, s, nClasses, actualTheta = makeTrainingData(useSaliences)
+local function makeModel()
+   local X, y, s, nClasses, initialTheta = makeTrainingData()
 
+   local lambda = 0.001
    local model = ModelLogregNnBatch(X, y, s, nClasses, lambda)
    return model, X, y
 end
@@ -67,9 +89,9 @@ end
 -------------------------------------------------------------------------------
 
 local function testConstruction()
-   local useSaliences = true
+   local zeroSaliences = true
    local lambda = 0.0001
-   local model = makeModel(useSaliences, lambda)
+   local model = makeModel()
    assert(model ~= nil)
 end
 
@@ -79,9 +101,38 @@ testConstruction()
 -- test method fit
 -------------------------------------------------------------------------------
 
-local function fitModel(model, toleranceLoss)
-   local vp = makeVp(1, 'fitModel')
+local function fitModelBottouEpoch(model, toleranceLoss, printLoss)
+   local vp = makeVp(1, 'fitModelBottouEpoch')
    vp(1, 'model', model)
+   assert(model)
+   assert(toleranceLoss)
+   assert(printLoss ~= nil)
+
+   local function nextStepSizes(currentStepSize)
+      return {currentStepSize, 0.5 * currentStepSize, 1.5 * currentStepSize}
+   end
+
+   local fittingOptions = {
+      method = 'bottouEpoch',
+      printLoss = printLoss,
+      initialStepSize = 1,
+      nEpochsBeforeAdjustingStepSize = 1,
+      nEpochsToAdjustStepSize = 2,
+      nextStepSizes = nextStepSizes,
+      nSteps = 2,    
+      maxEpochs = 1000,
+      toleranceLoss = toleranceLoss,
+      toleranceTheta = .01}
+   local optimalTheta, fitInfo = model:fit(fittingOptions)
+   return optimalTheta, fitInfo
+end
+
+local function fitModelGradientDescent(model, toleranceLoss, printLoss)
+   local vp = makeVp(1, 'fitModelGradientDescent')
+   vp(1, 'model', model)
+   assert(model)
+   assert(toleranceLoss)
+   assert(printLoss ~= nil)
 
    toleranceLoss = toleranceLoss or .001
 
@@ -90,37 +141,67 @@ local function fitModel(model, toleranceLoss)
    end
 
    local fittingOptions = {
-      method = 'bottouEpoch',
-      printLoss = true,
+      method = 'gradientDescent',
+      printLoss = printLoss,
       initialStepSize = 1,
-      nEpochsBeforeAdjustingStepSize = 1,
-      nEpochsToAdjustStepSize = 2,
-      nextStepSizes = nextStepSizes,
-      nSteps = 2,    
-      maxEpochs = 100,
+      maxEpochs = 1000,
       toleranceLoss = toleranceLoss,
       toleranceTheta = .01}
-   local fitInfo = model:fit(fittingOptions)
-   return fitInfo
+   local optimalTheta, fitInfo = model:fit(fittingOptions)
+   return optimalTheta, fitInfo
 end
 
 local function testFitInfo(fitInfo)
-   local vp = makeVp(2, 'testFitInfo')
+   local vp = makeVp(0, 'testFitInfo')
    assert(type(fitInfo) == 'table')
    vp(2, 'convergedReason', fitInfo.convergedReason)
    vp(2, 'finalLoss', fitInfo.finalLoss)
    vp(2, 'nEpochsUntilConvergence', fitInfo.nEpochsUntilConvergence)
    vp(2, 'optimalTheta size', fitInfo.optimalTheta:size())
-   -- MAYBE: Figure out some real tests
+   assert(fitInfo.convergedReason == 'toleranceLoss')
+end
+
+local function determineErrorRate(actual, predicted)
+   local vp = makeVp(1, 'determineErrorRate')
+   vp(1, 'actual', actual, 'predicted', predicted)
+   assert(actual:nDimension() == 1)
+   assert(predicted:nDimension() == 1)
+   local nSamples = actual:size(1)
+   assert(nSamples == predicted:size(1))
+   local nErrors = 0
+   for i = 1, nSamples do
+      if actual[i] ~= predicted[i] then
+         nErrors = nErrors + 1
+      end
+   end
+   return nErrors / nSamples
+end
+
+local function testOptimalTheta(optimalTheta, model, trainingX, trainingY)
+   local vp = makeVp(1, 'testOptimalTheta')
+   vp(1, 'model', model)
+   assert(optimalTheta:nDimension() == 1)
+
+   -- test supposed optimalTheta by checking the predictions
+   local probabilities, predictInfo = model:predict(trainingX, optimalTheta)
+   local errorRate = determineErrorRate(trainingY, predictInfo.mostLikelyClasses)
+   vp(1, 'errorRate', errorRate)
+   assert(errorRate == 0) -- for now
+end
+
+local function testFitDriver(howToFitModel, toleranceLoss, printLoss)
+   local vp = makeVp(2, 'testFit')
+   local model, X, y = makeModel()
+   local optimalTheta, fitInfo = howToFitModel(model, toleranceLoss, printLoss)
+   testOptimalTheta(optimalTheta, model, X, y)
+   testFitInfo(fitInfo)
 end
 
 local function testFit()
-   local vp = makeVp(2, 'testFit')
-   local useSaliences = true
-   local lambda = 0.0001
-   local model = makeModel(useSaliences, lambda)
-   local fitInfo = fitModel(model)
-   testFitInfo(fitInfo)
+   local toleranceLoss = 0.001
+   local printLoss = false
+   testFitDriver(fitModelBottouEpoch, toleranceLoss, printLoss)
+   testFitDriver(fitModelGradientDescent, toleranceLoss, printLoss)
 end
 
 testFit()
@@ -129,15 +210,26 @@ testFit()
 -- test method predict
 -------------------------------------------------------------------------------
 
-local function testPredictValues(useSaliences, lambda, toleranceLoss)
+local function testPredictValues(zeroSaliences, lambda, toleranceLoss)
    local vp = makeVp(1, 'testPredictValues')
-   local model, actualX, actualY = makeModel(useSaliences, lambda)
-   local fitInfo = fitModel(model, toleranceLoss)
+
+   assert(zeroSaliences ~= nil)
+   assert(lambda ~= nil)
+   assert(toleranceLoss ~= nil)
+   
+   -- make the model
+   local model, actualX, actualY = makeModel(zeroSaliences, lambda)
+   
+   -- fit the model
+   local toleranceLoss = 0.00001
+   local printLoss = true
+   local optimalTheta, fitInfo = fitModelBottouEpoch(model, toleranceLoss, printLoss)
+   -- check that we actually converged
+   assert(fitInfo.convergedReason ~= 'maxEpochs', fitInfo.convergedReason)
 
    -- predict each X used as training data
-   local predictedY, predictInfo = model:predict(actualX, fitInfo.optimalTheta)
+   local predictedY, predictInfo = model:predict(actualX, optimalTheta)
    vp(2, 'predictedY', predictedY)
-   printTableVariable('model')
    assert(predictedY:nDimension() == 2)
    assert(predictedY:size(1) == model.nSamples)
    assert(predictedY:size(2) == model.nClasses)
@@ -152,16 +244,21 @@ local function testPredictValues(useSaliences, lambda, toleranceLoss)
    for i = 1, model.nSamples do
       local actual = actualY[i]
       local predicted = predictedClasses[i]
-      vp(1, string.format('i %d actualY %f predictedY %f', i, actual, predicted))
+      vp(2, string.format('i %d actualY %f predictedY %f', i, actual, predicted))
       if actual ~= predicted then
          nErrors = nErrors + 1
       end
    end
-   vp(1, 'fraction errors', nErrors / model.nSamples)
-   stop()
+   local fractionErrors = nErrors / model.nSamples
+   vp(1, 'fraction errors', fractionErrors)
+   assert(fractionErrors <= 1.0)  -- it seems that the training data are just too noisy
 end
 
-testPredictValues(false, 0.0001, .00001)  -- no saliences, regularized
-testPredictValues(true, 0.0001)  -- use saliences, regularized
+local zeroSaliences = true
+local lambda = 0.0001
+local toleranceLoss = 1e-5
+testPredictValues(not zeroSaliences, lambda, toleranceLoss)
+stop()
+testPredictValues(zeroSaliences, lambda, toleranceLoss)
 
 print('ok ModelLogregNnBatch')
