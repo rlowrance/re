e20513708495caefd53b9b9c666fe826a496952e
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-21 06:57:23 -0500

    make printing optional

--- printTableValue_test.lua
+++ printTableValue_test.lua
@@ -3,12 +3,18 @@
 
 require 'printTableValue'
 
-t1 = {one = 1, abc = 'abc'}
-printTableValue('t1', t1)
-printTableValue(t1)
+local actuallyPrint = false
 
-t2 = {def = 'def', nested = t1}
-printTableValue('t2', t2)
-printTableValue(t2)
+if actuallyPrint then
+   t1 = {one = 1, abc = 'abc'}
+   printTableValue('t1', t1)
+   printTableValue(t1)
+
+   t2 = {def = 'def', nested = t1}
+   printTableValue('t2', t2)
+   printTableValue(t2)
+else
+   print('printing disabled')
+end
 
 print('ok printTableValue')
