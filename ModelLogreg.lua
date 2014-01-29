6933df52d62ffc4f14d94094fd6fc8a2b12676a0
Roy Lowrance <roy.lowrance@gmail.com>
2014-01-29 10:27:39 -0500

    arg s now stored correctly

--- ModelLogreg.lua
+++ ModelLogreg.lua
@@ -1,6 +1,13 @@
 -- ModelLogreg.lua
 -- weighted logistic regression
 
+if false then
+   m = ModelLogregCONCRETE_CLASS(X, y, s, nClasses) 
+   optimalTheta, fitInfo = m:fit(fittingOptions)  -- fittingOptions includes any regularizer
+   predictions, predictionInfo = m:predict(newX, optimalTheta)
+end
+
+require 'isTensor'
 require 'Model'
 require 'torch'
 
@@ -15,30 +22,31 @@ local ModelLogreg, parent = torch.class('ModelLogreg', 'Model')
 -- y        : 1D Tensor of integers >= 1, class numbers
 -- s        : 1D Tensor of saliences (weights)
 -- nClasses : number of classes (max value in y)
--- lambda   : number L2 regularizer importance
-function ModelLogreg:__init(X, y, s, nClasses, lambda)
+function ModelLogreg:__init(X, y, s, nClasses, errorIfSupplied)
+   assert(errorIfSupplied == nil, 'lambda is not supplied as part of call to method fit')
+
    parent.__init(self)
+
+   assert(isTensor(X), 'X is not a torch.Tensor')
    assert(X:nDimension() == 2, 'X is not a 2D Tensor')
    self.nSamples = X:size(1)
    self.nFeatures = X:size(2)
 
+   assert(isTensor(y), 'y is not a torch.Tensor')
    assert(y:nDimension() == 1, 'y is not a 1D Tensor')
    assert(y:size(1) == self.nSamples, 'y has incorrect size')
 
+   assert(isTensor(s), 's is not a torch.Tensor')
    assert(s:nDimension() == 1, 's is not a 1D Tensor')
    assert(s:size(1) == self.nSamples, 's has incorrect size')
 
    assert(type(nClasses) == 'number', 'nClasses is not a number')
    assert(nClasses >= 2, 'number of classes is not at least 2')
 
-   assert(type(lambda) == 'number', 'lambda is not a number')
-   assert(lambda >= 0, 'lambda is not at least zero')
-
    self.X = X
    self.y = y
-   self.x = s
+   self.s = s
    self.nClasses = nClasses
-   self.lambda = lambda
 end
 
 
