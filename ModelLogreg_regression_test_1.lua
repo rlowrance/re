bd83bfcb4a84ebb1d218225188f2a6304394cad7
Roy Lowrance <roy.lowrance@gmail.com>
2014-02-02 13:36:25 -0500

    port to use ModelLogreg new API

--- ModelLogreg_regression_test_1.lua
+++ ModelLogreg_regression_test_1.lua
@@ -12,7 +12,7 @@
 require 'ConfusionMatrix'
 require 'ifelse'
 require 'makeVp'
-require 'ModelLogregNnbatch'
+require 'ModelLogreg'
 require 'printTableVariable'
 require 'Random'
 
@@ -42,8 +42,7 @@ local function makeData()
 end
 
 local function makeModel(data)
-   local lambda = 0.001
-   local model = ModelLogregNnbatch(data.X, data.y, data.s, data.nClasses, lambda)
+   local model = ModelLogreg(data.X, data.y, data.s, data.nClasses)
    return model
 end
 
@@ -67,17 +66,22 @@ local function fitModelBottouEpoch(model, toleranceLoss, printLoss, initialStepS
    end
 
    local fittingOptions = {
-      method = 'bottouEpoch',
-      callBackEndOfEpoch = callback,
-      printLoss = printLoss,
-      initialStepSize = initialStepSize,
-      nEpochsBeforeAdjustingStepSize = 20,
-      nEpochsToAdjustStepSize = 2,
-      nextStepSizes = nextStepSizes,
-      maxEpochs = 900,  -- at epoch 999, the loss increases, probably because of a rounding problem
-      maxEpochs = 2000,
-      toleranceLoss = toleranceLoss,
-      toleranceTheta = nil}
+      method = 'bottou',
+      sampling = 'epoch',
+      methodOptions = {
+         printLoss = printLoss,
+         initialStepSize = initialStepSize,
+         nEpochsBeforeAdjustingStepSize = 20,
+         nEpochsToAdjustStepSize = 2,
+         nextStepSizes = nextStepSizes
+      },
+      samplingOptions = {},
+      convergence = {
+         maxEpochs = 900,
+         toleranceLoss = toleranceLoss
+      },
+      regularizer = {L2 = 0.001}
+   }
    local optimalTheta, fitInfo = model:fit(fittingOptions)
 
    print(string.format('there are %d stepSizes', #stepSizes))
@@ -194,6 +198,7 @@ local function testEverything()
    end
    
    local algos = {'bottouEpoch', 'gradientDescent'}
+   local algos = {'bottouEpoch'}  -- for now, ModelLogreg implements just this one
    
    local testResults = {}
    for _, algo in ipairs(algos) do
@@ -213,109 +218,3 @@ end
 testEverything()
 
 print('ok ModelLogreg regression test 1')
-error('stopping')
--- OLD CODE BELOW ME
-
-local function makeY(X)
-   local x1 = X[1]
-   local x2 = X[2]
-
-   local y = ifelse(x1 < 0.5,
-                    ifelse(x2 < 0.5, 1, 2),
-                    ifelse(x2 < 0.5, 3, 4))
-   return y
-end
-
-local function makeData(equalSaliences)
-   local nSamples = 100
-   local nFeatures = 2
-   local X = torch.rand(nSamples, nFeatures)
-
-   local nClasses = 4
-   local y = torch.Tensor(nSamples)
-   for i = 1, nSamples do
-      y[i] = makeY(X[i])
-   end
-   
-   local s = torch.Tensor(nSamples):fill(1)
-   if not equalSaliences then
-      error('not yet implemented')
-   end
-
-   return nClasses, X, y, s
-end
-
-local function trainModel(nClasses, X, y, s)
-   local vp = makeVp(2, 'trainModel')
-   local lambda = 0.001
-   
-   local model = ModelLogregNnBatch(X, y, s, nClasses, lambda)
-   
-   local function nextStepSizes(currentStepSize)
-      return {currentStepSize, .5 * currentStepSize, 1.5 * currentStepSize}
-   end
-
-   local fittingOptions = { -- for method bottouEpoch
-      initialStepSize = 1,
-      method = 'bottouEpoch',
-      nEpochsBeforeAdjustingStepSize = 3,
-      nEpochsToAdjustStepSize = 1,
-      nextStepSizes = nextStepSizes,
-      maxEpochs = 100,
-      toleranceLoss = 1e-6,
-      printLoss = true
-   }
-
-   local fittingOptions = { -- for method gradientDescent
-      initialStepSize = 1,
-      method = 'gradientDescent',
-      maxEpochs = 1000,
-      toleranceLoss = 1e-4,
-      printLoss = true
-   }
-
-   local optimalTheta, fitInfo = model:fit(fittingOptions)
-   assert(optimalTheta)
-   printTableVariable('fitInfo')
-   assert(fitInfo.convergedReason == 'toleranceLoss')
-   
-   return optimalTheta, model
-end
-   
-local function predict(optimalTheta, model, newX)
-   local probabilities, predictInfo = model:predict(newX, optimalTheta)
-   return predictInfo.mostLikelyClasses
-end
-
-local function calculateErrorRate(actual, predicted, printErrors)
-   local nSamples = actual:size(1)
-   assert(predicted:size(1) == nSamples)
-
-   local nErrors = 0
-   for i = 1, nSamples do
-      if actual[i] ~= predicted[i] then
-         nErrors = nErrors + 1
-         if printErrors then
-            print(string.format('sample %d actual %d predicted %d', i, actual[i], predicted[i]))
-         end
-      end
-   end
-   return nErrors / nSamples
-end
-
-local function test(equalSaliences)
-   local nClasses, X, y, s = makeData(equalSaliences)
-   local optimalTheta, model = trainModel(nClasses, X, y, s)
-   assert(optimalTheta)
-   assert(model)
-   local predictions = predict(optimalTheta, model, X)  -- predict the training samples
-   
-   local errorRate = calculateErrorRate(y, predictions)
-   print(string.format('use saliences %s errorRate %f', tostring(equalSaliences), errorRate))
-   assert(errorRate < .05)
-end
-
-test(true)   -- all saliences equal
-test(false)  -- some training samples are more important than others
-
-print('ok ModelLogreg_regression_test1')
