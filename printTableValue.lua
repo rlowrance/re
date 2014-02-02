0ad5d57b01f0d55d0020c74e61b0a488f583c6e7
Roy Lowrance <roy.lowrance@gmail.com>
2014-02-01 20:30:38 -0500

    print just shape for Tensors and Storage

--- printTableValue.lua
+++ printTableValue.lua
@@ -2,16 +2,81 @@
 -- print value of a table
 
 
+-- ARGS
+-- variableName : optional string, name of variable holding a table value
+-- tableValue   : table
 function printTableValue(variableName, tableValue)
-   local function printTableNameValue(variableName, tableValue)
-      for k, v in pairs(tableValue) do
-         print(string.format('%s.%s = %s', variableName, tostring(k), tostring(v)))
-         if type(v) == 'table' then
-            printTableValue(variableName .. '.' .. tostring(k), v)
+
+   local function printField(tableName, fieldName, fieldValue)
+      assert(fieldValue)
+      print(string.format('%s.%s = %s', tableName, fieldName, tostring(fieldValue)))
+   end
+
+   local function printStorage(tableName, fieldName, value, torchTypename)
+      printField(tableName, fieldName, torchTypename .. ' size ' .. value:size())
+   end
+
+   local function printTensor(tableName, fieldName, value, torchTypename)
+      local sizes = value:size()
+      local shape = torchTypename .. ' size '
+      for i = 1, #sizes do
+         if i == 1 then
+            shape = shape .. ' ' .. tostring(sizes[i])
+         else
+            shape = shape .. ' x ' .. tostring(sizes[i])
+         end
+      end
+      printField(tableName, fieldName, shape)
+   end
+
+   local function printUserdata(tableName, fieldName, value)
+      local torchTypename = torch.typename(value)
+      if torchTypename ~= nil and string.sub(torchTypename, -6) == 'Tensor' then
+         -- found a Tensor
+         printTensor(tableName, fieldName, value, torchTypename)
+      elseif torchTypename ~= nil and string.sub(torchTypename, -7) == 'Storage' then
+         -- found a Storage
+         printStorage(tableName, fieldName, value, torchTypename)
+      else
+         -- found either non-torch userdata or non-Tensor torch data
+         printField(tableName, fieldName, value)
+      end
+   end
+
+   local function printTableNameValue(tableName, tableValue)
+      -- sort the keys
+      local keysSorted = {}
+      for key in pairs(tableValue) do
+         table.insert(keysSorted, {tostring(key), key})
+      end
+
+      local function compare(a, b)
+         return a[1] < b[1]  -- sort on string version of key
+      end
+
+      table.sort(keysSorted, compare)
+
+      -- print keys in their sorted order
+      for _, key in ipairs(keysSorted) do
+         -- parse the key, which is a sequence of length 2
+         local fieldName = key[1]
+         local value = tableValue[key[2]]
+
+         local valueType = type(value)
+         --print('fieldName', fieldName, 'value', value, 'valueType', valueType)
+
+         if valueType == 'userdata' then
+            printUserdata(tableName, fieldName, value)
+         elseif valueType == 'table' then
+            printTableValue(tableName .. '.' .. fieldName, value)
+         else
+            printField(tableName, fieldName, value)
          end
       end
    end
 
+   -- handle the first arg, which is optional so that f(b) --> f('', b)
+   -- also type check arguments
    if type(variableName) == 'string' and type(tableValue) == 'table' then
       printTableNameValue(variableName, tableValue)
    elseif type(variableName) == 'table' and type(value) == 'nil' then
--- printTableValue_test.lua
+++ printTableValue_test.lua
@@ -2,8 +2,9 @@
 -- unit test
 
 require 'printTableValue'
+require 'torch'
 
-local actuallyPrint = false
+local actuallyPrint = true
 
 if actuallyPrint then
    t1 = {one = 1, abc = 'abc'}
@@ -13,6 +14,14 @@ if actuallyPrint then
    t2 = {def = 'def', nested = t1}
    printTableValue('t2', t2)
    printTableValue(t2)
+
+   t3 = {}
+   t3.one = 1
+   t3.f = function() end
+   t3.tensor1D = torch.Tensor(30)
+   t3.tensor2D = torch.Tensor(3, 5)
+   t3.storage = torch.Storage(10)
+   printTableValue('t3', t3)
 else
    print('printing disabled')
 end
