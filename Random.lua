bb37c71e5c28a2a18c28b403b4ad87a4ae023383
Roy Lowrance <roy.lowrance@gmail.com>
2014-02-08 09:46:56 -0500

    add method geometric

--- Random.lua
+++ Random.lua
@@ -54,5 +54,43 @@ function Random:integer(nSamples, lowest, highest)
    return result
 end
 
+-- return 1D Tensor of geometrically selected numbers in specified range
+-- ARGS
+-- nSamples : size of result
+-- lowest   : number, each number is in the range [lowest, highest]
+-- highest  : number
+-- NOTE: The selection procedure suggested by Yann is to define
+-- t(x) = a b^x and then to sample uniformly in [0,1] after
+-- selection a and b s.t. a b^0 = lowest and a b^1 = highest.
+-- MATH
+-- Since lowest = a b^0 = a and highest = a b^1 = a b, we have
+-- a = lowest
+-- b = highest /a = highest / lowest
+-- Then t(x) = a b ^x = lowest (highest / lowest) ^ x
+function Random:geometric(nSamples, lowest, highest)
+   local vp = makeVp(0, 'geometric')
+   
+   assert(nSamples > 0)
+   assert(lowest <= highest)
+   assert(lowest ~= 0)
 
+   local a = lowest
+   local b = highest / lowest
+   vp(2, 'a', a, 'b', b)
 
+   local function t(x) 
+      return a * (b ^ x)
+   end
+   assert(t(0) == lowest)
+   assert(t(1) == highest)
+   
+
+   local u = self:uniform(nSamples, 0, 1)
+   local result = torch.Tensor(nSamples)
+   for i = 1, nSamples do
+      result[i] = t(u[i])
+      vp(2, 'i', i, 'u', u[i], 'result', result[i])
+   end
+   vp(1, 'result', result)
+   return result
+end
--- Random_test.lua
+++ Random_test.lua
@@ -4,6 +4,8 @@
 require 'makeVp'
 require 'Random'
 
+local vp = makeVp(0, 'tester')
+
 local nSamples = 100
 local lowest = 1
 local highest = 10
@@ -25,5 +27,16 @@ local r1 = Random():integer(nSamples, lowest, highest)
 assert(r1:size(1) == nSamples)
 r1:apply(within)
 r1:apply(isInteger)
+
+--local nSamples = 10
+local r1 = Random():geometric(nSamples, lowest, highest)
+vp(1, 'geometric r1', r1)
+assert(r1:size(1) == nSamples)
+for i = 1, nSamples do
+   local value = r1[i]
+   assert(lowest <= value, value)
+   assert(value <= highest, value)
+end
+
 
 print('ok Random')
