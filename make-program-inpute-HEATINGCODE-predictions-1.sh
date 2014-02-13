0f8118bed7af60ba352b3e67a5ced2e8a602f029
Roy Lowrance <roy.lowrance@gmail.com>
2014-02-13 16:44:50 -0500

    new: run program-imput instance

---
+++ make-program-inpute-HEATINGCODE-predictions-1.sh
@@ -0,0 +1,10 @@
+CACHE=../data/v6/output/program_impute-cache.serialized
+#rm $CACHE
+luajit program_impute.lua \
+    --cache \
+    --train hasHEATING.CODE isTrain \
+    --test hasHEATING.CODE isTest \
+    --target HEATING.CODE \
+    --hpset 1 \
+    --output ../data/v6/output/program_impute_HEATINGCODE_predictions_1.sh
+
