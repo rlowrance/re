0717d13ca21f7c0800ce06c846d421a1cdb68554
Roy Lowrance <roy.lowrance@gmail.com>
2014-02-27 11:07:17 -0500

    improve source code layout

--- Timer.lua
+++ Timer.lua
@@ -39,22 +39,6 @@ end
 -- PUBLIC METHODS
 -------------------------------------------------------------------------------
 
-function Timer:stop()
-   self.timer:stop()
-end
-
-function Timer:resume()
-   self.timer:resume()
-end
-
-function Timer:reset()
-   self.timer:reset()
-end
-
-function Timer:wallclock()
-   return self.timer:time().real
-end
-
 function Timer:cpu()
    local t = self.timer:time()
    return t.user + t.sys
@@ -65,10 +49,26 @@ function Timer:cpuWallclock()
    return t.user + t.sys, t.real
 end
 
-function Timer:user()
-   return self.timer:time().user
+function Timer:reset()
+   self.timer:reset()
+end
+
+function Timer:resume()
+   self.timer:resume()
 end
 
 function Timer:system()
    return self.timer:time().sys
+end
+
+function Timer:stop()
+   self.timer:stop()
+end
+
+function Timer:user()
+   return self.timer:time().user
+end
+
+function Timer:wallclock()
+   return self.timer:time().real
 end
