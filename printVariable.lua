679f508df73b1d11d3acb2f3454e5b94accece07
Roy Lowrance <roy.lowrance@gmail.com>
2014-03-01 19:13:56 -0500

    correctly handle when function name is missing

--- printVariable.lua
+++ printVariable.lua
@@ -1,10 +1,17 @@
 -- printVariable.lua
 -- print value of variable in current function
 
+require 'ifelse'
 require 'StackFrame'
 
 function printVariable(variableName)
    assert(variableName ~= nil, 'missing variableName argument')
    local sf = StackFrame('caller')
-   print(string.format('function %s %s = %s', sf:functionName(), variableName, sf:variableValue(variableName)))
+   local functionName = sf:functionName()
+   local value = tostring(sf:variableValue(variableName))
+   if functionName == nil then
+      print(string.format('<unknown function> %s = %s', variableName, value))
+   else
+      print(string.format('function %s %s = %s', functionName, variableName, value))
+   end
 end
