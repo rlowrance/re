9da8e17f2da951f3d60ece406c88c54491c8c4ee
Roy Lowrance <roy.lowrance@gmail.com>
2014-03-03 11:05:01 -0500

    add arg to specify output file

--- printTableValue.lua
+++ printTableValue.lua
@@ -1,22 +1,38 @@
 -- printTableValua.lua
 -- print value of a table
 
-
 -- ARGS
 -- variableName : optional string, name of variable holding a table value
 -- tableValue   : table
-function printTableValue(variableName, tableValue)
+-- file         : optional open file, default is io.stdout
+function printTableValue(variableName, tableValue, file)
 
-   local function printField(tableName, fieldName, fieldValue)
+   -- create optional args
+
+   -- ptv(table) --> ptv ('', table, io.stdout)
+   if type(variableName) == 'table' and tableValue == nil and file == nil then
+      return printTableValue('', variableName, io.stdout)
+
+   -- ptv('name', table) --> ptv('name', table, io.stdout)
+   elseif type(variableName) == 'string' and type(tableValue) == 'table' and file == nil then
+      return printTableValue(variableName, tableValue, io.stdout)
+   
+   -- ptv(table, file) --> ptv('', table, file)
+   elseif type(variableName) == 'table' and tableValue ~= nil then
+      return printTableValue('', variableName, tableValue)
+   end
+
+   local function printField(tableName, fieldName, fieldValue, file)
       assert(fieldValue ~= nil)
-      print(string.format('%s.%s = %s', tableName, fieldName, tostring(fieldValue)))
+      file:write(string.format('%s.%s = %s', tableName, fieldName, tostring(fieldValue)))
+      file:write('\n')
    end
 
-   local function printStorage(tableName, fieldName, value, torchTypename)
-      printField(tableName, fieldName, torchTypename .. ' size ' .. value:size())
+   local function printStorage(tableName, fieldName, value, torchTypename, file)
+      printField(tableName, fieldName, torchTypename .. ' size ' .. value:size(), file)
    end
 
-   local function printTensor(tableName, fieldName, value, torchTypename)
+   local function printTensor(tableName, fieldName, value, torchTypename, file)
       local sizes = value:size()
       local shape = torchTypename .. ' size '
       for i = 1, #sizes do
@@ -26,24 +42,24 @@ function printTableValue(variableName, tableValue)
             shape = shape .. ' x ' .. tostring(sizes[i])
          end
       end
-      printField(tableName, fieldName, shape)
+      printField(tableName, fieldName, shape, file)
    end
 
-   local function printUserdata(tableName, fieldName, value)
+   local function printUserdata(tableName, fieldName, value, file)
       local torchTypename = torch.typename(value)
       if torchTypename ~= nil and string.sub(torchTypename, -6) == 'Tensor' then
          -- found a Tensor
-         printTensor(tableName, fieldName, value, torchTypename)
+         printTensor(tableName, fieldName, value, torchTypename, file)
       elseif torchTypename ~= nil and string.sub(torchTypename, -7) == 'Storage' then
          -- found a Storage
-         printStorage(tableName, fieldName, value, torchTypename)
+         printStorage(tableName, fieldName, value, torchTypename, file)
       else
          -- found either non-torch userdata or non-Tensor torch data
-         printField(tableName, fieldName, value)
+         printField(tableName, fieldName, value, file)
       end
    end
 
-   local function printTableNameValue(tableName, tableValue)
+   local function printNameValueFile(tableName, tableValue, file)
       -- sort the keys
       local keysSorted = {}
       for key in pairs(tableValue) do
@@ -66,34 +82,27 @@ function printTableValue(variableName, tableValue)
          --print('fieldName', fieldName, 'value', value, 'valueType', valueType)
 
          if valueType == 'userdata' then
-            printUserdata(tableName, fieldName, value)
+            printUserdata(tableName, fieldName, value, file)
          elseif valueType == 'table' then
-            printTableValue(tableName .. '.' .. fieldName, value)
+            printTableValue(tableName .. '.' .. fieldName, value, file)
          else
-            printField(tableName, fieldName, value)
+            printField(tableName, fieldName, value, file)
          end
       end
    end
 
-   local function errorBadType(tableValue)
+   -- tupe check args and call driver
+   if type(variableName) ~= 'string' then
+      error(string.format('variableName is type %s, not string', type(variableName)))
+   end
+
+   if type(tableValue) ~= 'table' then
       error(string.format('tableValue is type %s, not table', type(tableValue)))
    end
 
-   -- handle the first arg, which is optional so that f(b) --> f('', b)
-   -- also type check arguments
-   if type(variableName) == 'string' then
-      if type(tableValue) == 'table' then
-         printTableNameValue(variableName, tableValue)
-      else
-         errorBadType(tableValue)
-      end
-   elseif type(variableName) == 'table' then
-      if type(tableValue) == nil then
-         printTableNameValue('', tableValue)
-      else
-         errorBadType(tableValue)
-      end
-   else
-      error('calling sequence is printTableValue(variableName, tableValue) or printTableValue(tableValue)')
+   if file == nil then
+      error('file is nil, not an open file handle')
    end
+
+   return printNameValueFile(variableName, tableValue, file)
 end
