a805c80aa17300bc3f1b21f136f22a196501cade
Roy Lowrance <roy.lowrance@gmail.com>
2014-04-02 10:37:34 -0400

    also accept torch.LongTensor at a Tensor

--- isTensor.lua
+++ isTensor.lua
@@ -4,6 +4,7 @@ function isTensor(value)
    local typename = torch.typename(value)
    return typename == 'torch.DoubleTensor' or
           typename == 'torch.FloatTensor' or
+          typename == 'torch.LongTensor' or
           typename == 'torch.ByteTensor' or
           typename == 'torch.CharTensor' or
           typename == 'torch.IntTensor'
