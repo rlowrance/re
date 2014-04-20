dd984b0d83764f708b31e06ffeb3f3dc961f062b
Roy Lowrance <roy.lowrance@gmail.com>
2014-04-20 10:00:47 -0400

    remove idea of path to a file

--- readViaSerializedFile.lua
+++ readViaSerializedFile.lua
@@ -4,18 +4,89 @@
 require 'equalObjectValues'
 require 'fileExists'
 require 'makeVp'
+require 'memoryUsed'
 require 'pp'
 require 'tableCopy'
+require 'tableCount'
+require 'tableSplit'
 require 'torch'
 
-local function readAndWriteCache(path, fn, args, pathCache)
-   local obj = fn('object', path, args)
+local function readCache(pathCache)
+   local file = torch.DiskFile(pathCache, 'r')
+   assert(file)
+
+   local s = file:readObject()
+   assert(s == 'fnVersion')
+   local fnVersion = file:readObject()
+
+   local s = file:readObject()
+   assert(s == 'args')
+   local args = file:readObject()
+
+   local allValues = {}
+   repeat
+      local s = file:readObject()
+      if s ~= 'value' then 
+         break
+      end
+      local t = file:readObject()
+      for k, v in pairs(t) do
+         allValues[k] = v
+      end
+   until false
+
+   local result = {
+      fnVersion = fnVersion,
+      args = args,
+      values = allValues,
+   }
+
+   return result
+end
+
+local function writeTable(file, fieldName, t)
+   local threshold = 400000  -- much larger values result in luajit out of memory
+   local threshold = 200000
+   local threshold = 10000
+   local threshold = 5000
+   local size = tableCount(t)
+   if size > threshold then
+      print('writeTable', 'threshold', threshold, 'size', size)
+      local t1, t2 = tableSplit(t)
+      writeTable(file, fieldname, t1)
+      t1 = nil
+      writeTable(file, fieldname, t2)
+      t2 = nil
+      t = nil
+   else
+      print('writeTable', 'memoryUsed / 1e6', memoryUsed() / 1e6)
+      file:writeObject(fieldName)
+      file:writeObject(t)
+      t = nil
+   end
+end
+
+local function writeCache(diskObj, pathCache)
+   local file = torch.DiskFile(pathCache, 'w')
+
+   file:writeObject('fnVersion')
+   file:writeObject(diskObj.key)
+
+   file:writeObject('args')
+   file:writeObject(diskObj.args)
+
+   writeTable(file, 'value', diskObj.value)
+end
+
+local function readAndWriteCache(fn, args, pathCache)
+   local obj = fn('object', args)
    local diskObj = {
-      fnVersion = fn('version', path, args),
-      args = tableCopy(args),
+      fnVersion = fn('version', args),
+      args = args,
       value = obj,
    }
-   torch.save(pathCache, diskObj, 'binary')
+   print('readAndWriteCache:about to write cache', 'memoryUsed / 1e6', memoryUsed() / 1e6)
+   writeCache(diskObj, pathCache)
    return obj
 end
 
@@ -44,22 +115,25 @@ end
 
 -- read a file and transform it or the corresponding serialized transformation
 -- ARGS:
--- path : string, path to file
--- fn   : function(what, path, args) --> obj or version number
---        function to read the file at path and transform it using the args into an arbitrary object
---        where
---        what : string in {'version', 'object'}
---        path : string
---        args : table
--- args : table of arguments
+-- pathCache : string, path to cache file
+-- fn        : function(what, path, args) --> obj or version number
+--             function to read the file at path and transform it using the args into an arbitrary object
+--             where
+--             what : string in {'version', 'object'}
+--             args : table
+-- args      : table of arguments to fn
 -- RETURNS
 -- usedCache : boolean, true iff the value in the cache was used 
 -- obj       : object created by fn(path, 'readAndTransform', args)
 -- msg       : string, if not usedCache, why
 -- SIDE EFFECTS:
--- . Creates file path .. '.serialized'
-function readViaSerializedFile(path, fn, args)
-   local pathCache = path .. '.serialized'
+-- Creates cache containing the results of transforming the input
+function readViaSerializedFile(pathCache, fn, args)
+   assert(type(pathCache) == 'string')
+   assert(type(fn) == 'function')
+   assert(type(args) == 'table')
+
+   print('pathCache', pathCache)
    if fileExists(pathCache) then
       local diskObj = torch.load(pathCache, 'binary')
       assert(type(diskObj) == 'table')
@@ -67,18 +141,18 @@ function readViaSerializedFile(path, fn, args)
       -- check function version
       local fnVersion = fn('version', path, args)
       if diskObj.fnVersion ~= fnVersion then
-         return false, readAndWriteCache(path, fn, args, pathCache), msgVersion(diskObj.version, fnVersion)
+         return false, readAndWriteCache(fn, args, pathCache), msgVersion(diskObj.version, fnVersion)
       end
 
       -- check value of each arg
       local equal = equalObjectValues(diskObj.args, args)
       if not equal then
-         return false, readAndWriteCache(path, fn, args, pathCache), msgArgs(diskObj.args, args, 'diskobj.Args', 'args')
+         return false, readAndWriteCache(fn, args, pathCache), msgArgs(diskObj.args, args, 'diskobj.Args', 'args')
       end
 
       -- function version and all args are equal, so return version on disk
       return true, diskObj.value
    else
-      return false,  readAndWriteCache(path, fn, args, pathCache), 'no cache file'
+      return false,  readAndWriteCache(fn, args, pathCache), 'no cache file'
    end
 end
