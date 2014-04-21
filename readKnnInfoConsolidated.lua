6e9d1229cc0624b33c03ed26aa65a567a4645c95
Roy Lowrance <roy.lowrance@gmail.com>
2014-04-21 07:56:26 -0400

    conform to API for cachedComputation2.lua

--- readKnnInfoConsolidated.lua
+++ readKnnInfoConsolidated.lua
@@ -2,66 +2,62 @@
 
 require 'knn'
 require 'memoryUsed'
+require 'pp'
 require 'torch'
 
 -- read file program_impute_knninfo_consolidate_shards_to_serialized_objects[_debug].serializedObjects
+-- conform to API of function cachedComputation2()
 -- ARGS:
--- what  : string in {'version', 'object'}
--- path  : string, path to the input file
--- args  : table with these fields
+-- arg   : nil or table with these fields
 --         .newSize : integer > 0, each knnInfo is shortened to this size (<= 1024)
+--         .nShards : integer > 0, default 100; number of shards to read (for testing)
 -- RETURNS
--- result : version number or object
-function readKnnInfoConsolidated(what, path, args)
-   local debug = true   
-   if debug then print('readKnnInfoConsolidated', what, path, args) end
-
-   if what == 'version' then
-      return 3
+-- result : number, version number, if arg == nil
+--          table s.t table[queryIndex] = knnInfo
+-- NOTES: takes a long time, consider wrapping your call in cachedComputation2
+function readKnnInfoConsolidated(arg)
+   local debug = true
+   if arg == nil then
+      return 6  -- return version number
    end
 
-   local inputFile = torch.DiskFile(path, 'r')
-   assert(inputFile)
+   if debug then pp.table('arg', arg) end
+   assert(type(arg) == 'table')
 
-   local knnInfos = {}
-   local inputRecordCount = 0
-   repeat  -- find a queryIndex followed by an knnInfo
-      inputRecordCount = inputRecordCount + 1
+   local function isQueryIndex(value)
+      return type(value) == 'number' and value >= 1
+   end
 
-      -- read until we find a queryIndex (which is a number)
-      local queryIndex
-      repeat
-         queryIndex = inputFile:readObject()
-         if type(queryIndex) ~= 'number' then
-            print('queryIndex not a number', inputRecordCount, queryIndex(type), queryIndex)
+   local function consolidateShard(shardNumber, consolidated)
+      local inPathBase = '../data/v6/output/knninfo/impute-shards/program_impute_knninfo_make_shard_'
+      local inPath = inPathBase .. tostring(shardNumber) .. '.serialized'
+      print('inPath', inPath)
+      if fileExists(inPath) then
+         local shard = torch.load(inPath)
+         local count = 0
+         for queryIndex, knnInfo in pairs(shard) do
+            count = count + 1
+            if isQueryIndex(queryIndex) then
+               if knn.isKnnInfo(knnInfo) then
+                  consolidated[queryIndex] = knnInfo
+               else
+                  print('bad knnInfo', shardNumber, count, tostring(knnInfo))
+               end
+            else
+               print('bad queryIndex', shardNumber, count, tostring(queryIndex))
+            end
          end
-      until type(queryIndex) == 'number' or type(queryIndex) == 'nil'
-
-      if queryIndex == nil then
-         break
+         shard = nil  -- try to assist the garbage collector
+      else
+         error(string.format('did not find shard %d', shardNumber))
       end
+   end
 
-      -- read until we find a knnInfo
-      local knnInfo
-      repeat
-         knnInfo = inputFile:readObject()
-         if not knn.isKnnInfo(knnInfo) then
-            print('not q knnInfo', inputRecordCount, queryIndex, tostring(knnInfo))
-         end
-      until knn.isKnnInfo(knnInfo)
+   local consolidated = {}
+   for shardNumber = 0, arg.nShards - 1 do
+      consolidateShard(shardNumber, consolidated)
+      print('readKnnInfoConsolidate', 'shardNumber', shardNumber, 'memoryUsed / 1e6', memoryUsed() / 1e6)
+   end
 
-
-      knnInfos[queryIndex] = knn.trim(knnInfo, args.newSize)
-
-      -- maybe help the garbage collector
-      queryIndex = nil
-      knnInfo = nil
-
-      if inputRecordCount % 1000 == 0 then
-         local used = memoryUsed()  -- collect garbage
-         print('inputRecordCount', inputRecordCount, 'memory used/1e6', used / 1e6)
-      end
-   until false
-
-   return knnInfos
+   return consolidated
 end
