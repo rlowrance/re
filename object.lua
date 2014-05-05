e91d9a0776fe0d301ddf6dcc599db3f8f5701c39
Roy Lowrance <roy.lowrance@gmail.com>
2014-05-05 07:39:43 -0400

    better estimate of object sizes
    Still an estimate

--- object.lua
+++ object.lua
@@ -2,13 +2,17 @@
 -- useful functions that operate on arbitrary objects
 
 if false then
-   bytes = object.bytes(obj)   -- bytes in the object
+   nBytes = object.nBytes(obj)   -- bytes in the object
 end
 
 object = {}
 
 -- ref: www.wowwiki.com/Lua_object_memory_sizes
-function object.bytes(obj)
+function object.nBytes(obj)
+   local function bytesTensor(elementBytes, tensor)
+      return tensor:nElement() * elementBytes
+   end
+
    local t = type(obj)
    if t == 'boolean' then
       return 8
@@ -27,7 +31,7 @@ function object.bytes(obj)
       return 24 + string.len(obj)
    elseif t == 'thread' then
       return 16
-   elseif t = 'table' then
+   elseif t == 'table' then
       -- the table may have preallocated members
       -- this code doesn't see them
       -- each element in the array part takes 16 bytes
@@ -37,23 +41,20 @@ function object.bytes(obj)
          if type(k) == 'number' then
             sum = sum + 16  -- assume all number keys are in the array part
          else
-            sum = sum + 40 + object.bytes(k) + object.bytes(v)
+            sum = sum + 40 + object.nBytes(k) + object.nBytes(v)
          end
       end
       return sum
    elseif t == 'userdata' then
-      if true then
-         return 8 + #obj  -- #obj is the size of the user data (maybe)
-      end
       local tt = torch.typename(obj)
-      if tt == 'torch.IntTensor' or tt = 'torch.FloatTensor' then
+      if tt == 'torch.IntTensor' or tt == 'torch.FloatTensor' then
          return bytesTensor(4, obj)
       elseif tt == 'torch.LongTensor' or tt == 'torch.DoubleTensor' then
          return bytesTensor(8, obj)
-      elseif tt = 'torch.CharTensor' or tt == 'torch.ByteTensor' then
+      elseif tt == 'torch.CharTensor' or tt == 'torch.ByteTensor' then
          return bytesTensor(1, obj)
       else
-         error('torch.typename(obj)', tt)
+         return 8 + #obj   -- some say that #obj is the size of the user data
       end
    else
       error('type(obj)', t)
