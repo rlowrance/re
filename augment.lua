c25379224d719388a7ed8e3a40b74dd0ae1e9110
Roy Lowrance <roy.lowrance@gmail.com>
2014-05-05 13:30:28 -0400

    expand to handle vectors and matrices

--- augment.lua
+++ augment.lua
@@ -1,15 +1,26 @@
 -- augment.lua
 
--- insert a 1 into first position of vector
-function augment(v)
-   assert(type(v) == 'userdata' and
-          v:dim() == 1,
-          'v is not a 1D Tensor')
-   local size = v:size(1)
-   local result = torch.Tensor(size + 1)
-   result[1] = 1
-   for d = 1, size do
-      result[d + 1] = v[d]
+-- insert a 1 into first position of vector or matrix
+function augment(t)
+   local nDimension = t:nDimension()
+   if nDimension == 1 then
+      local size = t:size(1)
+      local result = torch.Tensor(size + 1)
+      result[1] = 1
+      for d = 1, size do
+         result[d + 1] = t[d]
+      end
+      return result
+   elseif nDimension == 2 then
+      -- reduce to vector case
+      local size1 = t:size(1)
+      local size2 = t:size(2)
+      local result = torch.Tensor(size1, size2 + 1)
+      for i = 1, size1 do
+         result[i] = augment(t[i])
+      end
+      return result
+   else
+      error('t not 1D nor 2D')
    end
-   return result
 end
--- augment_test.lua
+++ augment_test.lua
@@ -4,6 +4,7 @@
 require 'assertEq'
 require 'augment'
 require 'makeVp'
+require 'pp'
 
 local verbose = 0
 local vp = makeVp(verbose, 'tester')
@@ -16,5 +17,15 @@ local expected = torch.Tensor{1,2,3,4}
 vp(2, 'expected', expected)
 
 assertEq(a, expected, 0)
+
+local m = torch.rand(3, 10)
+local mAugmented = augment(m)
+--pp.tensor('m', m)
+--pp.tensor('mAugmented', mAugmented)
+assert(mAugmented:nDimension() == 2)
+assert(mAugmented:size(1) == m:size(1))
+assert(mAugmented:size(2) == m:size(2) + 1)
+assert(mAugmented[1][1] == 1)
+assert(mAugmented[1][2] == m[1][1])
 
 print('ok augment')
