1a33e09d142556c1b9fdea7bf331911a25f4b251
Roy Lowrance <roy.lowrance@gmail.com>
2014-05-17 07:49:10 -0400

    new: set Roy's fav options

---
+++ InitializeR.R
@@ -0,0 +1,23 @@
+# InitializeR.R
+InitializeR <- function(start.JIT=TRUE, duplex.output.to=NULL) {
+    # setup R's execution environment
+    # ARGS: None
+    # RETURNS: NULL
+
+    options(warn=2)  # turn warnings into errors
+    options(error=dump.frames)  # after a crash run command debugger()
+
+    set.seed(1)      # random number generator seed
+
+    if (start.JIT) {
+        require('compiler')
+        enableJIT(3)  # 3 ==> max JIT level
+        # now sourced file are compiled with JIT enabled
+    }
+
+    if (!is.null(duplex.output.to)) {
+        sink(file = duplex.output.to, 
+             type = 'output', 
+             split = TRUE)  # send output to console and the file specified
+    }
+}
