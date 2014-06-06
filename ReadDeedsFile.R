24a203c70d0bb35815a383ee060667820bbc980f
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-06 06:40:59 -0400

    new: read specific deeds file

---
+++ ReadDeedsFile.R
@@ -0,0 +1,22 @@
+# ReadDeedsFile.R
+ReadDeedsFile <- function(num, nrow=-1, path='../data/raw/from-laufer-2010-05-11/deeds/') {
+    # return data.frame containing all rows and columns from a parcels file
+    # ARGS
+    # num : integer, number of the file (1, 2, ... 8)
+    # nrow : number of rows to read, -1 for all
+    # path : path to directory containing the parcels files
+    # RETURNS a data.frame
+    path <- paste0(path,
+                  "CAC06037F",
+                  num,
+                  ".txt.gz")
+    read.table(path,
+               header=TRUE,
+               sep="\t",
+               quote="",
+               comment="",
+               stringsAsFactors=FALSE,
+               na.strings="",
+               nrows=nrow)
+    
+}
