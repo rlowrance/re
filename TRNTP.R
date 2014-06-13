8aefe7da8e8849b7b827f1d478b11d38faf11bcd
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-13 10:31:17 -0400

    new: decode transaction type

---
+++ TRNTP.R
@@ -0,0 +1,24 @@
+TRNTP <- function(code, kind) {
+    # determine if a transaction type code is a specified level, treating NA is not at level
+    # ARGS
+    # code : integer vector
+    # kind  : optional chr scalar
+
+    stopifnot(all(is.number(code)))
+    with.na <- 
+    switch(kind,
+           resale            = code == 1,
+           refinance         = code == 2,
+           new.construction  = code == 3,
+           timeshare         = code == 4,
+           construction.loan = code == 6,
+           seller.carryback  = code == 7,
+           nominal           = code == 9,
+           # composite types NONE FOR NOW
+
+           # default
+           ...                             = stop(sprintf('bad kind = %s', kind))
+           )
+    result <- ifelse(is.na(with.na), FALSE, with.na)
+    result
+}
