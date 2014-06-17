dac7bc4e206afd8de7eb606a47599e8a2a708a46
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-17 15:10:40 -0400

    catch up

--- simple-models.R
+++ simple-models.R
@@ -23,16 +23,18 @@ control <- list()
 # pick computation to do
 control$which <- 'cross.validate'
 
-control$which.cross.validate <- 'all'
+#control$which.cross.validate <- 'all'
 #control$which.cross.validate <- 'each.once'
 #control$which.cross.validate <- 'random.forest.nmonth'
 #control$which.cross.validate <- 'random.forest.hp.search'
 #control$which.cross.validate <- 'location.features'
-#control$which.cross.validate <- 'hpi'
+control$which.cross.validate <- 'index.cs'
+
+control$nfolds <- 10
 
 control$me <- 'simple-models'
 control$dir.output <- '../data/v6/output/'
-control$path.subset1 <- paste0(control$dir.output, 'transactions-subset1.csv')
+control$path.subset1 <- paste0(control$dir.output, 'transactions-subset1.csv.gz')
 control$path.log <- paste0(control$dir.output, 
                            control$me, 
                            '-',
@@ -40,15 +42,26 @@ control$path.log <- paste0(control$dir.output,
                            '-',
                            control$which.cross.validate,
                            '-', 
+                           control$nfolds, 
+                           '-',
                            DateTime(), 
                            '.txt')
 control$testing <- TRUE
 control$testing <- FALSE
 
+# possibly change some control variables based on the command line
+cl <- commandArgs(trailingOnly = TRUE)
+cl.index <- 1
+while (cl.index < length(cl)) {
+    command <- cl[cl.index]
+    value <- cl[cl.index + 1]
+    control[[command]] <- value
+    cl.index <- cl.index + 1
+}
 
-# override control using values from command line
-command.line.args <- commandArgs(trailingOnly = TRUE)  # return only args after --args
-stopifnot(length(command.line.args) == 0)  # for now, don't handle command line args
+cat('control variables\n')
+str(control)
+
 
 # initialize
 source('InitializeR.R')
@@ -61,6 +74,9 @@ if (TRUE) {
     #options(error=recover)
 }
 
+cat('control list\n')
+str(control)
+
 # source files now that JIT is set
 source('Center.R')
 source('CrossValidate2.R')
@@ -68,6 +84,7 @@ source('DuplexOutputTo.R')
 source('NumberFeaturesWithAnNA.R')
 source('Printf.R')
 source('ReadAndTransformTransactions.R')
+source('ReadCaseShiller.R')
 source('SplitDate.R')
 
 #library(rpart)
@@ -91,9 +108,9 @@ RenameColumns <- function(df, old.names, new.names) {
 }
 
 RenameColumns.Test <- function() {
-    df <- data.frame(a <- c(1,2,3),
-                     b <- c(10,20,30),
-                     c <- c(100, 200, 300))
+    df <- data.frame(a = c(1,2,3),
+                     b = c(10,20,30),
+                     c = c(100, 200, 300))
     renamed <- RenameColumns(df, c('a', 'c'), list('A', 'C'))
     stopifnot(renamed$A != NULL)
     stopifnot(all(renamed$A == c(1,2,3)))
@@ -109,14 +126,17 @@ Rmse <- function(actual, estimated) {
     # return square root of mean squared error
     stopifnot(length(actual) == length(estimated))
     stopifnot(length(actual) > 0)
-    error <- actual - estimated
-    mse <- sum(error * error) / length(actual)
+    na.indices <- is.na(actual) | is.na(estimated)
+    actual.ok <- actual[!na.indices]
+    estimated.ok <- estimated[!na.indices]
+    error <- actual.ok - estimated.ok
+    mse <- sum(error * error) / length(actual.ok)
     sqrt(mse)
 }
 
 Rmse.test <- function() {
-    actual <- c(10, 20, 30)
-    estimated <- c(11, 21, 31)
+    actual <- c(10, 20, 30, 40)
+    estimated <- c(11, 21, 31, NA)
     mse <- Rmse(actual, estimated)
     stopifnot(mse == 1)
 }
@@ -131,18 +151,18 @@ Within10Percent <- function(actual, estimated, precision = .10) {
     stopifnot(length(actual) > 0)
     stopifnot(sum(actual == 0) == 0)  # at least one actual is zero
     na.indices <- is.na(actual) | is.na(estimated)  # find NAs in either arg
-    actual <- actual[!na.indices]
-    estimated <- estimated[!na.indices]
-    error <- actual - estimated
-    relative.error <- error / actual
-    sum(abs(relative.error) <= precision) / length(actual)
+    actual.ok <- actual[!na.indices]
+    estimated.ok <- estimated[!na.indices]
+    error <- actual.ok - estimated.ok
+    relative.error <- error / actual.ok
+    sum(abs(relative.error) <= precision) / length(actual.ok)
 }
 
 Within10Percent.test <- function() {
     actual <- c(1,2,3)
     estimated <- c(1.05, 20, NA)
     within <- Within10Percent(actual, estimated)
-    cat('within', within, '\n')
+    #cat('within', within, '\n')
     stopifnot(all.equal(within, 1/2))
 }
 
@@ -500,6 +520,13 @@ last.testing.date <- first.testing.date + 30
 ErrorRateModel <- function(model, 
                            df, training.indices, testing.indices,
                            training.months.before, response, predictors) {
+    # determine error rates and model description
+    # ARGS
+    # model              : list of $Fit(df, selected), $Predict(df, selected), $description
+    # return list
+    # $rmse              : vector of RMSE values for estimates and actuals
+    # $within.10.percent : scalar, number of estimates within 10 percent of actuals
+    # $description       : chr
     first.training.date <- first.testing.date - training.months.before * 30
     selected.training <- training.indices & df$saleDate >= first.training.date & df$saleDate < first.testing.date
 
@@ -596,66 +623,226 @@ RandomForestHpSearchPrep <- function(nsamples) {
 
 random.forest.samples <- RandomForestHpSearchPrep(10)
 
+MakeModelIndex <- function(prior.index.months, index.name) {
+    # make Index model assuming 3-month lagged data and forecasting in Jan 2009
+    # return list 
+    # Fit(df, training.indices) --> fiited model
+    # Predict(fitted, df, testing.indices)  --> predictions vector
+    # $description : chr
+
+    #cat('starting MakeModelIndex', prior.index.months, index.name, '\n'); browser()
+
+    # Read Case-Shiller indicise for Los Angeles
+    result <- ReadCaseShiller(area = 'LosAngeles')
+    Retrieve <- result$Retrieve  # Retrieve(year, month) --> NA or index value
+
+    DetermineIndexFor2009Jan <- function() {
+        # project historic index values forward until Jan 2009
+        # use a linear model
+        # NOTE: index values are delayed by about 3 months
+        # the selection of training data should take this into account
+        #cat('starting DetermineIndexfor2009Jan\n'); browser()
+        verbose <- FALSE
+        stopifnot(prior.index.months >= 2)
+        stopifnot(prior.index.months <= 12)
+        months <- c()
+        indices <- c()
+        for (prior.month in 1:prior.index.months) {
+            index.month <- 12 - prior.month + 1
+            index.level <- Retrieve(2008, index.month)
+            months <- c(months, index.month)
+            indices <- c(indices, index.level)
+        }
+        if (verbose) {
+            print(months)
+            print(indices)
+        }
+        df <-data.frame(level = indices,
+                        month = months)
+        m <- lm(level ~ month, df)
+        if (verbose) {
+            print(m)
+            print(summary(m))
+        }
+        newdata <- data.frame(month = c(13))  # predict 2009 Jan (month index 13)
+        index.estimate <- predict(m, newdata)
+        #cat('ending DetermineIndexfor2009Jan\n'); browser()
+        index.estimate
+    }
+
+    PriorSale <- function(apn, df.training) {
+        # return NA or list(sale.year, sale.month, price)
+        # ARGS:
+        # apn : number of apn
+        # df.training : the entire training dataframe
+        #               searched to find the most recent sale of the APN
+        #cat('starting PriorSale', apn, '\n'); browser()
+        prior.sales <- df.training[df.training$apn == apn,
+                                   c('sale.year', 'sale.month', 'saleDate',
+                                     'price')]  # possibly empty
+        if (nrow(prior.sales) == 0) {
+            return(NA) # NA ==> there is no prior sale for this APN
+        }
+        largest.date <- max(prior.sales$saleDate)  
+        prior.sales.largest.date <- prior.sales[prior.sales$saleDate == largest.date, ]
+        # handle more than one occurrence of largest.date via [1]
+        sale.year <- prior.sales.largest.date$sale.year[1]
+        sale.month <- prior.sales.largest.date$sale.month[1]
+        price <- prior.sales.largest.date$price[1]
+        list(sale.year = sale.year,
+             sale.month = sale.month,
+             price = price)
+    }
+
+    Fit <- function(df, training.indices) {
+        # return all the training data
+        df[training.indices, c('apn', 'sale.month', 'sale.year', 'saleDate', 'price')]
+    }
+
+
+    PredictUsingIndex <- function(prior.sale) {
+        # use Retrieve() to update prior.sale price to current price
+        # return predicted price in current time period or NA if no index available
+        # for prior period
+        #cat('starting PredictUsingIndex\n'); browser()
+        if (is.na(prior.sale[[1]])) {
+            return(NA)
+        }
+
+        old.index <- Retrieve(year = prior.sale$sale.year, month = prior.sale$sale.month)
+        
+        if (is.na(old.index)) {
+            return(NA)
+        }
+
+        current.index <- DetermineIndexFor2009Jan()
+        predicted.price <- (prior.sale$price / old.index) * current.index
+        predicted.price
+    }
+
+    Predict <- function(fitted, df, testing.indices) {
+        testing <- FALSE
+        # predict test data using fitted Index model
+        # return list of predicted prices or NA values
+        #cat('starting MakeModelIndex::Predict', '\n'); browser()
+        df.training <- fitted
+        stopifnot(prior.index.months >= 2)
+        stopifnot(index.name == 'CS')
+        new.index <- DetermineIndexFor2009Jan()  # NOTE: has attributes
+        selected <- testing.indices & df$sale.year == 2009 & df$sale.month == 1
+        apns <- df[selected, 'apn']
+        if (testing) apns <- apns[1:3]
+        prior.sale <- lapply(apns, PriorSale, df.training)
+        #cat('after determining prior.sale\n'); browser()
+        predicted.price <- lapply(prior.sale, PredictUsingIndex)
+        # the NA values, if present, create a list
+        # always return a numeric vector
+        predicted.price <- as.numeric(predicted.price) 
+        if (testing) {cat('ending MakeModelIndex::Predict\n'); browser()}
+        if (testing) stop('unset testing')
+        #cat('ending MakeModelIndex::Predict\n'); browser()
+        predicted.price
+    }
+
+    list(Fit = Fit,
+         Predict = Predict,
+         description = sprintf('index %s prior months %d', index.name, prior.index.months))
+}
+
+
+ErrorRateIndex <- function(df, training.indices, testing.indices,
+                           prior.index.months, index.name) {
+    # determine error rates for index-based estimates
+    #cat('starting ErrorRateIndex', prior.index.months, index.name, '\n'); browser()
+    
+    # make sure we have the apn feature is preset
+    if (is.null(df$apn)) {
+        stop('missing apn feature from data')
+    }
+
+    model <- MakeModelIndex(prior.index.months, index.name)
+    fitted <- model$Fit(df, training.indices)
+    estimated <- model$Predict(fitted, df, testing.indices)
+    #cat('in ErrorRateIndex after estimated\n'); browser()
+
+    # use just the testing.indices in Jan 2009, not all of them
+    selected <- testing.indices & df$sale.year == 2009 & df$sale.month == 1
+    actual <- df[selected, c('price')]  # TODO: may have to drop NA values
+
+    # drop missing estimated or actual prices
+    missing <- is.na(estimated) | is.na(actual)
+    estimated.ok <- estimated[!missing]
+    actual.ok <- actual[!missing]
+    coverage <- length(estimated.ok) / length(estimated)
+
+    #cat('ending ErrorRateIndex\n'); browser()
+    list(rmse = Rmse(actual = actual.ok, estimated = estimated.ok),
+         within.10.percent = Within10Percent(actual = actual.ok, estimated = estimated.ok),
+         coverage = coverage,
+         description = model$description )
+}
+
+
 CompareViaCrossValidation <- function(control, transformed.data) {
     # cross validate models named via control$which.cross.validate chr
 
     ErrorRate <- function(model.number, df, training.indices, testing.indices) {
         # return $error.rate and $other.info
 
-        Rename <- function(lst, features.name) {
+        Rename <- function(lst, regressor.features.name) {
             # rename list of results to conform to API for CrossValidate2
             # also propogate the feature name into the description
             list(error.rate = lst$rmse,
                  other.info = lst$within.10.percent,
-                 description = paste(lst$description, features.name))
+                 description = paste(lst$description, regressor.features.name))
         }
 
-        Features <- function(features.name) {
+        Features <- function(regressor.features.name) {
             # return feature set
-            switch(features.name,
+            switch(regressor.features.name,
                    centered = PredictorsChopraCentered(),
                    centered.location = PredictorsChopraCenteredLocation(),
                    centered.log = PredictorsChopraCenteredLog(),
                    centered.log.location = PredictorsChopraCenteredLogLocation(),
                    raw = PredictorsChopraRaw(),
                    raw.location = PredictorsChopraRawLocation(),
-                   ... = stop(paste('bad features.name', features.name)))
+                   ... = stop(paste('bad regressor.features.name', regressor.features.name)))
         }
 
 
 
 
-        A <- function(training.months, features.name = 'centered') {
+        A <- function(training.months, regressor.features.name = 'centered') {
             Rename(ErrorRateLinear(df, training.indices, testing.indices, 
                                    training.months, 'price', 
-                                   Features(features.name)),
-                   features.name)
+                                   Features(regressor.features.name)),
+                   regressor.features.name)
                    
         }
 
-        B <- function(training.months, features.name = 'centered.log') {
+        B <- function(training.months, regressor.features.name = 'centered.log') {
             Rename(ErrorRateLinearLog(df, training.indices, testing.indices, 
                                       training.months, 'log.price', 
-                                      Features(features.name)),
-                   features.name)
+                                      Features(regressor.features.name)),
+                   regressor.features.name)
 
 
 
         }
 
-        C <- function(training.months, features.name = 'raw') {
+        C <- function(training.months, regressor.features.name = 'raw') {
             Rename(ErrorRateTree(df, training.indices, testing.indices,
                                  training.months, 'price',
-                                 Features(features.name)),
-                   features.name)
+                                 Features(regressor.features.name)),
+                   regressor.features.name)
 
         }
                     
-        D <- function(training.months, ntree = 100, features.name = 'raw') {
+        D <- function(training.months, ntree = 100, regressor.features.name = 'raw') {
             Rename(ErrorRateRandomForest(df, training.indices, testing.indices,
                                          training.months, 'price', ntree,
-                                         Features(features.name)),
-                   features.name)
+                                         Features(regressor.features.name)),
+                   regressor.features.name)
         }
 
         HPI <- function(prior.index.months, index.name) {
@@ -680,9 +867,24 @@ CompareViaCrossValidation <- function(control, transformed.data) {
 
         switch(control$which.cross.validate,
 
-               hpi = switch(model.number,
+               index.cs = switch(model.number,
+                            HPI(prior.index.months = 2, index.name = 'CS'),
                             HPI(prior.index.months = 3, index.name = 'CS'),
-                            HPI(prior.index.months = 3, index.name = 'Zip5')),
+                            HPI(prior.index.months = 4, index.name = 'CS'),
+                            HPI(prior.index.months = 5, index.name = 'CS'),
+                            HPI(prior.index.months = 6, index.name = 'CS'),
+                            HPI(prior.index.months = 7, index.name = 'CS'),
+                            HPI(prior.index.months = 8, index.name = 'CS'),
+                            HPI(prior.index.months = 9, index.name = 'CS'),
+                            HPI(prior.index.months = 10, index.name = 'CS'),
+                            HPI(prior.index.months = 11, index.name = 'CS'),
+                            ),
+
+               index.zip5 = switch(model.number,
+                            HPI(prior.index.months = 2, index.name = 'Zip5'),
+                            HPI(prior.index.months = 3, index.name = 'Zip5'),
+                            HPI(prior.index.months = 4, index.name = 'Zip5'),
+                            HPI(prior.index.months = 5, index.name = 'Zip5')),
 
                location.features = switch(model.number,  # compare best model form w and wo location features
                                           A(1, 'centered.location'), A(1, 'centered'),
@@ -730,8 +932,11 @@ CompareViaCrossValidation <- function(control, transformed.data) {
                )
     }
 
-    nfolds <- 5
+    nfolds <- control$nfolds
     nmodels <- switch(control$which.cross.validate, # must match switch statement in ErrorRate above
+                      index.cs = 10,  
+                      index.cs = 4,
+                      index.zip5 = 4,
                       location.features = 8,
                       all = 12 * 3 + 6 *2,
                       each.once = 4,
@@ -790,20 +995,6 @@ if(force.refresh.transformed.data | !exists('transformed.data')) {
                                                      ifelse(FALSE & control$testing, 1000, -1),
                                                      TRUE)  # TRUE --> verbose
 }
-CvAll <- function(control, transformed.data) {
-    # cross validate all models to select best model
-}
-
-CvEachOne <- function(control, transformed.data) {
-    # run each model once, to check syntax
-}
-
-CvRandomForestnTrees <- function(control, transformed.data) {
-    # cv to select best number of trees in random forest models
-}
-
-
-
 
 
 Main(control, 
