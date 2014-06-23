8e4789dd08455b707117ffcfc6b1df8baa943373
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-23 11:08:55 -0400

    new: source file if corresponding function not defined

---
+++ Require.R
@@ -0,0 +1,7 @@
+Require <- function(functionName) {
+    # source a file if a function is not defined
+    # ex: Require('Myfunction') --> source('Myfunction.R') if !exists(Myfunction, mode = 'function')
+    if (!exists(functionName, mode = 'funcion')) {
+        source(paste0(functionName, '.R'))
+    }
+}
