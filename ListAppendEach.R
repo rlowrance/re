55533fe68f2689957160ca523b79257729b19321
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-29 13:15:16 -0400

    new: move list names to top level

--- ListAppendEach.R
+++ ListAppendEach.R
@@ -1,7 +1,7 @@
-AppendEach <- function(x, values) {
+ListAppendEach <- function(x, values) {
     # append named elements in values to named elements in x
-    # ex: AppendEach(NULL, list(a=1,b=2)) --> list(a=1,b=2)
-    # ex: AppendEach(list(a=1,b=2), list(a=10,b=20)) --> list(a=list(1,10), b=list(2,20))
+    # example: AppendEach(NULL, list(a=1,b=2)) --> list(a=1,b=2)
+    # example: AppendEach(list(a=1,b=2), list(a=10,b=20)) --> list(a=list(1,10), b=list(2,20))
     if (is.null(x)) {
         values
     } else {
@@ -12,11 +12,11 @@ AppendEach <- function(x, values) {
     }
 }
 
-AppendEach.test <- function() {
+ListAppendEach.test <- function() {
     x <- NULL
     v1 <- list(a=1, b=2)
     v2 <- list(a=10, b=20)
-    xx <- AppendEach(AppendEach(x, v1), v2)
+    xx <- ListAppendEach(LisppendEach(x, v1), v2)
     n <- names(xx)
     stopifnot(n[1] == 'a')
     stopifnot(n[2] == 'b')
@@ -24,4 +24,4 @@ AppendEach.test <- function() {
     stopifnot(length(xx$b) == 2)
 }
 
-AppendEach.test()
+ListAppendEach.test()
