3b3564abbdaaa9a19d72b1944e27460dcce6c7f2
Roy Lowrance <roy.lowrance@gmail.com>
2014-06-30 09:56:47 -0400

    no longer use explicit param

--- CompareModelsCv01.R
+++ CompareModelsCv01.R
@@ -1,80 +1,72 @@
-CompareModelsCv01 <- function(control, transformed.data) {
-    # implement cross validation for assessor linear model for log.price using chopra's predictors
+CompareModelsCv01 <- function(testing.period, transformed.data) {
+    # define models for experiment: assessor linear logprice chopra
     # ARGS
-    # control          : list of control variables
+    # testing.period   : list($first.date,$last.date) list of Date 
+    #                    first and last dates for the testing period
     # transformed.data : data.frame
-    # RETURN list($result = <result from CrossValidate3>, description = chr scalar)
-    cat('starting CompareModelsCv01', control$nfolds, nrow(transformed.data), '\n'); browser()
+    # RETURN list(Model=<list of functions>, description=<list of char vector>)
+    #   2 parallel lists where
+    #   Model[[i]]: is a function(data, training.indices, testing.indices)
+    #               --> $actual = <vector of actual prices for the testing period>
+    #                   $predicted <vector of predicted prices or NA values> for corresponding transactions
+    #   description[[i]]: a vector of lists of chr, a description of the model
+    #cat('starting CompareModelsCv01', testing.period$first.date, testing.period$last.date, nrow(transformed.data), '\n'); browser()
     
-    Require('Assess')
-    Require('CrossValidate3')
     Require('ModelLinear')
     Require('PredictorsChopraCenteredLog')
 
     base.description <- list(scenario = 'assessor',
-                             testing.period = control$testing.period,
+                             testing.period = testing.period,
                              model = 'ModelLinear',
                              response = 'log.price',
                              predictors = 'Chopra centered log')
+    features <- list(response = 'log.price',
+                     predictors = PredictorsChopraCenteredLog())
 
-    Model.param.description <- function(model.index) {
-        # return list $Model $param which will be called by 
-        # CV3 via $Model(data, training.indices, testing.indices, param)
-        #cat('starting CvAssessorLinearLogPriceChopra::Model.param.description', model.index, '\n'); browser()
-        testing.period <- control$testing.period
-        features <- list(response = 'log.price',
-                         predictors = PredictorsChopraCenteredLog())
-        verbose.model <- TRUE
-        Model <- function(data, training.indices, testing.indices, param) {
-            # return evaluations (which is a list of scalars)
-            if (FALSE) {
-                cat('starting Cv01::Model', 
-                    nrow(data), sum(training.indices), sum(testing.indices),
-                    param$training.period$first.date, param$training.period$last.date,
-                    '\n')
-                browser()
-            }
-            actuals.predictions <- 
-                ModelLinear(data = data,
-                            training.indices = training.indices,
-                            testing.indices = testing.indices,
-                            scenario = 'assessor',
-                            training.period = param$training.period,
-                            testing.period = testing.period,
-                            features = features,
-                            verbose.model = TRUE)
-            result <- Assess(actuals = actuals.predictions$actuals,
-                             predictions = actuals.predictions$predictions)
-            result
-        }
 
+    MakeModelDescription <- function(model.index) {
+        # return list $Model $description
+        #cat('starting MakeModelDescription', model.index, '\n'); browser()
+
+        # determine training period
         assessor.mailing.date <- as.Date('2008-10-1')
         last.assessor.analysis.date <- assessor.mailing.date - 1
         days.before <- 30 * model.index
         training.period <- list(first.date = last.assessor.analysis.date - days.before,
                                 last.date = last.assessor.analysis.date)
-        param <- list(training.period = training.period)
-        model.description <- c(base.description,
-                               training.period = sprintf('%d days before Oct 1', days.before))
 
-        result <- list(Model = Model,
-                       param = param,
-                       description = model.description)
+        Model <- function(data, training.indices, testing.indices) {
+            # return $actual $prediction
+            # use training.period set just above
+            if (FALSE) {
+                cat('starting Cv01::Model', 
+                    nrow(data), sum(training.indices), sum(testing.indices),
+                    training.period$first.date, training.period$last.date,
+                    '\n')
+                browser()
+            }
+            # training.period <- params (but I want to me explicit)
+            verbose.model <- TRUE
+            actual.prediction <- 
+                ModelLinear(data = data,
+                            training.indices = training.indices,
+                            testing.indices = testing.indices,
+                            scenario = 'assessor',
+                            training.period = training.period,
+                            testing.period = testing.period,
+                            features = features,
+                            verbose.model = verbose.model)
+            stopifnot(length(actual.prediction) == 2)
+            actual.prediction
+        }
+
+        description <- c(base.description,
+                         training.period = sprintf('%d days before Oct 1', days.before))
+
+        result <- list(Model = Model, description = description)
         result
     }
 
-    mpd <- lapply(1:10, Model.param.description)
-
-    models.params <- lapply(mpd,
-                            function(x) list(Model = x$Model, param = x$param))
-    descriptions <- lapply(mpd,
-                           function(x) x$description)
-
-    cv.result <- CrossValidate3(data = transformed.data,
-                                nfolds = control$nfolds,
-                                models.params = models.params,
-                                verbose = TRUE)
-    result <- list(cv.result = cv.result,
-                   descriptions = descriptions)
+    result <- lapply(1:10, MakeModelDescription)
     result
 }
