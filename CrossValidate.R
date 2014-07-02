38ccb0e06739c906d2529a159eb5f2d697336603
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-02 17:11:45 -0400

    use revised Assess API

--- CrossValidate.R
+++ CrossValidate.R
@@ -9,7 +9,7 @@ CrossValidate <- function(data, nfolds, Models, Assess, verbose) {
     #                  a list with elements $actual and $prediction
     #                  where $actual     : numeric vector of values in the training set (never NA)
     #                        $prediction : numeric vector of predicts (possibly NA)
-    # Assess         : function(actual, prediction) --> list <error rates>
+    # Assess         : function(<Models[[i]] returned value>) --> list <error rates>
     #                  yields evaluations of the actual and prediction results from a model
     #                  the best model is the one with the lowest <error rates>[[1]]
     #                  all <error rates> are returned in the data.frame all.result
@@ -18,7 +18,7 @@ CrossValidate <- function(data, nfolds, Models, Assess, verbose) {
     # $best.model.index : index i of model with lowest error.rate
     # $all.assessment   : data.frame with $fold, $model.index, $error.rate $assessment.<Assess result name>
 
-    cat('starting CrossValidate', nrow(data), nfolds, length(Models), '\n'); browser()
+    #cat('starting CrossValidate', nrow(data), nfolds, length(Models), '\n'); browser()
 
     stopifnot(nfolds <= nrow(data))
 
@@ -47,20 +47,21 @@ CrossValidate <- function(data, nfolds, Models, Assess, verbose) {
             }
             Model <- Models[[this.model.index]]
             model.result <- Model(data, is.training, is.testing)
-
-            actual <- model.result$actual
-            prediction <- model.result$prediction
-            this.assessment <- Assess(actual = actual, prediction = prediction)
+            this.assessment <- Assess(model.result)
 
             # the first assessment is the one we use to decide the best model
             #cat('in CV, checking assessment\n'); browser()
             this.error.rate <- this.assessment[[1]]  
+            stopifnot(length(this.error.rate) == 1)
             if (verbose) {
                 cat(sprintf('CrossValidate: error rate on model %d fold %d is %f\n',
                             this.model.index, this.fold, this.error.rate))
             }
 
             # accumlate results
+            # NOTE: we through away the model.result, relying on the Assess function to
+            # reduce the model.result to a list of scalar values
+            #cat('in CrossValidate, accumulate results\n'); browser()
             all.fold <- c(all.fold, this.fold)
             all.model.index <- c(all.model.index, this.model.index)
             all.error.rate <- c(all.error.rate, this.error.rate)
@@ -70,6 +71,7 @@ CrossValidate <- function(data, nfolds, Models, Assess, verbose) {
 
     # create data frame with all results for all models and folds
     #cat('in CV: building all.results\n'); browser()
+
     fold.assessment <- data.frame(fold = all.fold,
                                   model.index = all.model.index,
                                   error.rate = all.error.rate,
@@ -151,13 +153,14 @@ CrossValidate.test <- function() {
         result
     }
 
-    Assess <- function(actual, prediction) {
+    Assess <- function(model.result) {
         #cat('entering Assess\n'); browser()
         if (verbose) {
             print('Assess')
-            print(actual)
-            print(prediction)
+            print(model.result)
         }
+        actual <- model.result$actual
+        prediction <- model.result$prediction
         result <- list(error.rate = MeanAbsError(actual = actual, prediction = prediction),
                        coverage = Coverage(actual = actual, prediction = prediction))
         if (verbose) {print(result$error.rate)}
