f9eeaa6d44da7bb653ed6341618e7ef992854ba6
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-02 17:16:06 -0400

    change arg name
    Add unit tests

--- WithinXPercent.R
+++ WithinXPercent.R
@@ -1,25 +1,68 @@
-WithinXPercent <- function(actual, estimated, precision = .10) {
+WithinXPercent <- function(actual, prediction, precision = .10) {
     # return fraction of estimates that are within (say) 10 percent of the actuals
-    #cat('starting WithinXPercent', length(actual), length(estimated), precision, '\n'); browser()
-    stopifnot(length(actual) == length(estimated))
-    stopifnot(length(actual) > 0)
-    stopifnot(sum(actual == 0) == 0)  # at least one actual is zero
-    na.indices <- is.na(actual) | is.na(estimated)  # find NAs in either arg
-    actual.ok <- actual[!na.indices]
-    estimated.ok <- estimated[!na.indices]
-    error <- actual.ok - estimated.ok
-    relative.error <- error / actual.ok
-    result <- sum(abs(relative.error) <= precision) / length(actual.ok)
+    #cat('starting WithinXPercent', length(actual), length(prediction), precision, '\n'); browser()
+    if (is.null(actual)) {
+        # no actuals are known
+        result <- 1
+    } else {
+        stopifnot(length(actual) == length(prediction))
+        stopifnot(length(actual) > 0)
+        na.indices <- is.na(actual) | is.na(prediction)  # find NAs in either arg
+        if (sum(na.indices) == length(actual)) {
+            # no comparisons are possible
+            result <- 1
+        } else {
+            actual.ok <- actual[!na.indices]
+            prediction.ok <- prediction[!na.indices]
+            error <- actual.ok - prediction.ok
+            relative.error <- error / actual.ok
+            result <- sum(abs(relative.error) <= precision) / length(actual.ok)
+        }
+    }
     result
 }
 
 WithinXPercent.test <- function() {
-    actual <- c(1,2,3)
-    estimated <- c(1.05, 20, NA)
-    within <- WithinXPercent(actual, estimated)
-    #cat('within', within, '\n')
-    stopifnot(length(within) == 1)
-    stopifnot(within == 1/2)
+    Test1 <- function() {
+        actual <- c(1,2,3)
+        prediction <- c(1.05, 20, NA)
+        within <- WithinXPercent(actual, prediction)
+        stopifnot(length(within) == 1)
+        stopifnot(within == 1/2)
+    }
+    Test1()
+
+    Test2 <- function() {
+        actual = c(1,1,1,1,1)
+        prediction = c(1,2,.5,1.09,.91)
+        within <- WithinXPercent(actual, prediction)
+        stopifnot(within == .6)
+    }
+    Test2()
+
+    Test3 <- function() {
+        actual = c(NA, 20, 30)
+        prediction = c(10, 21, NA)
+        within <- WithinXPercent(actual, prediction)
+        stopifnot(within == 1)
+    }
+    Test3()
+
+    Test4 <- function() {
+        actual = c(NA, NA)
+        prediction = c(1, 2)
+        within <- WithinXPercent(actual, prediction)
+        stopifnot(within == 1)
+    }
+    Test4()
+
+    Test5 <- function() {
+        actual <- NULL
+        prediction <- c(1)
+        within <- WithinXPercent(actual, prediction)
+        stopifnot(within == 1)
+    }
+    Test5()
 }
 
 WithinXPercent.test()
