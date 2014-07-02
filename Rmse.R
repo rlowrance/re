50f5748749f0ef3684e7298d39eda146c697ed0a
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-02 17:14:14 -0400

    clean up code

--- Rmse.R
+++ Rmse.R
@@ -1,20 +1,20 @@
-Rmse <- function(actual, estimated) {
+Rmse <- function(actual, prediction) {
     # return square root of mean squared error
-    #cat('starting Rmse', length(actual), length(estimated), '\n'); browser()
-    stopifnot(length(actual) == length(estimated))
+    #cat('starting Rmse', length(actual), length(prediction), '\n'); browser()
+    stopifnot(length(actual) == length(prediction))
     stopifnot(length(actual) > 0)
-    na.indices <- is.na(actual) | is.na(estimated)
+    na.indices <- is.na(actual) | is.na(prediction)
     actual.ok <- actual[!na.indices]
-    estimated.ok <- estimated[!na.indices]
-    error <- actual.ok - estimated.ok
+    prediction.ok <- prediction[!na.indices]
+    error <- actual.ok - prediction.ok
     mse <- sum(error * error) / length(actual.ok)
     sqrt(mse)
 }
 
 Rmse.test <- function() {
     actual <- c(10, 20, 30, 40)
-    estimated <- c(11, 21, 31, NA)
-    mse <- Rmse(actual, estimated)
+    prediction <- c(11, 21, 31, NA)
+    mse <- Rmse(actual, prediction)
     stopifnot(mse == 1)
 }
 
