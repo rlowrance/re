9279f85055dbeb0b1121062e034b780c92970c75
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-06 15:46:11 -0400

    Require() called function

--- PredictorsChopraCenteredLogMortgage.R
+++ PredictorsChopraCenteredLogMortgage.R
@@ -1,3 +1,4 @@
+Require('PredictorsChopraCenteredLogAvm')
 PredictorsChopraCenteredLogMortgage <- function() {
     # return chr vector of predictors used by Chopra that we have access to
     # NOTE: Chopra had school district quality, which we don't have
