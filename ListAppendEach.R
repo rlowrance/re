aee648368adf9069de8f6d685efe2f3cb08c63b4
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-10 13:11:25 -0400

    handle case where first arg is null

--- ListAppendEach.R
+++ ListAppendEach.R
@@ -2,6 +2,7 @@ ListAppendEach <- function(x, values) {
     # append named elements in values to named elements in x
     # example: ListAppendEach(NULL, list(a=1,b=2)) --> list(a=1,b=2)
     # example: ListAppendEach(list(a=1,b=2), list(a=10,b=20)) --> list(a=list(1,10), b=list(2,20))
+    #cat('starting ListAppendEach\n'); browser()
     if (is.null(x)) {
         values
     } else {
@@ -13,15 +14,31 @@ ListAppendEach <- function(x, values) {
 }
 
 ListAppendEach.test <- function() {
-    x <- NULL
-    v1 <- list(a=1, b=2)
-    v2 <- list(a=10, b=20)
-    xx <- ListAppendEach(ListAppendEach(x, v1), v2)
-    n <- names(xx)
-    stopifnot(n[1] == 'a')
-    stopifnot(n[2] == 'b')
-    stopifnot(length(xx$a) == 2)
-    stopifnot(length(xx$b) == 2)
+    TestAppend1 <- function() {
+        x <- NULL
+        v1 <- list(a = 1)
+        v2 <- list(a = 2)
+        xx <- ListAppendEach(ListAppendEach(x, v1), v2)
+        n <- names(xx)
+        stopifnot(n[1] == 'a')
+        stopifnot(length(n) == 1)
+        stopifnot(length(xx$a) == 2)
+    }
+    TestAppend1()
+
+    TestAppend2 <- function() {
+        x <- NULL
+        v1 <- list(a=1, b=2)
+        v2 <- list(a=10, b=20)
+        xx <- ListAppendEach(ListAppendEach(x, v1), v2)
+        n <- names(xx)
+        stopifnot(n[1] == 'a')
+        stopifnot(n[2] == 'b')
+        stopifnot(length(n) == 2)
+        stopifnot(length(xx$a) == 2)
+        stopifnot(length(xx$b) == 2)
+    }
+    TestAppend2()
 }
 
 ListAppendEach.test()
