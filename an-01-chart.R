04c72d7b6ac6097199c6cf099b91194286aff995
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-15 09:46:54 -0400

    new: analysis 1 in its own source file

---
+++ an-01-chart.R
@@ -0,0 +1,82 @@
+# main program to produce charts for from an-01.rsave
+# write files OUTPUT/an-01-chart-1.rsave
+#                    an-01-chart-log.txt
+
+source('FileInput.R')
+source('FileOutput.R')
+source('InitializeR.R')
+source('LoadColumns.R')
+
+library(ggplot2)
+
+# declare input and output files
+# NOTE: these declarations are used in part to form the makefile
+control <- list( testing = FALSE
+                ,show.graph = FALSE
+                ,path.in         = FileInput ('../data/v6/output/an-01.rsave')
+                ,path.out.result = FileOutput('../data/v6/output/an-01-chart.pdf')
+                ,path.out.log    =            '../data/v6/output/an-01-chart-log.txt'
+                )
+
+InitializeR(duplex.output.to = control$path.out.log)
+
+print('control variables')
+print(control)
+
+
+Main <- function(control) {
+    #cat('starting Main\n'); browser()
+
+    loaded.vars <- load(control$path.in)
+    stopifnot(length(loaded.vars) == 1)
+    stopifnot(loaded.vars[[1]] == 'analysis')
+
+    # make a Cleveland dot plot (see R Graphics Cookbook p 44)
+   
+    month.str <- sprintf('%04d-%02d', analysis$sale.year, analysis$sale.month)
+    month.factor <- factor( month.str
+                           ,levels = sort(month.str, decreasing = TRUE)
+                           )
+
+    data <- data.frame( median.price = analysis$median.price
+                       ,month = month.factor
+                       )
+
+    gg <- ggplot( data
+                 ,aes(x = median.price, y = month))
+
+    assumed.max.median.price = 800000
+    g <- 
+        gg +
+        geom_point(size = 3) +
+        xlim(0, assumed.max.median.price) +
+        theme_bw() +
+        theme( panel.grid.major.x = element_blank()
+              ,panel.grid.minor.x = element_blank()
+              ,panel.grid.major.y = element_blank()
+              )
+
+    width <- 14
+    height <- 10
+
+    if (control$show.graph) {
+        X11(width = width, height = height)
+        print(g)
+    }
+
+    pdf( file = control$path.out.result
+        ,width = width
+        ,height = height
+        )
+    print(g)
+    dev.off()
+
+    NULL
+}
+
+Main(control)
+
+print('control variables')
+print(control)
+
+cat('done')
---
+++ an-01.R
@@ -0,0 +1,69 @@
+# main program to run analysis 01 (median price by month)
+# write files OUTPUT/an-01.rsave
+#                    an-01-log.txt
+
+source('FileInput.R')
+source('FileOutput.R')
+source('InitializeR.R')
+source('LoadColumns.R')
+
+# declare input and output files
+# NOTE: these declarations are used in part to form the makefile
+control <- list( testing = FALSE
+                ,path.in         = FileInput ('../data/v6/output/transactions-subset1.csv.gz')
+                ,path.out.result = FileOutput('../data/v6/output/an-01.rsave')
+                ,path.out.log    =            '../data/v6/output/an-01-log.txt'
+                )
+
+InitializeR(duplex.output.to = control$path.out.log)
+
+print('control variables')
+print(control)
+
+
+Main <- function(control) {
+    #cat('starting Main\n'); browser()
+
+    data <- LoadColumns( path.base = '../data/v6/output/transactions-subset1'
+                        ,column.names = c('price', 'sale.year', 'sale.month')
+                        )
+
+    MedianPrice <- function(year, month) {
+        # return median price in data fro year and month
+        #cat('starting MedianPrice\n', year, month, '\n'); browser()
+        selected <- data$sale.year == year & data$sale.month == month
+        price <- data[selected, 'price']
+        median.price <- median(price, na.rm = TRUE)
+        median.price
+    }
+
+    # build up data.frame row by row
+    analysis <- NULL
+
+    for (sale.year in 2006:2009) {
+        last.sale.month <- ifelse(sale.year == 2009, 11, 12)
+        for (sale.month in 1:last.sale.month) {
+            median.price <- MedianPrice(sale.year, sale.month)
+            next.row <- data.frame( stringsAsFactors = FALSE
+                                   ,sale.year = sale.year
+                                   ,sale.month = sale.month
+                                   ,median.price = median.price
+                                   )
+            analysis <- rbind(analysis, next.row)
+        }
+    }
+
+    print('analysis')
+    print(analysis)
+
+    save(analysis, file = control$path.out.result)
+
+    NULL
+}
+
+Main(control)
+
+print('control variables')
+print(control)
+
+cat('done')
