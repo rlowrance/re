ace965e4846708246fd7c984e70702ff0666c515
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-28 15:10:17 -0400

    bad edit, file to be deleted

--- PredictorsChopraCenteredLevelAvm.R
+++ PredictorsChopraCenteredLevelAvm.R
@@ -1,7 +1,7 @@
-PredictorsChopraCenteredLevelAvm <- function() {
+PredictorsChopraLevelAvm <- function() {
     # return chr vector of predictors used by Chopra that we have access to
     # NOTE: Chopra had school district quality, which we don't have
-    c(PredictorsChopraCenteredLevelAssessor(),
-      'centered.land.value',
-      'centered.improvement.value')
+    c(PredictorsChopraLevelAssessor(),
+      'land.value',
+      'improvement.value')
 }
