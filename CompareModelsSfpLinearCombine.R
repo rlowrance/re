c0ee8c396018af6838d5c58e69b33b7102686275
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-28 10:22:45 -0400

    use correct name for saved variable

--- CompareModelsSfpLinearCombine.R
+++ CompareModelsSfpLinearCombine.R
@@ -6,15 +6,19 @@ CompareModelsSfpLinearCombine <- function( control
                                           ) {
     # combine shards OUTPUT/compare-models-shard-SCENARIO-RESPONSE-PREDICTORS.rsave
     # into           OUTPUT/compare-models-combined.rsave
-    cat('starting CompareModelsSfpLinearCombine\n'); browser()
+    #cat('starting CompareModelsSfpLinearCombine\n'); browser()
+    testing <- FALSE
+    if (testing) {
+        num.testing.periods <- 12
+    }
 
     all.rows <- NULL
     for (testing.period.index in 1:num.testing.periods) {
         path.in <- PathShard(control, testing.period.index)
         names.loaded <- load(path.in)
         stopifnot(length(names.loaded) == 1)
-        stopifnot(names.loaded[[1]] == 'data')
-        all.rows <- IfThenElse(is.null(all.rows), data, rbind(all.rows, data))
+        stopifnot(names.loaded[[1]] == 'all.row')
+        all.rows <- IfThenElse(is.null(all.rows), all.row, rbind(all.rows, all.row))
     }
 
     cat('Combine about to write\n'); browser()
