775c6ee1080e894b3675cd40f43b689b1e11b91d
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-28 15:10:44 -0400

    also refactored into Predictors.R

--- PredictorsChopraCenteredLevelAvm.R
+++
@@ -1,7 +0,0 @@
-PredictorsChopraLevelAvm <- function() {
-    # return chr vector of predictors used by Chopra that we have access to
-    # NOTE: Chopra had school district quality, which we don't have
-    c(PredictorsChopraLevelAssessor(),
-      'land.value',
-      'improvement.value')
-}
