34bc07b333d03903415053034a6ae16ed70e767d
Roy Lowrance <roy.lowrance@gmail.com>
2014-07-29 21:24:34 -0400

    refactor to make super clear

--- Predictors.R
+++ Predictors.R
@@ -1,78 +1,88 @@
 source('IfThenElse.R')
-Predictors <- function(set, form, center) {
+Predictors <- function(set, form, center, useAssessment) {
     # return set of predictors
     # ARGS
-    # set : always 'Chopra' for now
+    # set : always 'Cjjjhopra' for now
     # form : 'log' or 'level'
     # center : TRUE or FALSE
+    # useAssessment: TRUE or FALSE
 
-    Prefix <- function(chr, a.list) {
-        PrefixChr <- function(x) {
-            sprintf('%s.%s', chr, x)
-        }
-        result <- sapply(a.list, PrefixChr)
-        result
+    # categorization of features
+    # - continuous
+    #   -- size: for size features, the caller may want to use the log of the feature
+    #      --- positive (>0), so that taking log is possible
+    #      --- nonnegative (>=0), so that taking log is not possible, but can take log(x+1)
+    #   -- non-size
+    # - discrete
+
+    # These function all accept a feature name (as a string) and prepend centerd and log/log1p as appropriate
+
+    ContinuousSizePositive <- function(chr) {
+        v <- switch( form
+                    ,log = sprintf('log.%s', chr)
+                    ,level = chr
+                    )
+        IfThenElse(center, sprintf('centered.%s', v), v)
     }
 
-    MaybeCenter <- function(vars) {
-        IfThenElse(center, Prefix('centered', vars), vars)
+    ContinuousSizeNonnegative <- function(chr) {
+        v <- switch( form
+                    ,log = sprintf('log1p.%s', chr)
+                    ,level = chr
+                    )
+        IfThenElse(center, sprintf('centered.%s', v), v)
     }
 
-    MaybeLog <- function(vars) {
-        switch( form
-               ,log = Prefix('log', vars)
-               ,level = vars
-               ,stop('bad form')
-               )
+    ContinuousNonsize <- function(chr) {
+        IfThenElse(center, sprintf('centered.%s', chr), chr)
     }
-        
 
-    MaybeCenterLog <- function(vars) {
-        MaybeCenter(MaybeLog(vars))
+    Discrete <- function(chr) {
+        chr
     }
 
     #cat('starting Predictors\n', set, form, center, '\n'); browser()
 
     stopifnot(set == 'Chopra')
 
-    continuous.size.vars <- c( 'land.square.footage'
-                        ,'living.area'
-                        ,'bedrooms'
-                        ,'bathrooms'
-                        ,'parking.spaces'
-                        ,'median.household.income'
-                        )
+    chopra <- c( ContinuousSizePositive('land.square.footage')
+                ,ContinuousSizePositive('living.area')
+                ,ContinuousSizeNonnegative('bedrooms')
+                ,ContinuousSizeNonnegative('bathrooms')
+                ,ContinuousSizeNonnegative('parking.spaces')
+                ,IfThenElse(useAssessment, ContinuousSizePositive('improvement.value'), NULL)
+                ,IfThenElse(useAssessment, ContinuousSizePositive('land.value'), NULL)
+                ,ContinuousNonsize('median.household.income')
+                ,ContinuousNonsize('year.built')
+                ,ContinuousNonsize('fraction.owner.occupied')
+                ,ContinuousNonsize('avg.commute.time')
+                ,ContinuousNonsize('latitude')
+                ,ContinuousNonsize('longitude')
+                ,IfThenElse(useAssessment, ContinuousNonsize('fraction.improvement.value'), NULL)
+                ,Discrete('factor.is.new.construction')
+                ,Discrete('factor.has.pool')
+                )
 
-    continuous.non.size.vars <- c( 'year.built'
-                            ,'fraction.owner.occupied'
-                            ,'avg.commute.time'
-                            ,'latitude'
-                            ,'longitude'
-                            )
+    chopra
 
-    discrete.vars <- c('is.new.construction'
-                       ,'has.pool'
-                       )
-
-    all.vars <- c( MaybeCenterLog(continuous.size.vars)
-                  ,MaybeCenter(continuous.non.size.vars)
-                  ,discrete.vars)
-    all.vars
 }
 
 Predictors.test <- function() {
-    verbose <- TRUE
+    verbose <- FALSE
     V <- function(chr) {
         if (verbose) {
             cat(chr, '\n')
         }
     }
 
-    cat('starting Predictors.test\n'); browser()
-    V(Predictors('Chopra', 'log', FALSE))
-    V(Predictors('Chopra', 'log', TRUE))
-    V(Predictors('Chopra', 'level', FALSE))
-    V(Predictors('Chopra', 'level', TRUE))
+    #cat('starting Predictors.test\n'); browser()
+    p <- Predictors(set = 'Chopra', form='log', center=TRUE, useAssessment=TRUE)
+    if (verbose) print(p)
+    stopifnot(length(p) == 16)
+
+    p <- Predictors(set = 'Chopra', form='level', center=FALSE, useAssessment=FALSE)
+    if (verbose) print(p)
+    stopifnot(length(p) == 13)
 }
 
 Predictors.test()
