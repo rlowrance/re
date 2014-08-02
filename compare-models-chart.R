4e976f49bdc991538b34f31d1589b5e4e37725c2
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-02 13:18:34 -0400

    change name of sfpLinear driver

--- compare-models-chart.R
+++ compare-models-chart.R
@@ -14,6 +14,7 @@ Require('CommandArgs')
 
 Require('CompareModelsChartBmtp')
 Require('CompareModelsChartCv')
+Require('CompareModelsChartSfpLinear')
 
 Require('InitializeR')
 Require('ParseCommandLine')
@@ -68,46 +69,6 @@ AugmentControlVariables <- function(control) {
 
 
 
-SfpLinearChart01 <- function(all.rows, path.out.base) {
-    # write csv file
-    cat('starting SpfLinearChart01\n'); browser()
-    path.out <- paste0(path.out.base, '.csv')
-    write.csv( all.rows
-              ,file=path.out)
-}
-
-SfpLinearChart <- function(control, all.rows) {
-    # write file OUTPUT/compare-models-chart-sfplinear-NN.KIND
-    # where NN is control$choice
-
-    path.out.base <- paste0(control$dir.output, control$me, '-sfplinear-', control$choice)
-    switch( control$choice
-           ,'01' = SfpLinearChart01(all.rows, path.out.base)
-           ,stop('bad control$choice')
-           )
-}
-
-SfpLinear <- function(control) {
-    ReadAllRows <- function(path) {
-        # return all.rows data frame in saved file
-        #cat('starting ReadAllRows', path, '\n'); browser()
-        variables.loaded <- load(path)
-        stopifnot(length(variables.loaded) == 1)
-        stopifnot(variables.loaded[[1]] == 'all.rows')
-        all.rows
-    }
-
-    cat('starting SfpLinear\n'); browser()
-    verbose <- TRUE
-    path.combine <- paste0(control$dir.output, 'compare-models-sfplinear-combine.rsave')
-    all.rows <- ReadAllRows(path.combine)
-    if (verbose) {
-        print(str(all.rows))
-    }
-    result <- SfpLinearChart(control, all.rows)
-    result
-}
-
 
 
 Main <- function(control) {
@@ -117,7 +78,7 @@ Main <- function(control) {
     driver <- switch( control$what
                      ,cv = CompareModelsChartCv
                      ,bmtp = CompareModelsChartBmtp
-                     ,sfpLinear = SfpLinear
+                     ,sfpLinear = CompareModelsChartSfpLinear
                      ,stop('bad control$what')
                      )
     driver(control)
@@ -130,8 +91,8 @@ Main <- function(control) {
 
 # handle command line and setup control variables
 #command.args <- CommandArgs(defaultArgs = list('--what', 'cv', '--choice', '01'))
-command.args <- CommandArgs(defaultArgs = list('--what', 'bmtp', '--choice', 'assessor'))
-#command.args <- CommandArgs(defaultArgs = list('--what', 'sfpLinear', '--choice', '01'))
+#command.args <- CommandArgs(defaultArgs = list('--what', 'bmtp', '--choice', 'assessor'))
+command.args <- CommandArgs(defaultArgs = list('--what', 'sfpLinear', '--choice', '01'))
 
 control <- AugmentControlVariables(ParseCommandLineArguments(command.args))
 
