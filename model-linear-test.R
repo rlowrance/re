ccced573b8a8ca25b8ec4b1937c923d30f56a6c5
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-06 11:10:37 -0400

    new: early version, regression test of ModelLinear

---
+++ model-linear-test.R
@@ -0,0 +1,130 @@
+# model-linear-test.R
+# main program to test ModelLinear
+# Approach: use synthetic data
+
+source('Assess.R')
+source('DataSynthetic.R')
+source('InitializeR.R')
+source('MakeModelLinear.R')
+
+PrefixLog <- function(var) {
+    #cat('start PrefixLog', var, '\n'); browser()
+    result <- sprintf('log.%s', var)
+    result
+}
+
+TransformSizeToLog <- function(vars) {
+    #cat('start TransformSizeToLog\n'); browser()
+    Transform <- function(var) {
+        IfThenElse(var == 'land.size', PrefixLog(var), var)
+    }
+    result <- Map(Transform, vars)
+    result
+}
+
+ModelAssessorLevelLevel <- function(data, testing.period, response, predictors, num.training.days) {
+    #cat('start MakeAssessorLogLevel\n'); browser()
+    ModelCv <- MakeModelLinear( scenario = 'assessor'
+                               ,testing.period = testing.period
+                               ,data = data
+                               ,num.training.days = num.training.days
+                               ,response = response
+                               ,predictors = predictors
+                               ,verbose.model = FALSE
+                               )
+    ModelCv
+}
+
+ModelAssessorLogLevel <- function(data, testing.period, response, predictors, num.training.days) {
+    #cat('start MakeAssessorLogLevel\n'); browser()
+    ModelCv <- MakeModelLinear( scenario = 'assessor'
+                               ,testing.period = testing.period
+                               ,data = data
+                               ,num.training.days = num.training.days
+                               ,response = PrefixLog(response)
+                               ,predictors = predictors
+                               ,verbose.model = FALSE
+                               )
+    ModelCv
+}
+
+ModelAvmLevelLevel <- function(data, testing.period, response, predictors, num.training.days) {
+    #cat('start MakeAssessorLogLevel\n'); browser()
+    ModelCv <- MakeModelLinear( scenario = 'avm'
+                               ,testing.period = testing.period
+                               ,data = data
+                               ,num.training.days = num.training.days
+                               ,response = response
+                               ,predictors = predictors
+                               ,verbose.model = FALSE
+                               )
+    ModelCv
+}
+
+ModelResult <- function(ModelMaker, testing.period, response, predictors, num.training.days, data) {
+    ModelCv <- ModelMaker( data = data
+                          ,testing.period = testing.period
+                          ,response = response
+                          ,predictors = predictors
+                          ,num.training.days = num.training.days
+                          )
+
+    training.indices <- 1:nrow(data)
+    testing.indices <- 1:nrow(data)
+    model.result <- ModelCv( data = data
+                            ,training.indices = training.indices
+                            ,testing.indices = testing.indices
+                            )
+    model.result
+}
+
+
+Main <- function() {
+    #cat('start Main\n'); browser()
+    first.date <- as.Date('2007-01-01')
+    last.date <- as.Date('2008-12-31')
+    obs.per.day <- 10
+    obs.per.day <- 1; cat('TESTING\n')
+    ds <- DataSynthetic( obs.per.day = obs.per.day
+                        ,first.date = first.date
+                        ,last.date = last.date
+                        )
+    data <- ds$data
+    coefficients <- ds$coefficients
+
+    cat('in Main\n'); browser()
+    testing.period <- list(first.date = as.Date('2008-01-01'), last.date = as.Date('2008-01-31'))
+    response = 'price'
+    predictors = c('recordingDate', 'land.size', 'latitude', 'has.pool')
+    num.training.days <- 60
+    
+    Run <- function(name, ModelMaker) {
+        cat('start Run\n'); browser()
+        model.result <- ModelResult( ModelMaker = ModelMaker
+                                    ,testing.period = testing.period
+                                    ,response = response
+                                    ,predictors = predictors
+                                    ,num.training.days = num.training.days
+                                    ,data = data
+                                    )
+        assess <- Assess(model.result)
+        fitted <- model.result$fitted
+
+        cat('\n************** Result for', name, '\n')
+        cat('mean price', mean(data$price), '\n')
+        print(str(assess))
+        #print(model.result)
+        print(summary(fitted))
+        print('Actual coefficients\n'); print(coefficients)
+    }
+
+    #Run('assessor level level', ModelAssessorLevelLevel)
+    Run('avm level level', ModelAvmLevelLevel)
+    #Run('assessor log level', ModelAssessorLogLevel)
+
+    cat('end of Main\n'); browser()
+    # examine coefficients of trained model
+}
+
+InitializeR()
+Main()
