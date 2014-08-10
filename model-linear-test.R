768d7a4e7f275a2ec32cbf675c8c4360101ed8fc
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-10 14:11:52 -0400

    refactor to use common print function

--- model-linear-test.R
+++ model-linear-test.R
@@ -6,6 +6,7 @@ source('Assess.R')
 source('DataSynthetic.R')
 source('InitializeR.R')
 source('MakeModelLinear.R')
+source('ModelLinearTestPrintAllResults.R')
 source('Printf.R')
 
 Main <- function() {
@@ -153,54 +154,14 @@ Main <- function() {
                   ,NameAndModel('mortgage', 'level', 'logAssessment')
                   )
             )
+
+    # save to output
+    print('all.results\n')
+    print(all.results)
+    save(all.results, file='../data/V6/output/model-linear-test.rsave')
+
     PrintResult <- function(result) {
         Printf('use case: %40s  RMSE: %.0f\n', result$name, result$rmse)
-    }
-
-    PrintTable <- function(all.results) {
-        #cat('start PrintTable\n'); browser()
-        cat('RMSE\n')
-
-        format.header <- '%19s | %15s %15s %15s %15s\n'
-        format.data <- '%19s | %15.0f %15.0f %15.0f %15.0f\n'
-
-        Printf(format.header, 'scenario', 'level-level', 'level-log', 'log-level', 'log-log')
-
-        Rmse <- function(scenario, response.name, predictors.name) {
-            for (result in all.results) {
-                if (result$scenario == scenario &&
-                    result$response.name == response.name &&
-                    result$predictors.name == predictors.name) {
-                    return(result$rmse)
-                }
-            }
-            cat('in Rmse; about to fail', scenario, response.name, predictors.name, '\n'); browser()
-            stop('bad')
-        }
-
-
-        Printf( format.data
-               ,'assessor'
-               ,Rmse('assessor', 'level', 'level'), Rmse('assessor', 'level', 'log')
-               ,Rmse('assessor', 'log', 'level'), Rmse('assessor', 'log', 'log')
-               )
-        Printf( format.data
-               ,'avm w/o assessment'
-               ,Rmse('avm', 'level', 'level'), Rmse('avm', 'level', 'log')
-               ,Rmse('avm', 'log', 'level'), Rmse('avm', 'log', 'log')
-               )
-        Printf( format.data
-               ,'avm w/ assessment'
-               ,Rmse('avm', 'level', 'levelAssessment'), Rmse('avm', 'level', 'logAssessment')
-               ,Rmse('avm', 'log', 'levelAssessment'), Rmse('avm', 'log', 'logAssessment')
-               )
-        Printf( format.data
-               ,'mortgage'
-               ,Rmse('mortgage', 'level', 'levelAssessment'), Rmse('mortgage', 'level', 'logAssessment')
-               ,Rmse('mortgage', 'log', 'levelAssessment'), Rmse('mortgage', 'log', 'logAssessment')
-               )
-        
-
     }
 
 
@@ -208,11 +169,12 @@ Main <- function() {
     #cat('about to PrintResults\n'); browser()
     cat('\n\n\n************* SUMMARY ******************\n')
     Map(PrintResult, all.results)
-    PrintTable(all.results)
+    ModelLinearTestPrintAllResults(all.results)
+    #PrintTable(all.results)
     
 
     #cat('end of Main\n'); browser()
 }
 
-InitializeR()
+InitializeR(duplex.output.to = '../data/v6/output/model-linear-test-log.txt')
 Main()
