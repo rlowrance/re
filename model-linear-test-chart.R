5e2db80efa8e0d87777d08d1ff11315fff0b8d73
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-10 14:12:18 -0400

    new: create chart in separate main program

---
+++ model-linear-test-chart.R
@@ -0,0 +1,23 @@
+# model-linear-test-chart.R
+# main program to produce all charts associated with model-linear-test.R
+# that program compares results on a synthetic data set
+
+source('InitializeR.R')
+source('ModelLinearTestPrintAllResults.R')
+
+Main <- function() {
+    #cat('start Main\n'); browser()
+    path.in  <- '../data/v6/output/model-linear-test.rsave'
+    path.out <- '../data/v6/output/model-linear-test-chart1.txt'
+    loaded.variables <- load(path.in)
+    stopifnot(length(loaded.variables) == 1)
+    stopifnot(loaded.variables[[1]] == 'all.results')
+
+    ModelLinearTestPrintAllResults( all.results
+                                   ,file = path.out
+                                   )
+
+}
+
+InitializeR()
+Main()
