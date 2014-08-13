c267d361fbd1d9611f734ca502ca28b3002319a4
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-13 14:07:33 -0400

    change API

--- DataSynthetic.R
+++ DataSynthetic.R
@@ -1,123 +1,115 @@
-source('IfThenElse.R')
-DataSynthetic <- function(obs.per.day, first.date, last.date, inflation.annual.rate) {
+source('Assess.R')
+source('Printf.R')
+DataSynthetic <- function( obs.per.day
+                          ,first.date
+                          ,last.date
+                          ,market.bias
+                          ,market.sd
+                          ,assessment.bias
+                          ,assessment.sd
+                          ) {
     # generate random synthetic data set
     # RETURNS list
-    # $data : data.frame
-    # $coefficients : list of actual coefficients used to generate the data
+    # $data         : data.frame
+    # $coefficients : list of actual pre-inflation coefficients used to generate the data
     # ARGS
-    # obs.per.day : num, positive, number of observations in each day
-    # first.date  : Date, first saleDate generated
-    # last.date   : Date, last saleDate generated
+    # obs.per.day                            : num, positive, number of observations in each day
+    # first.date                             : Date, first saleDate generated
+    # last.date                              : Date, last saleDate generated
+    # $market.bias                           : num, mean ratio of true value to market value
+    # $market.sd                             : num, market standard deviation as a fraction of mean true value
+    # assessment.relative.bias               : num
+    # assessment.relative.sd                 : num
     #
     # call set.seed() before calling me, if you want reproducability
+
+    GenerateFeatures <- function( coefficients
+                                 ,land.size.min, land.size.max
+                                 ,latitude.min, latitude.max
+                                 ,has.pool.frequency) {
+        # return data frame
+        #cat('start GeneratedFeatures\n'); browser()
+        elapsed.days <- last.date - first.date + 1
+        one.obs.per.day <- data.frame( stringsAsFactors = FALSE
+                                      ,saleDate = first.date + 0:(elapsed.days - 1)
+                                      ,land.size = runif( elapsed.days
+                                                         ,min = land.size.min
+                                                         ,max = land.size.max
+                                                         )
+                                      ,latitude  = runif( elapsed.days
+                                                         ,min = latitude.min
+                                                         ,max = latitude.max
+                                                         )
+                                      ,has.pool  = 
+                                         ifelse( runif(elapsed.days, min = 0, max = 1) > has.pool.frequency
+                                                ,TRUE
+                                                ,FALSE
+                                                )
+                                      )
+        many.obs.per.day <- data.frame( stringsAsFactors = FALSE
+                                       ,saleDate = rep(one.obs.per.day$saleDate, obs.per.day)
+                                       ,land.size = rep(one.obs.per.day$land.size, obs.per.day)
+                                       ,latitude = rep(one.obs.per.day$latitude, obs.per.day)
+                                       ,has.pool = rep(one.obs.per.day$has.pool, obs.per.day)
+                                       )
+        many.obs.per.day
+    }
+
+    GenerateTrueValues <- function(coefficients, features) {
+        # return vector of true values
+        #cat('start GenerateTrueValues\n'); browser()
+        true.value <- 
+            coefficients$intercept +
+            coefficients$land.size * features$land.size +
+            coefficients$latitude  * features$latitude +
+            coefficients$has.pool  * features$has.pool
+    }
+    
+
+    # MAIN BODY STARTS HERE
     #cat('starting DataSynthetic\n'); browser()
     
-    stopifnot(inflation.annual.rate >= 0)
-    stopifnot(inflation.annual.rate <= 1)
-
     coefficients <- list( intercept = 10000
                          ,land.size = 100
                          ,latitude = 20
                          ,has.pool = 20000
                          )
 
-    # model
-    TrueValue <- function(land.size, latitude, has.pool, saleDate) {
-        #cat('starting TrueValue', land.size, latitude, has.pool, saleDate, '\n'); browser()
-
-        beta <- c(10000, 100, 20, 20000)
-        true.value.no.inflation <-
-            coefficients$intercept +
-            coefficients$land.size * land.size +
-            coefficients$latitude * latitude +
-            coefficients$has.pool * has.pool
-
-        # add in inflation
-        elapsed.days <- saleDate - first.date
-        days.per.year <- 365
-        inflation.factor <- 
-            (1 + inflation.annual.rate) ^ as.numeric(elapsed.days / days.per.year)
-        true.value.inflated <- inflation.factor * true.value.no.inflation
-
-        true.value.inflated
-    }
-
-    # generate data frame with randomly selected regressors
-    #cat('start of loop\n'); browser()
-    all.rows <- NULL  # a data.frame   
-    elapsed.days <- last.date - first.date + 1
-    set.seed(123)
-    for (day.number in 1:elapsed.days) {
-        for (obs in 1:obs.per.day) {
-            saleDate <- as.Date(day.number, origin = first.date)
-            # generate transaction on specified date
-            land.size <- runif(1, min = 1, max = 100)
-            latitude <- runif(1, min = 1, max = 90)
-            has.pool <- ifelse(runif(1, min = 0, max = 1) > .5, TRUE, FALSE)
-            true.value <- TrueValue(land.size, latitude, has.pool, saleDate)
-
-            # append to data.frame all.rows
-            next.row <- data.frame( saleDate = saleDate
-                                   ,recordingDate = saleDate + 14  # recorded 2 weeks after sale
-                                   ,land.size = land.size
-                                   ,log.land.size = log(land.size)
-                                   ,latitude = latitude
-                                   ,has.pool = has.pool
-                                   ,true.value = true.value
-                                   )
-            all.rows <- IfThenElse(is.null(all.rows), next.row, rbind(all.rows, next.row))
-        }
-    }
-
-    # price := true.value + market.error
-    # assessment := price + assessment.error
-    #cat('after loop\n'); browser()
-    mean.true.value <- mean(all.rows$true.value)
-    market.error <- rnorm( n = nrow(all.rows)
-                   ,mean = 0
-                   ,sd = mean.true.value * .1
-                   )
-    price <- all.rows$true.value + market.error
-    log.price <- log(price)
-
-    mean.price <- mean(price)
-    assessment.error <- rnorm( n = nrow(all.rows)
-                              ,mean = 0
-                              ,sd = mean.price * .1 
+    generated.features <- GenerateFeatures( coefficients
+                                           ,land.size.min = 1
+                                           ,land.size.max = 100
+                                           ,latitude.min = 0
+                                           ,latitude.max = 90
+                                           ,has.pool.frequency = .5
+                                           )
+    
+    generated.true.values <- GenerateTrueValues(coefficients, generated.features)
+    generated.prices <- rnorm( length(generated.true.values)
+                              ,mean = generated.true.values * market.bias
+                              ,sd   = generated.true.values * market.sd
                               )
-    assessment <- price + assessment.error
+    generated.assessments <- rnorm( length(generated.true.values)
+                                   ,mean = generated.true.values * assessment.bias
+                                   ,sd   = generated.true.values * market.sd
+                                   )
 
-
-
-
-    data <- cbind( all.rows
-                  ,price = price
-                  ,log.price = log.price
-                  ,assessment = assessment
-                  )
-
-    # check how accurate the assessments are vs. the prices
-    if (FALSE) {
-        model <- lm( formula = price ~ assessment + 0
-                    ,data = data
+    result <- cbind( generated.features
+                    ,true.value = generated.true.values
+                    ,price = generated.prices
+                    ,log.price = log(generated.prices)
+                    ,assessment = generated.assessments
                     )
-        print(summary(model))
-        predicted <- predict(model, data)
-        actual <- data$price
-        error <- predicted - actual
-    }
+
     if (FALSE) {
-        # determine how close the assessments are to actual prices
-        error <- assessment - price
-        relative.abs.error <- abs(error / price)
-        is.close <- relative.abs.error < .10
-        fraction.is.close <- sum(is.close) / length(price)
-        cat('fraction of assessments within 10 percent', fraction.is.close, '\n')
+        # check accuracy of market and assessment
+        assess.market <- Assess(list(actual = result$true.value, prediction = result$price))
+        assess.assessment <- Assess(list(actual = result$true.value, prediction = result$assessment))
+        Printf('fraction of prices within 10 percent of true value = %f\n', 
+               assess.market$within.10.percent)
+        Printf('fraction of assessments within 10 percent of true value = %f\n', 
+               assess.assessment$within.10.percent)
     }
 
-    result <- list( data = data
-                   ,coefficients = coefficients
-                   )
     result
 }
 
@@ -126,19 +118,16 @@ DataSynthetic.test <- function() {
     #cat('starting DataSynthetic.test\n'); browser()
     #data <- DataSynthetic(10, as.Date('2007-01-01'), as.Date('2008-12-31'))  # big test set
 
-    if (FALSE) { # big test set
-        data <- DataSynthetic( obs.per.day = 10
-                              ,first.date = as.Date('2007-01-01')
-                              ,last.date =  as.Date('2008-01-01')
-                              ,inflation.annual.rate = .1
-                              )
-    }
+    set.seed(123)
 
-    data <- DataSynthetic( obs.per.day = 1
+    data <- DataSynthetic( obs.per.day = 10
                           ,first.date = as.Date('2007-01-01')
-                          ,last.date =  as.Date('2007-01-01')
-                          ,inflation.annual.rate = .1
-                          )  # tiny test set
+                          ,last.date =  as.Date('2007-01-06')
+                          ,market.bias = 1
+                          ,market.sd = .10
+                          ,assessment.bias = 2
+                          ,assessment.sd = 0
+                          )
 }
 
 DataSynthetic.test()
