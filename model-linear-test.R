ca526a5b5f9b1b0c4de2fba983ced9a67cfe2251
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-14 17:12:56 -0400

    large refactoring

--- model-linear-test.R
+++ model-linear-test.R
@@ -6,93 +6,149 @@ source('Assess.R')
 source('DataSynthetic.R')
 source('InitializeR.R')
 source('MakeModelLinear.R')
-source('ModelLinearTestPrintAllResults.R')
+source('ModelLinearTestMakeReport.R')
 source('Printf.R')
 
-Experiment <- function(assessment.bias, relative.assessment.accuracy) {
+Experiment <- function(assessment.bias, assessment.relative.error) {
     # return data frame with these columns
     # $scenario = name of scenario, in 'assessor', 'avm w/o assessment', 'avm w/ assessment', 'mortgage'
     # $rmse = error from a log-log model, trained for 60 days and testing on Jan 2008 data
-    #cat('start Experiment', assessment.bias, relative.assessment.accuracy, '\n'); browser()
 
-    market.sd = .1
-    data <- DataSynthetic( obs.per.day = 10
-                          ,first.date = as.Date('2007-01-01')
-                          ,last.date = as.Date('2008-01-31')
-                          ,market.bias = 1
-                          ,market.sd = market.sd
-                          ,assessment.bias = switch( assessment.bias
-                                                    ,low = .8
-                                                    ,zero = 1
+
+    MakeSyntheticData <- function(assessment.bias, assessment.relative.error) {
+        # return synthetic data in a data.frame and also the coefficients used to generate that data
+        #cat('start MakeSyntheticData', assessment.bias, assessment.relative.error, '\n'); browser()
+        market.sd = .2  # standard deviation of market prices as a fraction of mean market price
+        ds <- DataSynthetic( obs.per.day = 100
+                            ,first.date = as.Date('2007-01-01')
+                            ,last.date = as.Date('2008-01-31')
+                            ,market.bias = 1
+                            ,market.sd = market.sd
+                            ,assessment.bias = switch( assessment.bias
+                                                      ,low = .8
+                                                      ,zero = 1
+                                                      )
+                            ,assessment.sd = switch( assessment.relative.error
+                                                    ,lower  = 0.5 * market.sd
+                                                    ,same   = 1.0 * market.sd
+                                                    ,higher = 2.0 * market.sd
                                                     )
-                          ,assessment.sd = switch( relative.assessment.accuracy
-                                                  ,more = .5 * market.sd
-                                                  ,same = market.sd
-                                                  ,less = 2 * market.sd
-                                                  )
-                          )
-
-    MakeModelLinearScenario <- function(scenario) {
-        # translate local scenario name into name used by MakeModelLinear
-        switch( scenario
-               ,'assessor' = scenario
-               ,'avm w/o assessment' = 'avmnoa'
-               ,'avm w/ assessment' = 'avm'
-               ,'mortgage' = scenario
-               )
+                            )
+        ds
     }
 
-    Predictors <- function(scenario) {
-        switch( scenario
-               ,'assessor' =
-               ,'avm w/o assessment' = c('land.size', 'latitude', 'has.pool')
-               ,'avm w/ assessment' =
-               ,'mortgage' = c('land.size', 'latitude', 'has.pool', 'assessment')
-               ,stop('bad scenario')
-               )
-    }
+    TestScenarios <- function(data, actual.coefficients) {
+        # return a data.frame containing the results of testing on each of the 4 scenarios
+        # The test is to determine the error on the January transaction after training for 60 days
+        # The model formula is price ~ <predictors>
 
-    all <- NULL
-    for (scenario in c('assessor', 'avm w/o assessment', 'avm w/ assessment', 'mortgage')) {
-        #cat('new scenario', scenario, '\n'); browser()
-        CvModel <- MakeModelLinear( scenario = MakeModelLinearScenario(scenario)
-                                   ,response = 'price'
-                                   ,predictors = Predictors(scenario)
-                                   ,testing.period = list( first.date = as.Date('2008-01-01')
-                                                          ,last.date = as.Date('2008-01-31')
-                                                          )
-                                   ,data = data
-                                   ,num.training.days = 60
-                                   ,verbose.model = TRUE
-                                   )
+        TranslateScenarioName <- function(scenario) {
+            # translate local scenario name into name used by MakeModelLinear
+            switch( scenario
+                   ,'assessor' = scenario
+                   ,'avm w/o assessment' = 'avmnoa'
+                   ,'avm w/ assessment' = 'avm'
+                   ,'mortgage' = scenario
+                   )
+        }
+
+        Predictors <- function(scenario) {
+            switch( scenario
+                   ,'assessor' =
+                   ,'avm w/o assessment' = c('land.size', 'latitude', 'has.pool')
+                   ,'avm w/ assessment' =
+                   ,'mortgage' = c('land.size', 'latitude', 'has.pool', 'assessment')
+                   ,stop('bad scenario')
+                   )
+        }
+
+        ExtractCoefficients <- function(cv.result) {
+            #cat('start ExtractCoefficients\n'); browser()
+            result <- cv.result$fitted$coefficients
+            result
+        }
+
+        CheckCoefficients <- function(fitted.coefficients, actual.coefficients) {
+            #cat('start CheckCoefficients\n'); browser()
+            print(fitted.coefficients)
+            print(actual.coefficients)
+            for (name in names(fitted.coefficients)) {
+                if (name != '(Intercept)') {
+                    actual <- switch( name
+                                     ,'has.poolTRUE' = actual.coefficients$has.pool
+                                     ,actual.coefficients[[name]]
+                                     )
+                    fitted <- fitted.coefficients[[name]]
+                    relative.abs.error <- (abs(actual - fitted) / actual) 
+                    stopifnot(relative.abs.error <= 0.2)
+                }
+            }
+        }
+
+        # BODY START HERE
+        #cat('start TestScenarios', nrow(data), '\n'); browser()
+        
+
         all.data.indices <- 1:nrow(data)
-        cv.result <- CvModel( data = data
-                             ,training.indices = all.data.indices
-                             ,testing.indices = all.data.indices
-                             )
-        assess <- Assess(cv.result)
-        next.row <- data.frame( stringsAsFactors = FALSE
-                               ,scenario = scenario
-                               ,rmse = assess$rmse
-                               )
-        all <- if (is.null(all)) next.row else rbind(all, next.row)
+        all <- NULL
+        for (scenario in c('assessor', 'avm w/o assessment', 'avm w/ assessment', 'mortgage')) {
+            cat('scenario', scenario, '\n')
+            #browser()
+            CvModel <- MakeModelLinear( scenario = TranslateScenarioName(scenario)
+                                       ,response = 'price'
+                                       ,predictors = Predictors(scenario)
+                                       ,testing.period = list( first.date = as.Date('2008-01-01')
+                                                              ,last.date = as.Date('2008-01-31')
+                                                              )
+                                       ,data = data
+                                       ,num.training.days = 60
+                                       ,verbose.model = FALSE
+                                       )
+            cv.result <- CvModel( data = data
+                                 ,training.indices = all.data.indices
+                                 ,testing.indices = all.data.indices
+                                 )
+            if (scenario == 'assessor') {
+                # the coefficients should be close to what was used to generate the data
+                CheckCoefficients(ExtractCoefficients(cv.result), actual.coefficients)
+            }
+            assess <- Assess(cv.result)
+            next.row <- data.frame( stringsAsFactors = FALSE
+                                   ,scenario = scenario
+                                   ,rmse = assess$rmse
+                                   )
+            all <- if (is.null(all)) next.row else rbind(all, next.row)
+        }
+        all
     }
 
-    all
+    # BODY BEGINS HERE
+    cat('start Experiment', assessment.bias, assessment.relative.error, '\n')
+    #browser()
+
+    sd <- MakeSyntheticData(assessment.bias, assessment.relative.error)
+    df <- TestScenarios(sd$data, sd$coefficients)
+    df # return data frame containing result of test, one row per test
+
+
+    # generate synthetic data
+    # define and test models for 4 scenarios on the synthetic data
     
 }
 
-Sweep <- function(f, list1, list2) {
+Sweep <- function(f, assessment.biases, assessment.relative.errors) {
     # return data.frame containing a row for each element in cross product of list1 and list2
     # and scenario and rmse for that scenario on appropriate synthetic data
     #cat('Sweep\n'); browser()
+
+    # build up a data.frame
     all <- NULL
-    for (element1 in list1) {
-        for (element2 in list2) {
-            one <- f(element1, element2)
+    for (assessment.bias in assessment.biases) {
+        for (assessment.relative.error in assessment.relative.errors) {
+            one <- f(assessment.bias, assessment.relative.error)
             new.row <- data.frame( stringsAsFactors = FALSE
-                                  ,assessment.bias = element1
-                                  ,assessment.relative.error = element2
+                                  ,assessment.bias = assessment.bias
+                                  ,assessment.relative.error = assessment.relative.error
                                   ,scenario = one$scenario
                                   ,rmse = one$rmse
                                   )
@@ -105,202 +161,16 @@ Sweep <- function(f, list1, list2) {
 Main <- function() {
     # Run experiments over cross product of situations, return df containing RMSE values
     
-    cat('start Main\n'); browser()
-    result.df <- Sweep( Experiment
-                            ,c('low', 'zero')
-                            ,c('more', 'same', 'less')
-                            )
+    #cat('start Main\n'); browser()
+    result.df <- Sweep( f = Experiment
+                       ,assessment.biases = c('zero', 'low')
+                       ,assessment.relative.errors = c('lower', 'same', 'higher')
+                       )
     save(result.df, file = '../data/v6/output/model-linear-test.rsave')
-    ModelLinearTestPrintAllResults(result.df)
-
+    report <- ModelLinearTestMakeReport(result.df)
+    print(report)
 
     return(result.df)
-    
-    # OLD BELOW ME
-                    
-    experiment.result <- list( Experiment(bias = 'no', accuracy = 'perfect')
-                              ,Experiment(bias = 'no', accuracy = 'more')
-                              ,Experiment(bias = 'no', accuracy = 'equal')
-                              ,Experiment(bias = 'no', accuracy = 'less')
-
-                              ,Experiment(bias = 'less', accuracy = 'perfect')
-                              ,Experiment(bias = 'less', accuracy = 'more')
-                              ,Experiment(bias = 'less', accuracy = 'equal')
-                              ,Experiment(bias = 'less', accuracy = 'less')
-                              )
-
-    PrintExperimentResult <- function(experiment.result) {
-    }
-
-    Map(PrintExperimentResult, experiment.result)
-
-    save(experiment.result, file = '../data/v6/output/model-linear-test.rsave')
-
-
-
-    first.date <- as.Date('2007-01-01')
-    last.date <- as.Date('2008-01-31')
-    obs.per.day <- 10
-    #obs.per.day <- 1; cat('TESTING\n')
-    ds <- DataSynthetic( obs.per.day = obs.per.day
-                        ,first.date = first.date
-                        ,last.date = last.date
-                        ,inflation.annual.rate = 0
-                        )
-    data <- ds$data
-    coefficients <- ds$coefficients
-
-    #cat('in Main\n'); browser()
-    
-    Run <- function(name, ModelCv) {
-        # return RMSE for trained and tested ModelCv (that uses the CrossValidate API)
-        PrintFitted <- function(fitted) {
-            CoefficientsToString <- function(one.fitted) {
-                # return string
-                #cat('start CoefficientsToString\n'); print(str(one.fitted)); browser()
-                coefficients <- one.fitted$coefficients
-                ToString <- function(name) {
-                    value <- coefficients[[name]]
-                    result <- 
-                        switch( name
-                               ,"(Intercept)" = sprintf('%s %7.0f', name, value)
-                               ,has.poolTRUE = sprintf('%s %5f', name, value)
-                               ,sprintf('%s %7.1f', name, value)
-                               )
-                    result
-                }
-
-                s <- Map(ToString, names(coefficients))
-                reduction <- Reduce(paste, s, '')
-                reduction
-            }
-            PrintDateCoefficients <- function(name) {
-                #cat('start PrintDateCoefficients\n'); print(name); browser()
-                one.fitted <- fitted[[name]]
-                Printf('%s: %s\n', as.character(name), CoefficientsToString(one.fitted))
-            }
-
-            #cat('start PrintFitted\n'); browser()
-            if (is.null(fitted$coefficients)) {
-                # a nested structure with many fitted models
-                Map(PrintDateCoefficients, names(fitted))
-            } else {
-                Printf('%s\n', CoefficientsToString(fitted))
-            }
-        }
-
-        #cat('start Run\n'); browser()
-        model.result <- ModelCv( data = data
-                                ,training.indices = 1:nrow(data)
-                                ,testing.indices = 1:nrow(data)
-                                )
-        assess <- Assess(model.result)
-        fitted <- model.result$fitted
-
-        cat('\n************** Result for', name, '\n')
-        cat('mean price', mean(data$price), '\n')
-        print(str(assess))
-        #print(model.result)
-        PrintFitted(fitted)
-        print('Actual coefficients\n'); print(coefficients)
-
-        list( name = name
-             ,rmse = assess$rmse
-             )
-    }
-
-    ParseAndRun <- function(lst) {
-        #cat('start ParseAndRun\n'); print(lst); browser()
-        run <- Run(lst$name, lst$Model)
-        list( scenario = lst$scenario
-             ,response.name = lst$response.name
-             ,predictors.name = lst$predictors.name
-             ,name = run$name
-             ,rmse = run$rmse
-             )
-    }
-
-    NameAndModel <- function(scenario, response.name, predictors.name) {
-
-        Response <- function(response.name) {
-            switch( response.name
-                   ,level = 'price'
-                   ,log = 'log.price'
-                   ,stop('bad response.name')
-                   )
-        }
-
-        Predictors <- function(predictors.name) {
-            #cat('start Predictors', predictors.name, '\n'); browser()
-            switch( predictors.name
-                   ,level = c('land.size', 'latitude', 'has.pool')
-                   ,levelAssessment = c('land.size', 'latitude', 'has.pool', 'assessment')
-                   ,log = c('log.land.size', 'latitude', 'has.pool')
-                   ,logAssessment = c('log.land.size', 'latitude', 'has.pool', 'log.assessment')
-                   ,stop('bad predictors.name')
-                   )
-        }
-        
-        testing.period <- list(first.date = as.Date('2008-01-01'), last.date = as.Date('2008-01-31'))
-        num.training.days <- 60
-        list( name = paste(scenario, response.name, predictors.name)
-             ,scenario = scenario
-             ,response.name = response.name
-             ,predictors.name = predictors.name
-             ,Model = MakeModelLinear( scenario = scenario
-                                      ,response = Response(response.name)
-                                      ,predictors = Predictors(predictors.name)
-                                      ,testing.period = testing.period
-                                      ,data = data
-                                      ,num.training.days = num.training.days
-                                      ,verbose.model = TRUE
-                                      )
-             )
-    }
-    
-    all.results <- 
-        Map( ParseAndRun
-            ,list( NameAndModel('assessor', 'log', 'level')
-                  ,NameAndModel('avm', 'log', 'levelAssessment')
-                  ,NameAndModel('avm', 'log', 'level')
-                  ,NameAndModel('mortgage', 'log', 'levelAssessment')
-
-                  ,NameAndModel('assessor', 'log', 'log')
-                  ,NameAndModel('avm', 'log', 'logAssessment')
-                  ,NameAndModel('avm', 'log', 'log')
-                  ,NameAndModel('mortgage', 'log', 'logAssessment')
-
-                  ,NameAndModel('assessor', 'level', 'level')
-                  ,NameAndModel('avm', 'level', 'levelAssessment')
-                  ,NameAndModel('avm', 'level', 'level')
-                  ,NameAndModel('mortgage', 'level', 'levelAssessment')
-
-                  ,NameAndModel('assessor', 'level', 'log')
-                  ,NameAndModel('avm', 'level', 'logAssessment')
-                  ,NameAndModel('avm', 'level', 'log')
-                  ,NameAndModel('mortgage', 'level', 'logAssessment')
-                  )
-            )
-
-    # save to output
-    print('all.results\n')
-    print(all.results)
-    save(all.results, file='../data/V6/output/model-linear-test.rsave')
-
-    PrintResult <- function(result) {
-        Printf('use case: %40s  RMSE: %.0f\n', result$name, result$rmse)
-    }
-
-
-
-    #cat('about to PrintResults\n'); browser()
-    cat('\n\n\n************* SUMMARY ******************\n')
-    Map(PrintResult, all.results)
-    ModelLinearTestPrintAllResults(all.results)
-    #PrintTable(all.results)
-    
-
-    #cat('end of Main\n'); browser()
 }
 
 InitializeR(duplex.output.to = '../data/v6/output/model-linear-test-log.txt')
