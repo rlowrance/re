c37f50579e684e1ae16674724d1cfdd754a0d147
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-14 20:51:24 -0400

    files moved to repo lowranceutilitiesr

--- Formula.R
+++
@@ -1,7 +0,0 @@
-Formula <- function(response, predictors) {
-    # return formula response ~ predictors
-    result <- as.formula(paste0(response,
-                                '~',
-                                paste0(predictors, collapse = '+')))
-    result
-}
--- Require.R
+++
@@ -1,7 +0,0 @@
-Require <- function(functionName) {
-    # source a file if a function is not defined
-    # ex: Require('Myfunction') --> source('Myfunction.R') if !exists(Myfunction, mode = 'function')
-    if (!exists(functionName, mode = 'function')) {
-        source(paste0(functionName, '.R'))
-    }
-}
