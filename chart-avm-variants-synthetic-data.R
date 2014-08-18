6d3e5c48d3cc35bcfa9a2b77441838aa04ef8ca5
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-18 08:40:19 -0400

    add prefix to explain the data

--- chart-avm-variants-synthetic-data.R
+++ chart-avm-variants-synthetic-data.R
@@ -7,22 +7,37 @@ load_all('/Users/roy/Dropbox/lowrancerealestater')
 
 ReadRsave <- function(control) {
     # return the only variable saved by the experiment
-    cat('start ReadRsave', control$path.in, '\n'); browser()
+    #cat('start ReadRsave', control$path.in, '\n'); browser()
 
     result.df <- NULL
     variables.loaded <- load(control$path.in)
-    stopifnot(length(variables.loaded) == 1)
-    stopifnot(!is.null(result.df))
-    result.df
+    stopifnot(length(variables.loaded) == 2)
+    stopifnot(variables.loaded[[1]] == 'control')
+    stopifnot(variables.loaded[[2]] == 'result.df')
+    result <- list(control = control, result.df = result.df)
+    result
 }
 
-CreateChart1 <- function(control, result.df) {
+CreateChart1 <- function(control, rsave) {
     # return a vector of lines, the txt for chart 1
-    cat('start CreateChart1\n'); browser()
+    #cat('start CreateChart1\n'); browser()
+    result.df <- rsave$result.df
 
     # format of the header and data lines in the report
     format.header <- '%12s %12s %12s %12s %12s %12s'
     format.data <-   '%12s %12s %12.0f %12.0f %12.0f %12.0f'
+
+    # identify key choices in the experiment
+    inflation.rate <- sprintf( 'ASSUMING INFLATION RATE OF %0.2f ANNUALLY'
+                              ,100 * rsave$control$inflation.annual.rate
+                              )
+    prefix <- c( 'COMPARISON OF ROOT MEAN SQUARED ERRORS'
+                ,'BY SCENARIO'
+                ,'BY BIAS IN THE ASSESSMENT'
+                ,'BY ACCURACY OF THE ASSESSMENT RELATIVE TO THE MARKET'
+                ,inflation.rate
+                ,' '
+                )
     
     Header <- function(a,b,c,d,e,f) {
         sprintf(format.header, a, b, c, d, e, f)
@@ -36,16 +51,16 @@ CreateChart1 <- function(control, result.df) {
                 ,Header(' ',          ' ',          ' ',        ' ',          ' ',          ' ')
                 )
 
-    Data <- function(assessment.bias, assessment.relative.error) {
+    Data <- function(assessment.bias.name, assessment.relative.sd.name) {
         Rmse <- function(scenario) {
             #cat('start Rmse', assessment.bias, assessment.relative.error, scenario, '\n'); browser()
-            result.df[result.df$assessment.bias == assessment.bias &
-                      result.df$assessment.relative.error == assessment.relative.error &
+            result.df[result.df$assessment.bias.name== assessment.bias.name &
+                      result.df$assessment.relative.sd.name == assessment.relative.sd.name &
                       result.df$scenario == scenario, 'rmse']
         }
         sprintf( format.data
-                ,assessment.bias
-                ,assessment.relative.error
+                ,assessment.bias.name
+                ,assessment.relative.sd.name
                 ,Rmse('assessor')
                 ,Rmse('avm w/o assessment')
                 ,Rmse('avm w/ assessment')
@@ -53,19 +68,19 @@ CreateChart1 <- function(control, result.df) {
                 )
     }
 
-    for (assessment.bias in unique(result.df$assessment.bias)) {
-        for (assessment.relative.error in unique(result.df$assessment.relative.error)) {
-            line <- Data(assessment.bias, assessment.relative.error)
+    for (assessment.bias.name in unique(result.df$assessment.bias.name)) {
+        for (assessment.relative.sd.name in unique(result.df$assessment.relative.sd.name)) {
+            line <- Data(assessment.bias.name, assessment.relative.sd.name)
             report <- c(report, line)
         }
         report <- c(report, ' ')
     }
-    report
+    c(prefix, report)
 }
 
 
 Main <- function() {
-    cat('start Main'); browser()
+    #cat('start Main\n'); browser()
 
     path.output = '../data/v6/output/'
     my.name <- 'chart-avm-variants-synthetic-data'
@@ -79,14 +94,15 @@ Main <- function() {
     print(control)
 
 
-    result.df <- ReadRsave(control)
+    rsave <- ReadRsave(control)
     chart1 <- CreateChart1( control = control
-                           ,result.df = result.df
+                           ,rsave = rsave
                            )
-    cat('about to write\n'); browser()
+    #cat('about to write\n'); browser()
     writeLines( text = chart1
                ,con = control$path.out.chart1
                )
+    writeLines( text=chart1)
 
     print(control)
 }
