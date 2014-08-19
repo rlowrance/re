8426a2db63fe393869a5b6faa3f4b1a608766a7e
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-19 08:01:11 -0400

    part of shasha.2014-08-08

--- chart-avm-variants-synthetic-data.R
+++ chart-avm-variants-synthetic-data.R
@@ -18,25 +18,24 @@ ReadRsave <- function(control) {
     result
 }
 
-CreateChart1 <- function(control, rsave) {
-    # return a vector of lines, the txt for chart 1
-    #cat('start CreateChart1\n'); browser()
+ChartComponents <- function(control, rsave) {
+    # return a list, each element a vector of lines, for the components in a chart
+    #cat('start ChartComponents\n'); browser()
     result.df <- rsave$result.df
 
     # format of the header and data lines in the report
-    format.header <- '%12s %12s %12s %12s %12s %12s'
-    format.data <-   '%12s %12s %12.0f %12.0f %12.0f %12.0f'
+    format.header <- '   %12s %12s %12s %12s %12s %12s'
+    format.data <-   '%12s %12s %12.0f %12.0f %12.0f %12.0f'  # line numbers NN_ are prepended
 
     # identify key choices in the experiment
     inflation.rate <- sprintf( 'ASSUMING INFLATION RATE OF %0.2f ANNUALLY'
                               ,100 * rsave$control$inflation.annual.rate
                               )
-    prefix <- c( 'COMPARISON OF ROOT MEAN SQUARED ERRORS'
+    title <- c( 'COMPARISON OF ROOT MEAN SQUARED ERRORS'
                 ,'BY SCENARIO'
                 ,'BY BIAS IN THE ASSESSMENT'
                 ,'BY ACCURACY OF THE ASSESSMENT RELATIVE TO THE MARKET'
                 ,inflation.rate
-                ,' '
                 )
     
     Header <- function(a,b,c,d,e,f) {
@@ -44,41 +43,96 @@ CreateChart1 <- function(control, rsave) {
     }
 
 
-    report <- c( Header(' ',          ' ',          ' ' ,       'RMSE',       'RMSE',       ' ')
+    header <- c( Header(' ',          ' ',          ' ' ,       'RMSE',       'RMSE',       ' ')
                 ,Header(' ',          'assessment', ' ' ,       'avm',        'avm',        ' ' )
                 ,Header('assessment', 'relative',   'RMSE',     'w/o',        'w/',         'RMSE')
                 ,Header('bias',       'error',      'assessor', 'assessment', 'assessment', 'mortgage')
-                ,Header(' ',          ' ',          ' ',        ' ',          ' ',          ' ')
                 )
 
-    Data <- function(assessment.bias.name, assessment.relative.sd.name) {
+    Data <- function(assessment.bias.name, assessment.relative.sd.name, body.line.number) {
+        # return data line 
         Rmse <- function(scenario) {
             #cat('start Rmse', assessment.bias, assessment.relative.error, scenario, '\n'); browser()
             result.df[result.df$assessment.bias.name== assessment.bias.name &
                       result.df$assessment.relative.sd.name == assessment.relative.sd.name &
                       result.df$scenario == scenario, 'rmse']
         }
-        sprintf( format.data
-                ,assessment.bias.name
-                ,assessment.relative.sd.name
-                ,Rmse('assessor')
-                ,Rmse('avm w/o assessment')
-                ,Rmse('avm w/ assessment')
-                ,Rmse('mortgage')
+        line.without.number <- sprintf( format.data
+                                       ,assessment.bias.name
+                                       ,assessment.relative.sd.name
+                                       ,Rmse('assessor')
+                                       ,Rmse('avm w/o assessment')
+                                       ,Rmse('avm w/ assessment')
+                                       ,Rmse('mortgage')
+                                       )
+        line.with.number <- sprintf('%02d %s', body.line.number, line.without.number)
+        line.with.number
+    }
+
+    body <- NULL
+    body.line.number <- 0
+    for (assessment.bias.name in unique(result.df$assessment.bias.name)) {
+        for (assessment.relative.sd.name in unique(result.df$assessment.relative.sd.name)) {
+            body.line.number <- body.line.number + 1  
+            line <- Data(assessment.bias.name, assessment.relative.sd.name, body.line.number)
+            body <- c(body, line)
+        }
+        body <- c(body, ' ')
+    }
+
+    ARE <- function(name) {
+        sprintf( 'assessment relative error: %s --> sd(assessment error) = %0.2f x true value'
+                ,name
+                ,rsave$control$assessment.relative.sd.values[[name]]
                 )
     }
 
-    for (assessment.bias.name in unique(result.df$assessment.bias.name)) {
-        for (assessment.relative.sd.name in unique(result.df$assessment.relative.sd.name)) {
-            line <- Data(assessment.bias.name, assessment.relative.sd.name)
-            report <- c(report, line)
-        }
-        report <- c(report, ' ')
+    AREM <- function(name) {
+        value <- rsave$control$assessment.relative.sd.values[[name]]
+        multiplier <- value / rsave$control$market.sd.fraction
+        sprintf( 'assessment relative error: %s --> sd(assessment error) = %4.2f x sd(market value)'
+                ,name
+                ,multiplier
+                )
     }
-    c(prefix, report)
+
+    legend <- c( 'assessment bias: zero --> mean(assessment) == mean(true value)'
+                ,'assessment bias: lower --> mean(assessment) < mean(true value)'
+                ,'assessment bias: higher --> mean(assessment) > mean(true value)'
+                ,ARE('nearzero')
+                ,AREM('lower')
+                ,AREM('same')
+                ,AREM('higher')
+                ,sprintf('where sd market value = %4.2f', rsave$control$market.sd.fraction)
+                )
+
+    result <- list( title = title
+                   ,header = header
+                   ,body = body
+                   ,legend = legend
+                   )
+    result
 }
 
+Chart1 <- function(components) {
+    result <- c( components$title
+                ,' '
+                ,components$header
+                ,' '
+                ,components$body
+                )
+    result
+}
 
+Chart2 <- function(components) {
+    #cat('start CreateChart2\n'); browser()
+    result <- c( Chart1(components)
+                ,' '
+                ,components$legend
+                )
+    result
+}
+    
 Main <- function() {
     #cat('start Main\n'); browser()
 
@@ -88,6 +142,7 @@ Main <- function() {
     control <- list( path.in = paste0(path.output, experiment.name, '.rsave')
                     ,path.out.log = paste0(path.output, my.name, '.log')
                     ,path.out.chart1 = paste0(path.output, my.name, '-chart1.txt')
+                    ,path.out.chart2 = paste0(path.output, my.name, '-chart2.txt')
                     )
     
     InitializeR(duplex.output.to = control$path.out.log)
@@ -95,14 +150,22 @@ Main <- function() {
 
 
     rsave <- ReadRsave(control)
-    chart1 <- CreateChart1( control = control
-                           ,rsave = rsave
-                           )
-    #cat('about to write\n'); browser()
+    
+    chart.components <- ChartComponents( control = control
+                                        ,rsave = rsave
+                                        )
+
+    chart1 <- Chart1(chart.components)
     writeLines( text = chart1
                ,con = control$path.out.chart1
                )
-    writeLines( text=chart1)
+
+    chart2 <- Chart2(chart.components)
+    writeLines( text = chart2
+               ,con = control$path.out.chart2
+               )
+
+    writeLines( text = chart2)  # append to stdout
 
     print(control)
 }
