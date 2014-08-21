f44af112fa0b0634159ac5d36c785149e1d8f801
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-20 21:11:05 -0400

    stratify data by neighborhood wealth

--- e-avm-variants.R
+++ e-avm-variants.R
@@ -60,11 +60,13 @@ DefineModelsNames <- function(control, data) {
     }
 
     Models <- list( MakeModel('assessor', control$predictors.without.assessment)
+                   ,MakeModel('assessor', control$predictors.with.assessment)
                    ,MakeModel('avm', control$predictors.without.assessment)
                    ,MakeModel('avm', control$predictors.with.assessment)
                    ,MakeModel('mortgage', control$predictors.with.assessment)
                    )
-    names <- c( 'assessor with assessment'
+    names <- c( 'assessor without assessment'
+               ,'assessor with assessment'
                ,'avm without assessment'
                ,'avm with assessment'
                ,'mortgage with assessment'
@@ -75,6 +77,7 @@ DefineModelsNames <- function(control, data) {
 }
  
 ExperimentResult <- function(cv.result, experiment.names) {
+    # return data.frame
     #cat('start ExperimentResult\n'); browser()
     fold.assessment <- cv.result$fold.assessment
 
@@ -104,6 +107,52 @@ ExperimentResult <- function(cv.result, experiment.names) {
     experiment.result
 }
 
+Stratify <- function(control, data){
+    # break data.frame into MECE subsets (stratum)
+    # for now, criteria is median.household.income and the 3 strata are wealth, poor, middle class
+    # Returns list with these elements
+    # $data : list of strata, each a data.frame
+    # $name : chr vector of names for the corresponding strata
+    #cat('start Stratify', nrow(data), '\n'); browser()
+
+    # determine where to split
+    #print(summary(data))
+    median.median.household.income <- median(data$median.household.income)
+    is.rich <- data$median.household.income > control$rich * median.median.household.income
+    is.poor <- data$median.household.income < control$poor * median.median.household.income
+    is.middle <- (!is.rich) & (!is.poor)
+
+    result <- list( data = list( data[is.rich,]
+                                ,data[is.poor,]
+                                ,data[is.middle,]
+                                )
+                   ,name = list( ' wealthy neighborhoods'
+                                ,'poor neighborhoods'
+                                ,'middle class neighborhoods'
+                                )
+                   )
+    result
+}
+
+CvExperiment <- function(control, data, models.names) { 
+    # cross validate to determine expected generalization error, then tabulate experimental results
+    #cat('start CvExperiment\n'); browser()
+
+    cv.result <-  # 
+        CrossValidate( data = data
+                      ,nfolds = if (control$testing) 2 else control$nfolds
+                      ,Models = models.names$Models
+                      ,Assess = Assess
+                      ,experiment = models.names$experiment.names
+                      )
+
+    experiment.result <- ExperimentResult( cv.result = cv.result
+                                          ,experiment.names = models.names$names
+                                          )
+
+    experiment.result
+}
+
 Main <- function(split.names) {
     #cat('start Main'); browser()
 
@@ -125,6 +174,9 @@ Main <- function(split.names) {
                                            ,last.date = as.Date('2008-01-31')
                                            )
                     ,num.training.days = 60
+                    ,rich = 2.0
+                    ,poor = 0.5
+                    ,testing = FALSE
                     )
 
     InitializeR(duplex.output.to = control$path.out.log)
@@ -137,31 +189,62 @@ Main <- function(split.names) {
     
     models.names <- DefineModelsNames( control = control
                                       ,data = data)
-    cv.result <-  # 
-        CrossValidate( data = data
-                      ,nfolds = control$nfolds
-                      ,Models = models.names$Models
-                      ,Assess = Assess
-                      ,experiment = models.names$experiment.names
-                      )
-    experiment.result <- ExperimentResult( cv.result = cv.result
-                                          ,experiment.names = models.names$names
-                                          )
 
-    # print results of the experiment
-    Printf( 'Experiment Results for reponse variable %s predictors %s nfolds %d\n'
-           ,control$response
-           ,control$predictors
-           ,control$nfolds
-           )
-    
-    print(experiment.result)
+    # determine results for stratified versions of the data
+    stratify <- Stratify(control, data)
+
+    StrataResult <- function(strata.index) {
+        #cat('start StrataResult', strata.index, '\n'); browser()
+        strata.name <- stratify$name[[strata.index]]
+        strata.data <- stratify$data[[strata.index]]
+        options(warn = 1)  # expect warning: no samples were selected for training
+        experiment.result <- CvExperiment( control = control
+                                          ,data = strata.data
+                                          ,models.names = models.names
+                                          )
+        options(warn = 2)  # convert warnings into errors
+        result <- list( strata.name = stratify$name[[strata.index]]
+                       ,experiment.result = experiment.result
+                       )
+
+        result
+    }
+
+    #strata.result <- Map(StrataResult, c(2))
+    strata.results <- Map( StrataResult, 1:length(stratify$name))
+    all.result <- CvExperiment( control = control
+                               ,data = data
+                               ,models.names = models.names
+                               )
+
+    # print both sets of results
+
+    cat('strata.results\n')
+    print(str(strata.results))
+
+    cat('all.result\n')
+    print(str(all.result))
+
+    #cat('examine\n'); browser()
+
+    PrintStrataResult <- function(strata.result) {
+        cat('strata.name', strata.result$strata.name, '\n')
+        cat('experiment results\n')
+        print(strata.result$experiment.result)
+        cat(' ')
+    }
+
+    Map(PrintStrataResult, strata.results)
+
+    cat('all.result\n')
+    print(all.result)
 
     # save results
     description <- 'Cross Validation Result\nLog-Level model\nPredict Jan 2008 transactions using 60 days of training data'
-    save(description, control, cv.result, experiment.result, file = control$path.out.save)
+    save(description, control, all.result, strata.results, file = control$path.out.save)
 
     print(control)
+    if (control$testing) cat('DISCARD RESULTS: TESTING\n')
 }
 
 
