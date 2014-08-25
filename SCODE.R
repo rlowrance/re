17ce3008c4932e734a07ecb7c0cc5e6a67ebc162
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-25 06:59:45 -0400

    add code to description

--- SCODE.R
+++ SCODE.R
@@ -4,24 +4,57 @@ SCODE <- function(code, kind) {
     # code  : character vector
     # kind  : chr scalar
 
-    stopifnot(all(is.character(code)))
-    with.na <- 
-    switch(kind,
-           confirmed            = code == 'C',
-           estimated            = code == 'E',
-           sale.price.full      = code == 'F',
-           sale.price.partial.1 = code == 'L',
-           not.of.public.record = code == 'N',
-           sale.price.partial.2 = code == 'P',
-           lease                = code == 'R',
-           unknown              = code == 'U',
-           verified             = code == 'V',
-           # composite types NONE FOR NOW
-           sale.price.partial   = (code == 'L') | (code == 'P'),
+    Match <-function(code, kind) {
+        with.na <- 
+            switch( kind
+                   ,confirmed            = code == 'C'
+                   ,estimated            = code == 'E'
+                   ,sale.price.full      = code == 'F'
+                   ,sale.price.partial.1 = code == 'L'
+                   ,not.of.public.record = code == 'N'
+                   ,sale.price.partial.2 = code == 'P'
+                   ,lease                = code == 'R'
+                   ,unknown              = code == 'U'
+                   ,verified             = code == 'V'
+                   # composite types NONE FOR NOW
+                   ,sale.price.partial   = (code == 'L') | (code == 'P')
 
-           # default
-           ...                             = stop(sprintf('bad kind = %s', kind))
-           )
-    result <- ifelse(is.na(with.na), FALSE, with.na)
+                   # default
+                   ,...                             = stop(sprintf('bad kind = %s', kind))
+                   )
+        ifelse(is.na(with.na), FALSE, with.na)
+    }
+
+    LookupCode <- function(code) {
+        # lookup 1 or more codes
+        LookupOneCode <- function(one.code) {
+            switch( one.code
+                   ,'C' = 'confirmed'
+                   ,'E' = 'estimated'
+                   ,'F' = 'sale.price.full'
+                   ,'L' = 'sale.price.partial.1'
+                   ,'N' = 'not.of.public.record'
+                   ,'P' = 'sale.price.partial.2'
+                   ,'R' = 'lease'
+                   ,'U' = 'unknown'
+                   ,'V' = 'verified'
+                   ,'L' = 'sale.price.partial'
+                   ,'P' = 'sale.price.partial'
+                   ,'unknown code'
+                   )
+        }
+        if (length(code) == 1) LookupOneCode(code) else sapply(code, LookupOneCode)
+    }
+
+    stopifnot(all(is.character(code)))
+    
+    #cat('start SCODE\n'); browser()
+    result <- 
+        if (hasArg('code') && hasArg('kind')) 
+            Match(code, kind)
+        else if (hasArg('code'))
+            LookupCode(code)
+        else
+            stop('bad call')
     result
 }
