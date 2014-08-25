f38c9502f76c912abd49eeccb9c78ce4f27f2bed
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-25 07:00:16 -0400

    add code to description

--- TRNTP.R
+++ TRNTP.R
@@ -4,21 +4,54 @@ TRNTP <- function(code, kind) {
     # code : integer vector
     # kind  : optional chr scalar
 
-    stopifnot(all(is.numeric(code)))
-    with.na <- 
-    switch(kind,
-           resale            = code == 1,
-           refinance         = code == 2,
-           new.construction  = code == 3,
-           timeshare         = code == 4,
-           construction.loan = code == 6,
-           seller.carryback  = code == 7,
-           nominal           = code == 9,
-           # composite types NONE FOR NOW
+    Match <- function(code, kind) {
+        with.na <- 
+            switch( kind
+                   ,resale            = code == 1
+                   ,refinance         = code == 2
+                   ,new.construction  = code == 3
+                   ,timeshare         = code == 4
+                   ,construction.loan = code == 6
+                   ,seller.carryback  = code == 7
+                   ,nominal           = code == 9
+                   # composite types NONE FOR NOW
 
-           # default
-           ...                             = stop(sprintf('bad kind = %s', kind))
-           )
-    result <- ifelse(is.na(with.na), FALSE, with.na)
+                   # default
+                   ,...                             = stop(sprintf('bad kind = %s', kind))
+                   )
+        ifelse(is.na(with.na), FALSE, with.na)
+    }
+
+    LookupCode <- function(code) {
+        # lookup one or more codes
+        OneCode <- function(one.code) {
+            if (1 <= one.code && one.code <= 9)  {
+                switch( one.code
+                       ,'resale'            # 1
+                       ,'refinance'         # 2
+                       ,'new.construction'  # 3
+                       ,'timeshare'         # 4
+                       ,'unknown code'
+                       ,'construction.loan' # 6
+                       ,'seller.caryback'   # 7
+                       ,'unknown code'
+                       ,'nominal'           # 9
+                       )
+            } else {
+                'unknown code'
+            }
+        }
+        if (length(code) == 1) OneCode(code) else sapply(code, OneCode)
+    }
+
+    stopifnot(all(is.numeric(code)))
+
+    result <- 
+        if (hasArg('code') && hasArg('kind')) 
+            Match(code, kind)
+        else if (hasArg('code'))
+            LookupCode(code)
+        else
+            stop('bad call')
     result
 }
